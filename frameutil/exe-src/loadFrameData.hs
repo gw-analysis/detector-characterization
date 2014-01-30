5e42419b9d0f5afe30e35799d2c07ffb72a73247
enoshima <kazuhiro.hayama@gmail.com>
2014-01-30 23:51:42 +0900

    changed plot style from linear to log scale

--- frameutil/exe-src/loadFrameData.hs
+++ frameutil/exe-src/loadFrameData.hs
@@ -1,4 +1,6 @@
 
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import HasKAL_FrameUtils
 
 {- For fft -}
@@ -15,10 +17,15 @@ import Data.Colour
 import Data.Colour.Names
 import Control.Lens
 import Data.Default.Class
+import Graphics.Rendering.Chart.Gtk
+import Text.Printf
 
 
 
-main :: IO (PickFn ())
+{- for renderableToPNGFile -}
+-- main :: IO (PickFn ())
+{- for renderableToWindow-}
+--main :: IO()
 main = do
   fdata <- readFrame "Channel_Name" "test-1066392016-300.gwf"
 --  print $ show $ take 3 (eval fdata)
@@ -34,14 +41,66 @@ main = do
 -- plotting --
       sampleRate2 = sampleRate/2
       fvec    = map (sampleRate2 *) $ toList (linspace len_power2 (0, 1::Double))
-      plotLines = plot_lines_values .~ [(zip fvec (map sqrt powerspectrum))]
+      plotLines = plot_lines_values .~ values
                   $ plot_lines_style . line_color .~ opaque red
                   $ plot_lines_title .~ "mag"
                   $ def
       layout    = layout_plots .~ [toPlot plotLines]
                   $ layout_title .~ "Magnetic field at the KAGRA site"
+                  $ layout_title_style . font_size .~ 20
+                  $ layout_x_axis . laxis_title .~ "frequency[Hz]" 
+                  $ layout_x_axis . laxis_style . axis_label_style . font_size .~ 20 
+                  $ layout_x_axis . laxis_title_style . font_size .~ 20
+                  $ layout_y_axis . laxis_title .~ "spectrum[T/Hz]" 
+                  $ layout_y_axis . laxis_title_style . font_size .~ 20
+                  $ layout_y_axis . laxis_style . axis_label_style . font_size .~ 20
+                  $ layout_y_axis . laxis_generate .~ autoScaledLogAxis logPSDAxis
+                  $ layout_legend .~ Just (legend_label_style . font_size .~ 20 $ def)       
                   $ def
+      values = [[(LogValue x, LogValue y)|(x,y)<- zip fvec (map sqrt powerspectrum)]]          
 
+--  renderableToWindow (toRenderable layout) 640 480
   renderableToPNGFile (toRenderable layout) 640 480  "test_magnetic.png"
+
+
+
+{- 
+
+Helper function 
+
+-}
+
+logPSDAxis = loga_labelf .~ (mags . fromLogValue) $ def
+mags :: Double -> String
+mags  k
+    | k < 0      = '-' : mags (-k)
+    | k >= 1e9   = (k/1e9)  `with` "G"
+    | k >= 1e6   = (k/1e6)  `with` "M"
+    | k >= 1e4   = (k/1e3)  `with` "K"
+    | k >= 1     = k        `with` " "
+    | k >= 1e-3  = (k*1e3)  `with` "m"
+    | k >= 1e-6  = (k*1e6)  `with` "µ"
+    | k >= 1e-9  = (k*1e9)  `with` "n"
+    | k >= 1e-12 = (k*1e12) `with` "p"
+    | otherwise  = printf "%g s" k
+    where with (t :: Double) (u :: String)
+              | t >= 1e9  = printf "%.4g %s" t u
+              | t >= 1e6  = printf "%.0f %s" t u
+              | t >= 1e5  = printf "%.0f %s" t u
+              | t >= 1e4  = printf "%.0f %s" t u
+              | t >= 1e3  = printf "%.0f %s" t u
+              | t >= 1e2  = printf "%.0f %s" t u
+              | t >= 1e1  = printf "%.0f %s" t u
+              | otherwise = printf "%.0f %s" t u
+
+fromLogValue (LogValue v) = v
+
+
+
+
+
+
+
+
 
 
