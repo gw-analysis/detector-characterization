48195c2e6760dfe7813f54f49c6d46e0052d5429
enoshima <kazuhiro.hayama@gmail.com>
2014-02-09 16:04:02 +0900

    loadFrameData.hs updated

--- HasKAL/exe-src/loadFrameData.hs
+++ HasKAL/exe-src/loadFrameData.hs
@@ -1,53 +1,3 @@
-<<<<<<< HEAD:HasKAL/exe-src/loadFrameData.hs
-
-import HasKAL.FrameUtils.FrameUtils
-
-{- For fft -}
-import Numeric.GSL.Fourier
-import Numeric.LinearAlgebra
-
-{- For complex number-}
---import Data.Complex
-
-{- For plotting -}
-import Graphics.Rendering.Chart
-import Graphics.Rendering.Chart.Backend.Cairo
-import Data.Colour
-import Data.Colour.Names
-import Control.Lens
-import Data.Default.Class
-
-
-
-main :: IO (PickFn ())
-main = do
-  fdata <- readFrame "Channel_Name" "test-1066392016-300.gwf"
---  print $ show $ take 3 (eval fdata)
-  let sampleRate = 1000::Double
-  let dat = subVector 1 15000 (fromList $ map realToFrac (eval fdata))
-      fft_val = fft $ dat
-      power =  map abs $ toList $ fst . fromComplex $ fft_val * conj fft_val
-      len_power = length power
-      scale_psd = 1/((fromIntegral len_power) * sampleRate)
-      len_power2 = floor $ fromIntegral(len_power)/2
-      powerspectrum = take len_power2  $ map (*scale_psd) power
-
--- plotting --
-      sampleRate2 = sampleRate/2
-      fvec    = map (sampleRate2 *) $ toList (linspace len_power2 (0, 1::Double))
-      plotLines = plot_lines_values .~ [(zip fvec (map sqrt powerspectrum))]
-                  $ plot_lines_style . line_color .~ opaque red
-                  $ plot_lines_title .~ "mag"
-                  $ def
-      layout    = layout_plots .~ [toPlot plotLines]
-                  $ layout_title .~ "Magnetic field at the KAGRA site"
-                  $ def
-
-  renderableToPNGFile (toRenderable layout) 640 480  "test_magnetic.png"
-
-
-=======
-
 {-# LANGUAGE ScopedTypeVariables #-}
 
 import HasKAL.FrameUtils.FrameUtils
@@ -128,7 +78,7 @@ mags  k
     | k >= 1e4   = (k/1e3)  `with` "K"
     | k >= 1     = k        `with` " "
     | k >= 1e-3  = (k*1e3)  `with` "m"
-    | k >= 1e-6  = (k*1e6)  `with` "µ"
+    | k >= 1e-6  = (k*1e6)  `with` "袖"
     | k >= 1e-9  = (k*1e9)  `with` "n"
     | k >= 1e-12 = (k*1e12) `with` "p"
     | otherwise  = printf "%g s" k
@@ -144,13 +94,3 @@ mags  k
 
 fromLogValue (LogValue v) = v
 
-
-
-
-
-
-
-
-
-
->>>>>>> d4e5feb416a2dd90a8eaea8f75e11df8ba6e06fd:frameutil/exe-src/loadFrameData.hs
