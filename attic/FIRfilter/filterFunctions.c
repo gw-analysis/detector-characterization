2c27d85bc929ab44124e1858eb0d1de323e2ed86
enoshima <kazuhiro.hayama@gmail.com>
2014-03-14 16:40:17 +0900

    start FIR implementation

---
+++ attic/FIRfilter/filterFunctions.c
@@ -0,0 +1,19 @@
+double fir_filter (double input, double fir_coeff[], double fir_buffer[], unsigned *index, unsigned length){
+
+    double output;
+    unsigned idx;
+
+    fir_buffer[*index]        = input;
+    fir_buffer[*index+length] = input;
+
+    *index=*index+1;
+    if(*index==length) *index=0;
+
+    output = 0;
+    for (idx=0;idx<length;idx++){
+        output = output + fir_coeff[idx]*fir_buffer[*index+idx];
+    }
+
+    return output;
+}
+
