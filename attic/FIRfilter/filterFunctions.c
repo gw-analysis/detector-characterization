fa9c3bdd70551feeaebf53cee0940ad304b2ee83
enoshima <kazuhiro.hayama@gmail.com>
2014-03-15 23:14:11 +0900

    FIR filter updated

--- attic/FIRfilter/filterFunctions.c
+++ attic/FIRfilter/filterFunctions.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-double fir_filter (double input, double fir_coeff[], double fir_buffer[], unsigned *index, unsigned length){
+int fir_filter (double *output,  double *input, unsigned inputlen, double fir_coeff[], double fir_buffer[], unsigned *index, unsigned length){
 
-    double output;
-    unsigned idx;
+    unsigned idx, oidx;
+    //const int Nin = sizeof input /sizeof input[0];
+    //output = (double *)malloc(sizeof(double)*Nin);
+    //memset(output, 0, sizeof(double)*Nin);
 
-    fir_buffer[*index]        = input;
-    fir_buffer[*index+length] = input;
+    for (oidx=0;oidx<inputlen;oidx++){
 
-    *index=*index+1;
-    if(*index==length) *index=0;
+        fir_buffer[*index]        = input[oidx];
+        fir_buffer[*index+length] = input[oidx];
 
-    output = 0;
-    for (idx=0;idx<length;idx++){
-        output = output + fir_coeff[idx]*fir_buffer[*index+idx];
+        *index=*index+1;
+        if(*index==length) *index=0;
+
+        //utput[0]=0;
+        for (idx=0;idx<length;idx++){
+            output[oidx] = output[oidx]
+                + fir_coeff[idx]*fir_buffer[*index+idx];
+        }
+        //*index=*index+1;
+
     }
 
-//    printf("Filter output is %f\n", output);
-    return output;
+    return 1;
 }
 
--- attic/FIRfilter/go_fir.c
+++ attic/FIRfilter/go_fir.c
@@ -1,14 +1,16 @@
 
 #include <stdio.h>
 
-# define FIR_LEN 1000
-double x,y;
+# define FIR_LEN 4
+double x[]={1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21};
+double y[]={0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0};
 double fir_buffer[2*FIR_LEN];
 double fir_coeff[FIR_LEN];
 unsigned fir_ix;
 unsigned temp;
+unsigned inputlen = 11;
 
-double fir_filter(double, double[], double[], unsigned*, unsigned);
+double fir_filter(double*, double*, unsigned, double[], double[], unsigned*, unsigned);
 
 int main (void){
     printf("Start program.\n");
@@ -19,17 +21,19 @@ int main (void){
         fir_buffer[temp+FIR_LEN]=0.0;
     }
     // impulse response of echo
-    fir_coeff[490]=1;
-    fir_coeff[440]=0.6;
-    fir_coeff[200]=0.3;
-    fir_coeff[130]=0.6;
-    fir_coeff[1]=0.4;
+    //fir_coeff[490]=1;
+    //fir_coeff[440]=0.6;
+    //fir_coeff[200]=0.3;
+    //fir_coeff[130]=0.6;
+    fir_coeff[3]=0;
+    fir_coeff[2]=0;
+    fir_coeff[1]=0;
+    fir_coeff[0]=1;
     fir_ix=0;
 
     //main part
-    x = 5;
-    y = fir_filter(x, fir_coeff, fir_buffer, &fir_ix, FIR_LEN);
-    printf("Filter output is %f\n", y);
+    fir_filter(y, x, inputlen, fir_coeff, fir_buffer, &fir_ix, FIR_LEN);
+    printf("Filter output is %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n", y[0], y[1], y[2], y[3], y[4], y[5], y[6], y[7], y[8], y[9], y[10], y[11]);
     return 1;
 
 }
