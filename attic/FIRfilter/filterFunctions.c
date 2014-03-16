2f0b83b7a9f9235ab739a44dd331126b6175278e
enoshima <kazuhiro.hayama@gmail.com>
2014-03-16 23:15:15 +0900

    IIR filter implemented

--- attic/FIRfilter/filterFunctions.c
+++ attic/FIRfilter/filterFunctions.c
@@ -6,6 +6,42 @@
 //-- prototype
 int fir_filter_core(double*,  double*,  unsigned,  double[],  double[],  unsigned*,  unsigned);
 int fir_filter (double*, double*, unsigned, double[], unsigned);
+int iir_filter (double*,  double*,  unsigned,  double[],  double[], unsigned);
+
+
+
+int iir_filter (double *output, double *input, unsigned inputlen, double num_coeff[], double denom_coeff[], unsigned filterlen){
+
+    unsigned inputidx, k;
+    double y, Reg[filterlen];
+
+    //-- initialize the delay registers
+    for (k=0;k<filterlen;k++){
+        Reg[k] = 0.0;
+    }
+
+    for (inputidx=0;inputidx<inputlen;inputidx++){
+        //-- Shift the delay register values.
+        for (k=filterlen;k>0;k--) {
+            Reg[k] = Reg[k-1];
+        }
+
+        //-- denominator part
+        Reg[0] = input[inputidx];
+        for (k=1;k<=filterlen;k++) {
+            Reg[0] -= denom_coeff[k] * Reg[k];
+        }
+
+        //-- numerator part
+        y = 0;
+        for (k=0;k<=filterlen;k++) {
+            y += num_coeff[k] * Reg[k];
+        }
+        output[inputidx] = y;
+    }
+    return 1;
+}
+
 
 
 int fir_filter (double *output, double *input, unsigned inputlen, double fir_coeff[], unsigned filterlen){
