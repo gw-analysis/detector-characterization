9e34418e1dc001e02e51a43c25a84725bf6ff0b5
enoshima <kazuhiro.hayama@gmail.com>
2014-04-11 18:57:09 +0900

    gwpsd updated

--- attic/debug_spectruutils/SpectrumUtils.hs
+++ attic/debug_spectruutils/SpectrumUtils.hs
@@ -18,19 +18,25 @@ gwpsd dat nfft fs = do
   let ndat = length dat
       maxitr = floor $ fromIntegral (ndat) / fromIntegral (nfft) :: Int
       datlist = takesV (take maxitr (repeat nfft)) $ fromList dat
-      fft_val = map fft
-              $ map toComplex
-              $ map (tuplify2 (constant 0 nfft)) (map (windowed (hanning nfft)) datlist)
+      fft_val = applyFFT . applytoComplex . applyTuplify2 . applyWindow $ datlist
       power =  map (abs . fst . fromComplex) $ zipWith (*) fft_val (map conj fft_val)
       meanpower = scale (1/(fromIntegral maxitr)) $ foldr (+) (zeros nfft) power
       scale_psd = 1/(fromIntegral nfft * fs)
   toList $ scale scale_psd meanpower
-
+  where
+    applyFFT :: [Vector (Complex Double)] -> [Vector (Complex Double)]
+    applyFFT = map fft
+    applytoComplex :: [(Vector Double, Vector Double)] -> [Vector (Complex Double)]
+    applytoComplex = map toComplex
+    applyTuplify2 :: [Vector Double] -> [(Vector Double, Vector Double)]
+    applyTuplify2 = map (tuplify2 (constant 0 nfft))
+    applyWindow :: [Vector Double] -> [Vector Double]
+    applyWindow = map (windowed (hanning nfft))
 
 zeros :: Int -> Vector Double
 zeros nzero = constant 0 nzero
 
-tuplify2 :: a -> a -> (a, a)
+tuplify2 :: Vector Double -> Vector Double -> (Vector Double, Vector Double)
 tuplify2 x y = (y, x)
 
 windowed :: Vector Double -> Vector Double -> Vector Double
