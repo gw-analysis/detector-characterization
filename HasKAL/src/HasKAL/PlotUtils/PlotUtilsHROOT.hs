ed8b80ad29d98f6af64fc146e5917a867b03e253
yuzuri <yuzurihara@yukimura.hep.osaka-cu.ac.jp>
2014-04-12 17:19:36 +0900

    HOOR plot module fixed

--- HasKAL/src/HasKAL/PlotUtils/PlotUtilsHROOT.hs
+++ HasKAL/src/HasKAL/PlotUtils/PlotUtilsHROOT.hs
@@ -1,66 +1,119 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+--module PlotUtilsHROOT(
+module HasKAL.PlotUtils.PlotUtilsHROOT(
+       PlotTypeOption(Line, Point, LinePoint, PointLine, Dot),
+       LogOption(Linear, LogX, LogY, LogXY),
+       hroot_core,
+       plot,
+       plot_st,
+       plot_sf
+       ) where
 
-module PlotUtilsHROOT
-       ( plot_spectrum, plot_volt ) where
+import HROOT
 
-import HasKAL.FrameUtils.FrameUtils
-
-{- For fft -}
-import Numeric.GSL.Fourier
-import Numeric.LinearAlgebra
-
-import HROOT hiding (eval)
+data PlotTypeOption = Line | Point | LinePoint | PointLine | Dot deriving Eq
+data LogOption = Linear | LogX | LogY | LogXY deriving Eq
 
 
+hroot_core::[Double] -> [Double] -> String -> String -> LogOption -> PlotTypeOption ->IO()
+hroot_core xdata ydata xLabel yLabel flagLog flagPlotLine= do
 
-plot_spectrum::String -> IO()
-plot_spectrum dataName = do
-      	       fdata <- readFrame "Channel_Name" dataName
-	       let sampleRate = 1000::Double
-	       let dat = subVector 1 15000 (fromList $ map realToFrac (eval fdata))
-	           fft_val = fft $ dat
-		   power =  map abs $ toList $ fst . fromComplex $ fft_val * conj fft_val
-                   len_power = length power
-                   scale_psd = 1/((fromIntegral len_power) * sampleRate)
-		   len_power2 = floor $ fromIntegral(len_power)/2
-		   powerspectrum = take len_power2  $ map (*scale_psd) power
-
---	       print dat
-
-	       let sampleRate2 = sampleRate/2
-	           lstx = map (sampleRate2 *) $ toList (linspace len_power2 (0, 1::Double))
-		   lsty = powerspectrum
-		   n = length lsty
-
-	       tapp <- newTApplication "1" [0] ["2"]
-	       tcanvas <- newTCanvas "Graph1" "Graph2" 640 480
-	       g1 <- newTGraph n lstx lsty
-
-	       draw g1 "AC"
-	       run tapp 1
-	       delete g1
-	       delete tapp
+	   tapp <- newTApplication "test" [0] ["test"]
+           tcanvas <- newTCanvas  "Test" "Plot" 640 480
+	   g1 <- newTGraph (length xdata) xdata ydata
 
 
 
+                
+	   draw g1 (checkPlotLine flagPlotLine)
+--	   draw g1 "AL*"
 
-plot_volt::String -> IO()
-plot_volt dataName = do
-               fdata <- readFrame "Channel_Name" dataName
+           -- let y2 = [4, 3, 2, 1, 6]
+	   -- g2 <- newTGraph (length xdata) xdata y2
+	   -- draw g2 "L"
 
-	       let dat = map realToFrac (eval fdata)
---	       print dat
+           -- 重ね書きはmapを使って実装予定
 
-	       let lsty = take 3000 dat
-	       	   n = length lsty
-		   lstx = [1..3000]
---	       print n
+	   run tapp 1
 
-               tapp <- newTApplication "1" [0] ["2"]
-               tcanvas <- newTCanvas "Graph1" "Graph2" 640 480
-               g1 <- newTGraph n lstx lsty
+	   delete g1
+	   delete tapp
 
-               draw g1 "AC"
-               run tapp 1
-               delete g1
-               delete tapp
+
+
+	   -- Label logスケールのためのフラグは今は捨てているが
+	   -- HROOT-0.8を使って実装予定
+	   -- label付け、logスケールへの変更の関数をきちんと把握できていないため
+	   -- 下に書いた関数はまだ正しくない
+	   {--
+	   setXLabel 
+		| xLabel == "" = " "
+		| otherwise = xlabel
+
+           setXLabel
+		| xLabel == "" = " "
+		  | otherwise = xlabel
+
+           setLogX
+                | flagLogX == 1 = 1
+                | otherwise = 0
+
+           setLogX
+                | flagLogX == 1 = 1
+                | otherwise = 0
+	   --}
+
+
+
+-- 細かいことはいいからplotしたい人向け
+plot::[Double] -> [Double] ->IO()
+plot xdata ydata = do
+
+	   tapp <- newTApplication "test" [0] ["test"]
+           tcanvas <- newTCanvas  "Test" "Plot" 640 480
+	   g1 <- newTGraph (length xdata) xdata ydata
+	   draw g1 "AL*"
+           run tapp 1
+	   delete g1
+	   delete tapp
+
+-- 時系列データをplotする
+-- 時系列なので、logスケールの引数はなし
+plot_st::[Double] -> [Double] -> String -> String -> PlotTypeOption ->IO()
+plot_st xdata ydata xLabel yLabel flagPlotLine = do
+
+	   tapp <- newTApplication "test" [0] ["test"]
+           tcanvas <- newTCanvas  "Test" "Plot" 640 480
+	   g1 <- newTGraph (length xdata) xdata ydata
+	   draw g1 (checkPlotLine flagPlotLine)
+           run tapp 1
+	   delete g1
+	   delete tapp
+-- log-logスケールでスペクトルをplotする
+plot_sf::[Double] -> [Double] -> String -> String -> LogOption -> PlotTypeOption ->IO()
+plot_sf xdata ydata xLabel yLabel flagLog flagPlotLine= do
+
+	   tapp <- newTApplication "test" [0] ["test"]
+           tcanvas <- newTCanvas  "Test" "Plot" 640 480
+	   g1 <- newTGraph (length xdata) xdata ydata
+
+           -- ここでlogにする HROOT-0.8を使って実装予定
+
+	   draw g1 (checkPlotLine flagPlotLine)
+           run tapp 1
+	   delete g1
+	   delete tapp
+
+
+
+
+
+checkPlotLine :: PlotTypeOption -> String
+checkPlotLine flagPlotLine
+        | flagPlotLine == Line      = "AL"
+        | flagPlotLine == Point     = "AP*"
+        | flagPlotLine == LinePoint = "AL*"
+        | flagPlotLine == PointLine = "AL*"
+        | flagPlotLine == Dot       = "AP"
+        | otherwise                 = "AL"
+
+
