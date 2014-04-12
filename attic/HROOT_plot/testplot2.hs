7c910ba52be36f182347e426a00fc546a19a52b8
yuzuri <yuzurihara@yukimura.hep.osaka-cu.ac.jp>
2014-04-12 17:33:05 +0900

    upload sample source

---
+++ attic/HROOT_plot/testplot2.hs
@@ -0,0 +1,29 @@
+import HasKAL.FrameUtils.FrameUtils
+import HasKAL.SpectrumUtils.SpectrumUtils
+import HasKAL.PlotUtils.PlotUtilsHROOT
+--import FrameUtils
+
+{- For fft -}
+import Numeric.GSL.Fourier
+import Numeric.LinearAlgebra
+
+import HROOT hiding (eval)
+
+
+
+main = do
+
+     fdata <- readFrame "Channel_Name" "test-1066392016-300.gwf"
+--     let dat = subVector 1 15000 (fromList $ map realToFrac (eval fdata))
+     let dat   = map realToFrac (eval fdata)
+         lstxt = take (length dat) [1,2..]
+
+     let lsty  = gwpsd dat (length dat) 1000
+         lstxf = take (length lsty) [1,2..]
+
+--     plot_sf lstx lsty "" "" LogXY Line
+     plot_st lstxt dat "" "" Line
+
+
+
+
