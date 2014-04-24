367fe44f6d557d0ec0e9dd8bae11621b875757cc
enoshima <kazuhiro.hayama@gmail.com>
2014-04-25 06:36:33 +0900

    mapM-dash updated to solve the issues on stack overflow

--- attic/strictmapping/StrictMapping.hs
+++ attic/strictmapping/StrictMapping.hs
@@ -1,15 +1,16 @@
 
 
+
 module StrictMapping
 (mapM',
 forM'
 ) where
 
 mapM' :: Monad m => (a -> m b) -> [a] -> m [b]
-mapM' _ []     = return []
-mapM' f (x:xs) = do y  <- f x
-                    ys <- y `seq` mapM' f xs
-                    return (y:ys)
+mapM' f x = go x []
+  where go []     acc = return $ reverse acc
+        go (b:bs) acc = do y <- f b
+                           go bs (y:acc)
 
 forM' :: Monad m => [a] -> (a -> m b) -> m [b]
 forM' = flip mapM'
