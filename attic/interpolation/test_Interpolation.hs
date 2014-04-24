26112a64fa006eeb4143eb174878053f7bf13538
enoshima <kazuhiro.hayama@gmail.com>
2014-04-24 17:33:11 +0900

    use strict forM in test_Interpolation.hs

--- attic/interpolation/test_Interpolation.hs
+++ attic/interpolation/test_Interpolation.hs
@@ -3,12 +3,14 @@
 import Interpolation
 import InterpolationType
 import Control.Monad
+import StrictMapping
 
 main :: IO ()
 main = do
   let x = map (\a -> a + 0.5 * sin a) [0..9] :: [Double]
       y = map (\a -> a + cos (a * a)) [0..9] :: [Double]
 
-  out <- forM [0, 1..9] $ \interp_x ->
+  out <- forM' [0, 1..9] $ \interp_x ->
     interp x y interp_x Spline
   putStrLn $ show out
+
