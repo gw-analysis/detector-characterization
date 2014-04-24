98625bfe2b73f36de401eab3d81bd828871dd99c
enoshima <kazuhiro.hayama@gmail.com>
2014-04-25 06:55:06 +0900

    minor changes

--- attic/interpolation/test_Interpolation.hs
+++ attic/interpolation/test_Interpolation.hs
@@ -2,7 +2,7 @@
 
 import Interpolation
 import InterpolationType
-import Control.Monad
+--import Control.Monad
 import StrictMapping
 
 main :: IO ()
@@ -10,7 +10,7 @@ main = do
   let x = map (\a -> a + 0.5 * sin a) [0..9] :: [Double]
       y = map (\a -> a + cos (a * a)) [0..9] :: [Double]
 
-  out <- forM' [0, 1..9] $ \interp_x ->
+  out <- forM' [0,1..9] $ \interp_x ->
     interp x y interp_x Spline
-  putStrLn $ show out
+  print out
 
