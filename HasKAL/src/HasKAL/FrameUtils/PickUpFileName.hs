477a4a5cae0eba42176ccb2267e8a55b234f3459
asano <asano@yukimura.hep.osaka-cu.ac.jp>
2014-05-20 15:20:30 +0900

    modified PickUpFileName.hs (GPS Type : Integer)

--- HasKAL/src/HasKAL/FrameUtils/PickUpFileName.hs
+++ HasKAL/src/HasKAL/FrameUtils/PickUpFileName.hs
@@ -1,8 +1,11 @@
-{-
-File name : PickUpFileName.hs
-Author : Mitsuhiro Asano
+{-****************************************************
+ *PickUpFileName.hs
+ *Created:2014/05/20
+ *Author:Mitsuhiro Asano
+ ****************************************************
+Last Modified: 2014/05/20 15:16:13
 -}
-
+ 
 module PickUpFileName (pickUpFileName) where
 
 import Data.Char
@@ -18,26 +21,26 @@ main = do
      contents <- hGetContents handle
      let contentsList = lines contents
 
-     mapM putStrLn $ pickUpFileName "993597840" "993598000" contentsList
+     mapM putStrLn $ pickUpFileName 993597840 993598000 contentsList
 -}
 
-pickUpFileName :: String -> String -> [String] -> [String]
+pickUpFileName :: Integer -> Integer -> [String] -> [String]
 pickUpFileName gpsTimeStart gpsTimeFinish contentsList = do
      let extractstartGPStime x = read $ (!!2) $ splitOn "-" $ last $ splitOn "/" x :: Integer 
        	 contentsVec = V.fromList contentsList
        	 gpstimeList :: [Integer]
        	 gpstimeList = map extractstartGPStime contentsList 
        	 time2index :: M.Map Integer Int
-       	 time2index = M.fromList $ zip gpstimeList [1..]    	  
-	 startIndex = case M.lookupLE (read gpsTimeStart) time2index of
-     	  	      	   Nothing -> -1
-			   Just (_,i) -> i
-	 finishIndex = case M.lookupLE (read gpsTimeFinish) time2index of
-      	     	       	    Nothing -> -1
-			    Just (_,j) -> j
+       	 time2index = M.fromList $ zip gpstimeList [1..]		  
+	 startIndex = case M.lookupLE gpsTimeStart time2index of
+     	                   Nothing -> -1
+		           Just (_,i) -> i
+	 finishIndex = case M.lookupLE gpsTimeFinish time2index of
+                            Nothing -> -1
+	     	     	    Just (_,j) -> j
 
      case (startIndex,finishIndex) of (-1,_) -> ["Nothing"]
-	     		    	      (_,-1) -> ["Nothing"]
+          			      (_,-1) -> ["Nothing"]
 				      (_,_)  -> drop (startIndex -1) $ take (finishIndex) contentsList
---				      (_,_)  -> take (finishIndex - startIndex +1) $ drop (startIndex -1) contentsList
+--          	     	              (_,_)  -> take (finishIndex - startIndex +1) $ drop (startIndex -1) contentsList
 
