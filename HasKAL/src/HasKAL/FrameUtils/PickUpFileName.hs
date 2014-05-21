eaf30ae0c32daa3670402d6c21f194dfc336d206
asano <asano@yukimura.hep.osaka-cu.ac.jp>
2014-05-21 15:24:00 +0900

    modified PickUpFileName.hs

--- HasKAL/src/HasKAL/FrameUtils/PickUpFileName.hs
+++ HasKAL/src/HasKAL/FrameUtils/PickUpFileName.hs
@@ -3,11 +3,12 @@
  *Created:2014/05/20
  *Author:Mitsuhiro Asano
  ****************************************************
-Last Modified: 2014/05/21 13:22:08
+Last Modified: 2014/05/21 15:23:31
 -}
 
-module PickUpFileName (
-pickUpFileName
+module HasKAL.FrameUtils.PickUpFileName (
+quicksort
+,pickUpFileName
 ,pickUpFileNameinFile
 ,pickUpFileNameinoutFile
 ) where
@@ -15,6 +16,7 @@ pickUpFileName
 
 import Data.Char
 import System.IO
+import System.IO.Unsafe
 import qualified Data.Vector as V
 import Data.Vector ((!))
 import qualified Data.Map as M 
@@ -30,8 +32,7 @@ main = do
      mapM putStrLn $ pickUpFileName 993597840 993598000 contentsList
 
 --pickUpFileNameinFile
-     hoge <- P.pickUpFileNameinFile 993597840 993598000 "GEOframecache.dat"
-     putStrLn hoge 
+     mapM putStrLn $ P.pickUpFileNameinFile 993597840 993598000 "GEOframecache.dat"
 
 --pickUpFileNameinoutFile
      P.pickUpFileNameinoutFile 993597840 993598000 "GEOframecache.dat" 
@@ -39,13 +40,20 @@ main = do
 --   KAGRA's FileName is "K-K1_C-1083981344-32.gwf" (one example)
 -}
 
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+     let smallOrEqual = [a | a <- xs, a <= x]
+     	 larger = [a | a <- xs, a > x]
+     in quicksort smallOrEqual ++ [x] ++ quicksort larger
+
 
 pickUpFileName :: Integer -> Integer -> [String] -> [String]
 pickUpFileName gpsTimeStart gpsTimeFinish contentsList = do
      let extractstartGPStime x = read $ (!!2) $ splitOn "-" $ last $ splitOn "/" x :: Integer 
-       	 contentsVec = V.fromList contentsList
+       	 contentsVec = V.fromList contentsList --(quicksort contentsList)
        	 gpstimeList :: [Integer]
-       	 gpstimeList = map extractstartGPStime contentsList 
+       	 gpstimeList = map extractstartGPStime contentsList --(quicksort contentsList) 
        	 time2index :: M.Map Integer Int
        	 time2index = M.fromList $ zip gpstimeList [1..]		  
 	 startIndex = case M.lookupLE gpsTimeStart time2index of
@@ -57,10 +65,31 @@ pickUpFileName gpsTimeStart gpsTimeFinish contentsList = do
 
      case (startIndex,finishIndex) of (-1,_) -> ["Nothing"]
           			      (_,-1) -> ["Nothing"]
-				      (_,_)  -> drop (startIndex -1) $ take (finishIndex) contentsList
---          	     	              (_,_)  -> take (finishIndex - startIndex +1) $ drop (startIndex -1) contentsList
+				      (_,_)  -> drop (startIndex -1) $ take (finishIndex) contentsList --(quicksort contentsList)
 
 
+pickUpFileNameinFile :: Integer -> Integer -> String -> [String]
+pickUpFileNameinFile gpsTimeStart gpsTimeFinish fileName = pickUpFileName gpsTimeStart gpsTimeFinish contentsList
+     where contentsList = lines $ unsafePerformIO $ readFile fileName
+
+
+pickUpFileNameinoutFile :: Integer -> Integer -> String -> IO ()
+pickUpFileNameinoutFile gpsTimeStart gpsTimeFinish fileName = writeFile "tmpCachedFrameFile.lst" contentsList
+     where contentsList = unlines $ pickUpFileNameinFile gpsTimeStart gpsTimeFinish fileName
+
+
+
+
+
+
+
+
+
+
+
+------------ MEMO ------------
+
+{-
 pickUpFileNameinFile :: Integer -> Integer -> String -> IO String
 pickUpFileNameinFile gpsTimeStart gpsTimeFinish fileName = do
      handle <- openFile fileName ReadMode	
@@ -111,3 +140,7 @@ pickUpFileNameinoutFile gpsTimeStart gpsTimeFinish fileName = do
 
      withFile "tmpCachedFrameFile.lst" WriteMode $ \handle -> do
      hPutStrLn handle asano
+
+-}
+
+
