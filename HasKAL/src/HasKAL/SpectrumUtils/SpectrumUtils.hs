92526af6f6c0d61eb828dbc6e412f11c478ae93e
enoshima <kazuhiro.hayama@gmail.com>
2014-06-17 02:08:50 +0900

    add MedianAverage spectrum density

--- HasKAL/src/HasKAL/SpectrumUtils/SpectrumUtils.hs
+++ HasKAL/src/HasKAL/SpectrumUtils/SpectrumUtils.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -XBangPatterns #-}
 
 module HasKAL.SpectrumUtils.SpectrumUtils (
   gwpsd
@@ -15,20 +16,24 @@ import HasKAL.SpectrumUtils.GwPsdMethod
 import HasKAL.SignalProcessingUtils.WindowType
 import HasKAL.SignalProcessingUtils.WindowFunction
 
+--import HasKAL.SpectrumUtils.AuxFunction(sort, median)
+import Data.List (sort, foldl')
+
 gwpsd :: [Double]-> Int -> Double -> [(Double, Double)]
 gwpsd dat nfft fs = gwpsdCore Welch dat nfft fs
 
 gwpsdCore :: PSDMETHOD -> [Double] -> Int -> Double -> [(Double, Double)]
 gwpsdCore method dat nfft fs
-  | method==Welch = gwpsdWelch dat nfft fs
+  | method==Welch = gwpsdWelch dat nfft fs Hann
+  | method==MedianAverage = gwpsdMedianAverageCore dat nfft fs Hann
   | otherwise =  error "No such method implemented. Check GwPsdMethod.hs"
 
-gwpsdWelch :: [Double]-> Int -> Double -> [(Double, Double)]
-gwpsdWelch dat nfft fs = do
+gwpsdWelch :: [Double]-> Int -> Double -> WindowType -> [(Double, Double)]
+gwpsdWelch dat nfft fs w = do
   let ndat = length dat
       maxitr = floor $ fromIntegral (ndat) / fromIntegral (nfft) :: Int
       datlist = takesV (take maxitr (repeat nfft)) $ fromList dat
-      fft_val = applyFFT . applytoComplex . applyTuplify2 . applyWindow Hann $ datlist
+      fft_val = applyFFT . applytoComplex . applyTuplify2 . applyWindow w $ datlist
       power =  map (abs . fst . fromComplex) $ zipWith (*) fft_val (map conj fft_val)
       meanpower = scale (1/(fromIntegral maxitr)) $ foldr (+) (zeros nfft) power
       scale_psd = 1/(fromIntegral nfft * fs)
@@ -45,11 +50,85 @@ gwpsdWelch dat nfft fs = do
       | windowtype==Hann = map (windowed (hanning nfft))
       | otherwise = error "No such window implemented. Check WindowType.hs"
 
+gwpsdMedianAverageCore :: [Double] -> Int -> Double -> WindowType -> [(Double, Double)]
+gwpsdMedianAverageCore dat nfft fs w = do
+  let --ns = 2*(length dat) `div` nfft - 1 :: Int
+      -- number of sample for overwrapping
+      --delta = div (length dat) 2 :: Int
+      -- calculate median of power spectrum at each frequency bin
+      distatFreqOdd = toColumns . fromRows $ (psdOdd dat nfft w) :: [Vector Double]
+      nsodd = length distatFreqOdd
+      medianListOdd' = map median $ map (sort . toList) distatFreqOdd
+      medianListOdd = map (/medianBiasFactor nsodd) medianListOdd'
+
+      distatFreqEven= toColumns . fromRows $ (psdEven dat nfft w) :: [Vector Double]
+      nseven = length distatFreqEven
+      medianListEven' =  map median $ map (sort . toList) distatFreqEven
+      medianListEven= map (/medianBiasFactor nseven) medianListEven'
+
+      medianList = map (/fromIntegral (nsodd+nseven))
+        $ zipWith (+) (map (*(fromIntegral nsodd)) medianListOdd) (map (*(fromIntegral nseven)) medianListEven)
+
+      -- set corresponding frequency
+      fvec = toList $ linspace nfft (0, fs) :: [Double]
+  zip fvec medianList
+
+
+--psdOdd:: [Double] -> Int -> Double -> WindowType -> [Vector (Double, Double)]
+psdOdd:: [Double] -> Int -> WindowType -> [Vector Double]
+psdOdd dat nfft w = do
+  let ndat = length dat :: Int
+      maxitr = floor $ fromIntegral (ndat) / fromIntegral (nfft) :: Int
+      datlist = takesV (take maxitr (repeat nfft)) $ fromList dat :: [Vector Double]
+--      power = forM datlist $ \x -> calcPower x fs w
+--  forM power $ \x -> zipVector (linspace nfft (0, fs)) x
+  map (\x -> calcPower x w) datlist
+
+--psdEven:: [Double] -> Int -> Double -> WindowType -> [Vector (Double, Double)]
+psdEven:: [Double] -> Int -> WindowType -> [Vector Double]
+psdEven dat' nfft w = do
+  let dat = drop (div nfft 2) dat'
+  psdOdd dat nfft w
+
+
+
+
+{- helper functions -}
+calcPower :: Vector Double -> WindowType -> Vector Double
+calcPower dat w = abs . fst . fromComplex $ fftVal * conj fftVal
+  where
+    fftVal = fft . applyRealtoComplex . applyWindowFunction w $ dat :: Vector (Complex Double)
+
+applyRealtoComplex :: Vector Double -> Vector (Complex Double)
+applyRealtoComplex x = toComplex $ tuplify2 (constant 0 nfft) x
+  where nfft = dim x
+
+applyWindowFunction :: WindowType -> Vector Double -> Vector Double
+applyWindowFunction windowtype x
+  | windowtype==Hann = windowed (hanning nfft) x
+  | otherwise = error "No such window implemented. Check WindowType.hs"
+  where nfft = dim x
+
 zeros :: Int -> Vector Double
 zeros nzero = constant 0 nzero
 
 tuplify2 :: Vector Double -> Vector Double -> (Vector Double, Vector Double)
 tuplify2 x y = (y, x)
+
+
+mean :: Floating a => [a] -> a
+mean x = fst $ foldl' (\(!m, !n) x -> (m+(x-m)/(n+1),n+1)) (0,0) x
+
+median :: (Floating a, Ord a) => [a] -> a
+median x | odd n  = head  $ drop (n `div` 2) x'
+         | even n = mean $ take 2 $ drop i x'
+                  where i = (length x' `div` 2) - 1
+                        x' = sort x
+                        n  = length x
+
+medianBiasFactor :: Int -> Double
+medianBiasFactor n = foldl' (\acc x -> acc + (-1)**(fromIntegral x+1)/fromIntegral x) 0 [1..n]
+
 
 
 
