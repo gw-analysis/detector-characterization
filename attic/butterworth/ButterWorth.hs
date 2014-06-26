a68ac8305961f87e3cfbc8ec2be117c88347b376
enoshima <kazuhiro.hayama@gmail.com>
2014-06-26 12:35:05 +0900

    ButterWorth updating

--- attic/butterworth/ButterWorth.hs
+++ attic/butterworth/ButterWorth.hs
@@ -1,26 +1,39 @@
 module ButterWorth
-( butterPoly
+(
 --,
 )
 where
 
 
 import FilterType
+import Data.Complex
 
 --butter :: Int -> Double -> FilterType -> Double
 --butter n freq filterType
 --  | filterType==Low = butterCore n a
 
+butterTransferFunction :: Int -> Complex Double -> Complex Double
+butterTransferFunction n' s = foldl (\acc k' -> 1/(s - pole n' k')*acc) 0 [1..n']
 
 
+pole :: Int -> Int -> Complex Double
+pole n' k' = realToFrac (cos (pi*(2*k+n-1)/(2*n))) + j * realToFrac (sin (pi*(2*k+n-1)/(2*n)))
+  where n = fromIntegral n' :: Double
+        k = fromIntegral k' :: Double
 
-{- Normalized ButterWorth polynomials -}
-butterPoly :: Int -> Double -> Double
-butterPoly n s
-    | even n = foldl (\acc k->(s**2-2*s*cos((2*fromIntegral k+fromIntegral n-1)*pi/(2*fromIntegral n))+1)*acc) 0 [1..(n `div` 2)]
-    | odd n  = (s+1) * (foldl (\acc k->(s**2-2*s*cos((2*fromIntegral k+fromIntegral n-1)*pi/(2*fromIntegral n))+1)*acc) 0 [1..((n-1) `div` 2)])
 
-butterCore :: Int -> Double -> Double
-butterCore n s = gain0 / butterPoly n s
-  where gain0 = 1.0 :: Double
+j :: RealFloat a => Complex a
+j = 0 :+ 1.0
+
+
+getzp :: Int -> Int -> Double -> Complex Double
+getzp n k freq = (2 + realToFrac dT * pole n k) / (2 - realToFrac dT * pole n k)
+  where dT = 1/freq
+
+gamma :: Double -> Int -> Int -> Complex Double
+gamma freq n k = realToFrac (2.0*freq) - pole n k
+
+delta :: Double -> Int -> Int -> Complex Double
+delta freq n k = realToFrac ((-2.0)*freq) - pole n k
+
 
