a1be346db9d3789b7e528b8d828c27ca5a9ab560
yuzuri <yuzurihara@yukimura.hep.osaka-cu.ac.jp>
2014-07-14 07:36:20 +0900

    add shell script to generate list of channels

--- attic/CorrelationAnalysis/setupChannelList.sh
+++ attic/CorrelationAnalysis/setupChannelList.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 # usage setupChannelList.sh  filename  samplingrate
 
-#/usr/bin/FrChannels $1 | grep $2 | cut -d" " -f1 > channelList.txt
-/usr/bin/FrChannels $1 | grep $2 > channelList.txt
+if [ $# -eq 2 ]; then
+
+    #/usr/bin/FrChannels $1 | grep $2 | cut -d" " -f1 > channelList.txt
+    /usr/bin/FrChannels $1 | grep $2 > channelList.txt
+
+else
+    
+    /usr/bin/FrChannels $1 > channelList.txt
+fi
