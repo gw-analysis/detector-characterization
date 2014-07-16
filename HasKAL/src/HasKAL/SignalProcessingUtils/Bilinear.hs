81a40dc38035f134058ddc140bfc7269bea400bb
enoshima <kazuhiro.hayama@gmail.com>
2014-07-17 01:12:10 +0900

    change output types of butterworth and chebyshev filters to ([Double],[Double])

--- HasKAL/src/HasKAL/SignalProcessingUtils/Bilinear.hs
+++ HasKAL/src/HasKAL/SignalProcessingUtils/Bilinear.hs
@@ -7,23 +7,21 @@ where
 import HasKAL.SignalProcessingUtils.Signatures
 import Data.Complex(Complex)
 
-denominatorCore :: GammaFunc -> DeltaFunc -> Double -> Double -> Int -> Int -> Complex Double
-denominatorCore gamma delta fs fc 1 0 = gamma fs fc 1 1
-denominatorCore gamma delta fs fc 1 1 = delta fs fc 1 1
-denominatorCore gamma delta fs fc n m
-  | m == 0 = (delta fs fc (n-1) 0) * (denominatorCore gamma delta fs fc (n-1) 0)
-  | m == n  = (gamma fs fc n n) * (denominatorCore gamma delta fs fc n (n-1))
-  | m < n = (gamma fs fc n m) * (denominatorCore gamma delta fs fc (n-1) m)
-    + (delta fs fc n m) * (denominatorCore gamma delta fs fc (n-1) (m-1))
-  | m > n  = 1
+denominatorCore :: GammaFunc -> DeltaFunc -> Int -> Double -> Double -> Int -> Int -> Complex Double
+denominatorCore gamma _ n fs fc 1 0 = gamma fs fc n 0
+denominatorCore _ delta n fs fc 1 1 = delta fs fc n 1
+denominatorCore gamma delta n fs fc m k
+  | k == 0 = gamma fs fc n n * denominatorCore gamma delta n fs fc (m-1) 0
+  | k == m = delta fs fc n n * denominatorCore gamma delta n fs fc (m-1) (k-1)
+  | k < m = gamma fs fc n n * denominatorCore gamma delta n fs fc (m-1) k
+    + delta fs fc n n * denominatorCore gamma delta n fs fc (m-1) (k-1)
 
-numeratorCore :: GammaFunc -> DeltaFunc -> Double -> Double -> Int -> Int -> Complex Double
-numeratorCore alpha beta fs fc 1 0 = alpha fs fc 1 1
-numeratorCore alpha beta fs fc 1 1 = beta fs fc 1 1
-numeratorCore alpha beta fs fc n m
-  | m == 0 = (alpha fs fc (n-1) 0) * (numeratorCore alpha beta fs fc (n-1) 0)
-  | m == n  = (alpha fs fc n n) * (numeratorCore alpha beta fs fc n (n-1))
-  | m < n = (alpha fs fc n m) * (numeratorCore alpha beta fs fc (n-1) m)
-    + (beta fs fc n m) * (numeratorCore alpha beta fs fc (n-1) (m-1))
-  | m > n = 1
+numeratorCore :: GammaFunc -> DeltaFunc -> Int -> Double -> Double -> Int -> Int -> Complex Double
+numeratorCore _ _ _ _ _ 1 0 = 1
+numeratorCore _ _ _ _ _ 1 1 = -1
+numeratorCore alpha beta n fs fc m k
+  | k == 0 = alpha fs fc n n * numeratorCore alpha beta n fs fc (m-1) 0
+  | k == m  = beta fs fc n n * numeratorCore alpha beta n fs fc (m-1) (k-1)
+  | k < m = alpha fs fc n n * numeratorCore alpha beta n fs fc (m-1) k
+    + beta fs fc n n * numeratorCore alpha beta n fs fc (m-1) (k-1)
 
