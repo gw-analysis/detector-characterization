1800a7fb9057489c5d06fd8360c2211e4ace1d38
enoshima <kazuhiro.hayama@gmail.com>
2014-07-18 00:30:52 +0900

    refactoring Levinson.hs, wrapper function of Numerical Recipes

--- attic/LinearAlgebra/Levinson.hs
+++ attic/LinearAlgebra/Levinson.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
 
 module Levinson
 ( levinson
@@ -14,13 +13,11 @@ levinson r p = do
   let rv = p |> r :: Vector Double
       partialrv = subVector 1 p rv
       autCorr = toList $ join [reverseVCD partialrv, rv]
-      autCorrFloat = map realToFrac (0:autCorr) :: [Float]
-      rvFloat = map realToFrac (0:toList rv) :: [Float]
+      autCorrFloat = map realToFrac autCorr :: [Float]
+      rvFloat = map realToFrac (toList rv) :: [Float]
       outDouble = nr_toeplz autCorrFloat rvFloat p
   map realToFrac outDouble
 
---applyConj :: Vector (Complex Double ) -> Vector (Complex)
---applyConj = mapVector conj
 
 reverseVCD :: Vector Double -> Vector Double
 reverseVCD = fromList . reverse . toList
--- attic/LinearAlgebra/NumericalRecipes/Functions.hs
+++ attic/LinearAlgebra/NumericalRecipes/Functions.hs
@@ -7,26 +7,22 @@ module NumericalRecipes.Functions
 import Foreign.Ptr(Ptr)
 import Foreign.C.Types(CFloat)
 import System.IO.Unsafe(unsafePerformIO)
-import Foreign.Marshal.Array (withArrayLen, peekArray)
+import Foreign.Marshal.Array (withArray, peekArray, allocaArray)
 import StrictMapping.StrictMapping
 import Control.Monad()  --((>>=))
 
 nr_toeplz :: [Float] -> [Float] -> Int -> [Float]
 nr_toeplz r y p = unsafePerformIO $ do
-  let x = replicate p (0::CFloat)
-      r'= map realToFrac r :: [CFloat]
-      y'= map realToFrac y :: [CFloat]
+  let r'= 0:map realToFrac r :: [CFloat]
+      y'= 0:map realToFrac y :: [CFloat]
 
-  ptr_r <- withArrayLen r' $ \len ptr_tmp -> do
-    return ptr_tmp
-  ptr_y <- withArrayLen y' $ \len ptr_tmp ->
-    return ptr_tmp
-  ptr_x <- withArrayLen x $ \len ptr_tmp -> do
-    return ptr_tmp
-  c_nr_toeplz ptr_r ptr_y ptr_x p
-  tmpretval <- peekArray p ptr_x >>= \list_x ->
-    forM' list_x $ \x ->return (realToFrac x :: Float)
-  return (1:(tail tmpretval))
+  withArray r' $ \ptrr ->
+    withArray y' $ \ptry ->
+    allocaArray (p+1) $ \ptrx ->
+    do c_nr_toeplz ptrr ptry ptrx p
+       tmpretval <- peekArray (p+1) ptrx >>= \list_x ->
+         forM' list_x $ \x ->return (realToFrac x :: Float)
+       return $ tail tmpretval
 
 
 foreign import ccall unsafe "nr.h toeplz" c_nr_toeplz :: Ptr CFloat -> Ptr CFloat ->  Ptr CFloat -> Int -> IO()
