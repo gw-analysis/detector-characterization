be4ee8f6d4f92f0c730a28a29e052abcb1bc7d17
enoshima <kazuhiro.hayama@gmail.com>
2014-07-18 13:23:24 +0900

    added Levinson-Durbin algorithm

---
+++ HasKAL/src/HasKAL/SignalProcessingUtils/Levinson.hs
@@ -0,0 +1,28 @@
+
+module HasKAL.SignalProcessingUtils.Levinson
+( levinson
+--,
+) where
+
+import Numeric.LinearAlgebra
+import HasKAL.ExternalUtils.NumericalRecipes.Functions
+
+levinson :: [Double] -> Int -> [Double]
+levinson r p = do
+  let rv = p |> r :: Vector Double
+      partialrv = subVector 1 p rv
+      autCorr = toList $ join [reverseVCD partialrv, rv]
+      autCorrFloat = map realToFrac autCorr :: [Float]
+      rvFloat = map realToFrac (toList rv) :: [Float]
+      outDouble = nr_toeplz autCorrFloat rvFloat p
+  map realToFrac outDouble
+
+
+reverseVCD :: Vector Double -> Vector Double
+reverseVCD = fromList . reverse . toList
+
+
+
+
+
+
