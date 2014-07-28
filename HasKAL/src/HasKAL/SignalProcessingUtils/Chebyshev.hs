4751b101e600af8868734bf74e5bff0d3aba80f8
enoshima <kazuhiro.hayama@gmail.com>
2014-07-29 01:14:55 +0900

    add code to calculate pole, some fiexes

--- HasKAL/src/HasKAL/SignalProcessingUtils/Chebyshev.hs
+++ HasKAL/src/HasKAL/SignalProcessingUtils/Chebyshev.hs
@@ -1,5 +1,6 @@
 module HasKAL.SignalProcessingUtils.Chebyshev
 ( chebyshev1
+, filterPole
 )
 where
 
@@ -12,7 +13,7 @@ chebyshev1:: Int -> Double -> Double -> Double -> FilterType -> ([Double], [Doub
 chebyshev1 n r fs fc filterType = do
   let denominatorList = map (denominator fs fc n) [0..n]
       numeratorList = map ((* (h1 n r*h0 r fs fc n)) . numerator fs fc n) [0..n]
-  (map realPart denominatorList, map realPart numeratorList)
+  (map realPart numeratorList, map realPart denominatorList )
   where
     denominator :: Double -> Double -> Int -> Int -> Complex Double
     denominator = bilinear gamma delta n
@@ -38,8 +39,8 @@ deltaCore :: Double -> Double -> Double -> Int -> Int -> FilterType -> Complex D
 deltaCore r fs fc n m filt
   | filt==Low = (-2.0 - filterPole n m r * realToFrac (2.0*pi*fc/fs))
     / (2.0 - filterPole n m r * realToFrac (2.0*pi*fc/fs))
-  | filt==High = realToFrac (2.0*pi*fc/fs) - 2.0*filterPole n m r
-    / realToFrac (2.0*pi*fc/fs) + 2.0*filterPole n m r
+  | filt==High = (realToFrac (2.0*pi*fc/fs) + 2.0*filterPole n m r)
+    / (realToFrac (2.0*pi*fc/fs) - 2.0*filterPole n m r)
 
 gammaCore :: Double -> Double -> Double -> Int -> Int -> FilterType -> Complex Double
 gammaCore _ _ _ _ _ filt
@@ -58,23 +59,23 @@ betaCore _ _ _ _ _ filt
 
 h1 :: Int -> Double -> Complex Double
 h1 n r
-  | odd n  = foldl (\acc k -> acc * filterPole n k r) 1.0 [1..n]
-  | even n = realToFrac (10.0**(r/20.0)) * (foldl (\acc k -> acc * filterPole n k r) 1.0 [1..n])
+  | odd n  = foldl (\acc k -> acc * (-1)*filterPole n k r) 1.0 [1..n]
+  | even n = realToFrac (10.0**(r/20.0)) * foldl (\acc k -> acc * (-1)*filterPole n k r) 1.0 [1..n]
 
 h0Core :: Double -> Double -> Double -> Int -> FilterType -> Complex Double
 h0Core r fs fc n filt
   | filt==Low = foldl (\acc m' -> acc * (realToFrac (2*pi*fc/fs)
     / (2.0 - filterPole n m' r * realToFrac (2.0*pi*fc/fs)))) 1.0 [1..n]
-  | filt==High = 1 / foldl (\acc m' -> acc * (1.0/(realToFrac (2.0*pi*fc/fs) + 2.0*filterPole n m' r))) 1.0 [1..n]
+  | filt==High = foldl (\acc m' -> acc * 2.0/(realToFrac (2.0*pi*fc/fs) - 2.0*filterPole n m' r)) 1.0 [1..n]
 
 
 filterPole :: Int -> Int -> Double -> Complex Double
 filterPole n' k' r = realToFrac ((((1/gam)-gam)/2.0) * sin (pi*(2*k-1)/(2*n)))
-  + jj * realToFrac ((((1/gam)+gam)/2.0) * sin (pi*(2*k-1)/(2*n)))
+  + jj * realToFrac ((((1/gam)+gam)/2.0) * cos (pi*(2*k-1)/(2*n)))
   where n = fromIntegral n' :: Double
         k = fromIntegral k' :: Double
-        gam = ((1.0+(1.0+epcilon**2.0)**0.5)/2.0)**(1.0/n)
-        epcilon = (10.0**(r/10.0)-1.0)**0.5
+        gam = ((1.0+sqrt (1.0+epcilon**2.0))/epcilon)**(1.0/n)
+        epcilon = sqrt (10.0**(r/10.0)-1.0)
 
 jj :: RealFloat a => Complex a
 jj = 0 :+ 1.0
