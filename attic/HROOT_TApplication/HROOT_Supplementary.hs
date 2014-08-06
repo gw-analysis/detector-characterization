65b4c88572efdf583a65e301907d2e70bb21dda1
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2014-08-06 11:09:43 +0900

    move some functions to new module

---
+++ attic/HROOT_TApplication/HROOT_Supplementary.hs
@@ -0,0 +1,47 @@
+{-******************************************
+  *     File Name: HROOT_Supplementary.hs
+  *        Author: Takahiro Yamamoto
+  * Last Modified: 2014/08/06 10:45:15
+  *******************************************-}
+
+module HROOT_Supplementary (
+   setLog'
+  ,newTApp'
+  ,runOrSave'
+) where
+
+import qualified Control.Monad as CM
+import qualified Data.Maybe as DM
+import qualified Foreign.C.String as FCS
+import qualified HROOT as HR
+import qualified System.IO.Unsafe as SIOU
+
+import HasKAL.PlotUtils.PlotOption.PlotOptionHROOT
+import qualified GlobalTApplication as HPG
+
+{--  External Functions  --}
+setLog' :: HR.TCanvas -> LogOption -> IO ()
+setLog' tCan flag
+  | flag == Linear = return ()
+  | flag == LogX   = HR.setLogx tCan 1
+  | flag == LogY   = HR.setLogy tCan 1
+  -- | flag == LogZ   = HR.setLogz tCan 1
+  | flag == LogXY  = mapM_ (setLog' tCan) [LogX, LogY]
+  -- | flag == LogXZ  = mapM_ (setLog' tCan) [LogX, LogZ]
+  -- | flag == LogYZ  = mapM_ (setLog' tCan) [LogY, LogZ]
+  -- | flag == LogXYZ  = mapM_ (setLog' tCan) [LogX, LogY, LogZ]
+
+newTApp' :: String -> IO (Maybe HR.TApplication)
+newTApp' fname = case fname of
+  "X11" -> CM.liftM DM.Just HPG.gTApp
+  _     -> return DM.Nothing
+
+runOrSave' :: HR.TCanvas -> Maybe HR.TApplication ->  String -> IO ()
+runOrSave' tCan tApp fname = case fname of
+  "X11" -> HR.run (DM.fromJust tApp) 1
+  _     -> HR.saveAs tCan (str2cstr fname) (str2cstr "")
+
+
+{-- Supplementary Functions for Foreign Types --}
+str2cstr :: String -> FCS.CString
+str2cstr = SIOU.unsafePerformIO.FCS.newCString
--- attic/HROOT_TApplication/PlotModuleForNewHROOT.hs
+++ attic/HROOT_TApplication/PlotModuleForNewHROOT.hs
@@ -1,7 +1,7 @@
 {-******************************************
   *     File Name: PlotModuleForNewHROOT.hs
   *        Author: Takahiro Yamamoto
-  * Last Modified: 2014/08/05 17:07:50
+  * Last Modified: 2014/08/06 10:50:48
   *******************************************-}
 
 module PlotModuleForNewHROOT (
@@ -16,20 +16,20 @@ module PlotModuleForNewHROOT (
 ) where
 
 import qualified Control.Monad as CM
-import qualified Data.IORef as DIO
-import qualified Data.Maybe as DM
-import qualified HROOT as HR
-import qualified System.IO.Unsafe as SIOU
 import qualified Foreign.C.String as FCS
 import qualified Foreign.C.Types as FCT
 import qualified Foreign.Marshal.Array as FMA
 import qualified Foreign.Ptr as FP
 import qualified Foreign.Storable as FS
+import qualified HROOT as HR
+import qualified System.IO.Unsafe as SIOU
 
 import HasKAL.PlotUtils.PlotOption.PlotOptionHROOT
+import qualified HROOT_Supplementary as HRS
+import qualified GlobalTApplication as HPG
 
 data MultiPlot = Over | Divide deriving (Eq)
---data ColorOpt = BLACK | RED | GREEN | BLUE | YELLOW | PINK | CYAN deriving (Eq)
+data ColorOpt = BLACK | RED | GREEN | BLUE | YELLOW | PINK | CYAN deriving (Eq, Ord)
 
 {-- External Functions --}
 plot :: LogOption -> PlotTypeOption -> (String, String) -> String -> String -> [(Double, Double)] -> IO ()
@@ -54,12 +54,9 @@ dPlotX log mark xyLables titles dats = dPlot log mark xyLables titles "X11" dats
 {-- Internal Functions --}
 plotBase :: MultiPlot -> LogOption -> PlotTypeOption -> [(String, String)] -> [String] -> String -> [[(Double, Double)]] -> IO ()
 plotBase multi log mark xyLables titles fname dats = do
-  tApp <- case fname of -- :: IO (Maybe HR.TApplication)
-    "X11" -> CM.liftM DM.Just $ DIO.readIORef gTApp
-    _     -> return DM.Nothing
-
+  tApp <- HRS.newTApp' fname
   tCan <- HR.newTCanvas (str2cstr "title") (str2cstr "HasKAL ROOT") 640 480
-  setLog' tCan log
+  HRS.setLog' tCan log
 
   tGras <- CM.forM dats $ \dat -> HR.newTGraph (toEnum $ length dat) (getPtrX dat) (getPtrY dat)
   CM.zipWithM_ HR.setTitle tGras $ map str2cstr titles -- title
@@ -68,33 +65,20 @@ plotBase multi log mark xyLables titles fname dats = do
   CM.zipWithM_ setXYLabel' tGras xyLables -- lable (X軸、Y軸)
 
   case multi of -- :: IO ()
-    Over -> draws' tGras mark
+    Over -> do 
+      draws' tGras mark
     Divide -> do
       HR.divide_tvirtualpad tCan 2 2 0.01 0.01 0 -- 最大4つ(2x2)に固定
       CM.forM_ [1..(min 4 $ length dats)] $ \lambda -> do
         HR.cd tCan (toEnum $ lambda)
         draws' [tGras !! (lambda-1)] mark
 
-  case fname of -- :: IO ()
-    "X11" -> HR.run (DM.fromJust tApp) 1
-    _     -> HR.saveAs tCan (str2cstr fname) (str2cstr "")
-
+  HRS.runOrSave' tCan tApp fname
   CM.mapM HR.delete tGras
   HR.delete tCan
 
 
-{--  Supplementary Functions for HROOT --}
-setLog' :: HR.TCanvas -> LogOption -> IO ()
-setLog' tCan flag
-  | flag == Linear = return ()
-  | flag == LogX   = HR.setLogx tCan 1
-  | flag == LogY   = HR.setLogy tCan 1
-  -- | flag == LogZ   = HR.setLogz tCan 1
-  | flag == LogXY  = mapM_ (setLog' tCan) [LogX, LogY]
-  -- | flag == LogXZ  = mapM_ (setLog' tCan) [LogX, LogZ]
-  -- | flag == LogYZ  = mapM_ (setLog' tCan) [LogY, LogZ]
-  -- | flag == LogXYZ  = mapM_ (setLog' tCan) [LogX, LogY, LogZ]
-
+{--  Supplementary Functions for TGraph --}
 draws' :: [HR.TGraph] -> PlotTypeOption -> IO ()
 draws' tGras flag
   | flag == Line      = CM.zipWithM_ HR.draw tGras $ map str2cstr $ "AL" : repeat "L"
@@ -113,16 +97,17 @@ setXYLabel' tGra (labelX, labelY) = do
   HR.setTitle (SIOU.unsafePerformIO $ HR.tGraphGetXaxis tGra) $ str2cstr labelX
   HR.setTitle (SIOU.unsafePerformIO $ HR.tGraphGetYaxis tGra) $ str2cstr labelY
 
--- for TApplication class as global var.
-gTApp :: DIO.IORef HR.TApplication
-gTApp = SIOU.unsafePerformIO globalTAppIO
+color2cint :: ColorOpt -> FCT.CInt
+color2cint color
+  | color == BLACK  = 1
+  | color == RED    = 2
+  | color == GREEN  = 3
+  | color == BLUE   = 4
+  | color == YELLOW = 5
+  | color == PINK   = 6
+  | color == CYAN   = 7
 
-globalTAppIO :: IO (DIO.IORef HR.TApplication)
-globalTAppIO = do
-  tApp <- HR.newTApplication (str2cstr "test") (list2ptr [toEnum 0]) $ list2ptr [str2cstr "test"]
-  DIO.newIORef tApp
-
-{-- Supplementary Functions --}
+{-- Supplementary Functions for Foreign Types --}
 str2cstr :: String -> FCS.CString
 str2cstr = SIOU.unsafePerformIO.FCS.newCString
 
