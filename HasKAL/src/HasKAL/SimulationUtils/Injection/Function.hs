8b7a6b16c08b145e5841530e86d7adc82a720d2e
enoshima <kazuhiro.hayama@gmail.com>
2014-08-13 20:52:06 +0900

    start adding Injection codes (not complete yet)

---
+++ HasKAL/src/HasKAL/SimulationUtils/Injection/Function.hs
@@ -0,0 +1,45 @@
+{-
+- Module for software injection
+-}
+
+module HasKAL.SimulationUtils.Injection.Function
+( injDetectorResponse
+--,
+) where
+
+import HasKAL.DetectorUtils.Detector
+import HasKAL.DetectorUtils.DetectorParam
+import HasKAL.DetectorUtils.Functions
+import HasKAL.DetectorUtils.Signature
+import System.IO.Unsafe
+import HasKAL.TimeUtils.Signature
+import HasKAL.SimulationUtils.Injection.Signature
+
+
+
+getPolarizations:: SOURCE_TYPE -> GravitationalWave
+getPolarizations srcType
+  | sigType srcType == "SG235Q8d9" =
+    ( map (\x -> read x :: Double) (lines $ unsafePerformIO $ readFile "SG235Q8d9.txt")
+    , map (\x -> read x :: Double) (lines $ unsafePerformIO $ readFile "SG235Q8d9.txt"))
+  | otherwise = error "not recognized"
+
+
+injDetectorResponse :: Detector -> SOURCE_TYPE -> GPSTIME -> [(GPSTIME, Double)]
+injDetectorResponse detName srcType gps = do
+  let detparam
+        | detName == LIGO_Hanford = ligoHanford
+        | detName == LIGO_Livingston = ligoLivingston
+        | detName == KAGRA = kagra
+        | otherwise = error "not recognized"
+
+      (antennaPattern, tauS) = fplusfcrossts detparam (longitude srcType) (latitude srcType) (psi srcType)
+
+      detresp = genDetectorResponse antennaPattern $ getPolarizations srcType
+
+      startGPSTime = fromIntegral (fst gps) + 1E-9 * fromIntegral (snd gps) + tauS
+      gpsTime = [startGPSTime+dt|dt<-[0, (fs srcType)..]] :: [Double]
+      timetrain = map (\x -> (truncate x,  truncate ((x-fromIntegral (truncate x))*1E9))) gpsTime
+  zip timetrain detresp
+
+
---
+++ HasKAL/src/HasKAL/SimulationUtils/Injection/Signature.hs
@@ -0,0 +1,12 @@
+module HasKAL.SimulationUtils.Injection.Signature where
+
+type SigType = String
+
+
+data SOURCE_TYPE = SOURCE_TYPE {  sigType :: SigType
+                                , longitude :: Double
+                                , latitude :: Double
+                                , psi :: Double
+                                , fs :: Double
+                               } deriving (Show)
+
