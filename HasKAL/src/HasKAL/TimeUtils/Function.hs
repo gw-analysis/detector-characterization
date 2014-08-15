f5e00578080194bcedcb26038b75bf84fa922ae3
enoshima <kazuhiro.hayama@gmail.com>
2014-08-15 21:08:21 +0900

    aa

--- HasKAL/src/HasKAL/TimeUtils/Function.hs
+++ HasKAL/src/HasKAL/TimeUtils/Function.hs
@@ -2,7 +2,7 @@ module HasKAL.TimeUtils.Function
 ( formatGPS
 --,
 ) where
-import HasKAL.TimeUtils.Function
+import HasKAL.TimeUtils.Signature
 
 formatGPS :: Double -> GPSTIME
 formatGPS x = (truncate x, truncate ((x-fromIntegral (truncate x))*1E9))
