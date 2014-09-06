894b87abc84ccf3b50c0320a7d90449a176904b8
yuzuri <yuzurihara@yukimura.hep.osaka-cu.ac.jp>
2014-09-07 04:13:25 +0900

    modify analysis code to generate time domain plot and spectrogram of seismic noise channel and GW channel

--- attic/CorrelationAnalysis/frameData2SpectrogramePic.hs
+++ attic/CorrelationAnalysis/frameData2SpectrogramePic.hs
@@ -21,17 +21,44 @@ import HasKAL.MonitorUtils.CorrelationMon.CalCorrelation
 import HasKAL.MonitorUtils.CorrelationMon.CorrelationMethod
 import HasKAL.FrameUtils.FrameUtils -- read Frame file
 
-import HasKAL.PlotUtils.PlotOption.PlotOptionHROOT
+import qualified HasKAL.PlotUtils.PlotOption.PlotOptionHROOT as PMOP
 import qualified HasKAL.PlotUtils.HROOT.PlotGraph as PM
 import qualified HasKAL.PlotUtils.HROOT.PlotGraph3D as PM3
 --import qualified HasKAL.PlotUtils.HROOT.Histogram as H
 import qualified HasKAL.PlotUtils.HROOT.SignalHandlerHROOT as RSH
 
 import qualified HasKAL.SpectrumUtils.SpectrumUtils as SU
+--import qualified HasKAL.SignalProcessingUtils.Interpolation as SPUI
+--import qualified HasKAL.SignalProcessingUtils.InterpolationType as SPUIT
 
 main = do
 
- let frameFileName = ["/home/yuzurihara/frame/clio/X-R-1034657456-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657584-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657472-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657600-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657488-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657616-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657504-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657632-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657520-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657648-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657536-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657664-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657552-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657680-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657568-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657696-16.gwf"]
+
+ -- fft check
+ -- /home/yuzurihara/frame/clio/X-R-1034657456-16.gwf
+
+
+ -- read data of 
+ trfData' <- readFile "./modG_factor.txt"
+ let trfDataf = map string2Double $ map (!!0) $ map words $ lines trfData'
+ let trfDataG = map string2Double $ map (!!1) $ map words $ lines trfData'
+ --print $ zip trfDataf trfDataG
+
+ -- interporate of transfer function -> not necessaly?
+ -- interp xx yy interp_xx interpType    
+ --let dataInterpFreq = map [1..
+ --print $ SPUI.interp trfDataf trfDataG 5.0 SPUIT.Linear
+ --map SPUI.interp trfDataf trfDataG 5.0 SPUIT.Linear
+ --PM.plotO PM.Linear PM.Point ("frequency [Hz]","interporated transfer function") "hoge" plotdata2
+
+
+ -- plot transfer function, check
+ let fname = "CLIO_transfer_function.png" ::String
+ let channelName = "CLIO_transfer_function" ::String
+ PM.plot PM.LogXY PM.Line ("freqnency [Hz]","transfer function") channelName fname $ zip trfDataf trfDataG
+
+
+ --let frameFileName = ["/home/yuzurihara/frame/clio/X-R-1034657456-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657584-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657472-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657600-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657488-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657616-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657504-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657632-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657520-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657648-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657536-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657664-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657552-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657680-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657568-16.gwf","/home/yuzurihara/frame/clio/X-R-1034657696-16.gwf"]
  let startGPS = 456 :: Int
      endGPS   = 696 :: Int
      listGPS' = [startGPS,(startGPS+16)..endGPS]::[Int]
@@ -57,14 +84,14 @@ main = do
   let dataSpect = SU.gwspectrogram ifs_harf ifs_harf dfs dataChannel
   let fname =  (showFFloat (Just 0) getGpsTime "" ) ++ "_" ++ (channelName) ++ "_seis_spect.png"
   let channelName' = (channelName) ++ "__" ++ (showFFloat (Just 0) getGpsTime "" ) :: String
-  PM3.spectrogram LogYZ COLZ " " channelName' fname dataSpect
+  PM3.spectrogram PMOP.LogYZ PMOP.COLZ " " channelName' fname dataSpect
 
   -- plot s(t)
   let xdata1  = map (/dfs) $ take (length dataChannel) [1,2..]
       plotdata1 = zip xdata1 (dataChannel)
   let fname =  (showFFloat (Just 0) getGpsTime "" ) ++ "_" ++ (channelName) ++ "_vt.png"
   let channelName' = (channelName) ++ "__" ++ (showFFloat (Just 0) getGpsTime "" ) :: String
-  PM.plot PM.Linear PM.Line ("time[sec]","s(t)") channelName' fname plotdata1
+  PM.plot PMOP.Linear PMOP.Line ("time[sec]","s(t)") channelName' fname plotdata1
 
 
   -- plot lock flag
@@ -78,9 +105,10 @@ main = do
       plotdata2 = zip xdata2 data2
   let fname =  (showFFloat (Just 0) getGpsTime "" ) ++ "_" ++ (channelName) ++ "_lock_flag.png"
   let channelName' = (channelName) ++ "__" ++ (showFFloat (Just 0) getGpsTime "" ) :: String
-  PM.plot PM.Linear PM.Line ("time[sec]","lock flag") channelName' fname plotdata2
+  PM.plot PMOP.Linear PMOP.Line ("time[sec]","lock flag") channelName' fname plotdata2
 
-  -- plot GW channel
+
+  -- plot non-calibrated GW channel in time domain
   let channelName = "X1:CTR-LSC_ETMX_OUT_DQ"
   let dfs = 16384 ::Double
   readData3 <- readFrame channelName (fileName)
@@ -91,11 +119,60 @@ main = do
   let channelName' = (channelName) ++ "__" ++ (showFFloat (Just 0) getGpsTime "" ) :: String
   PM.plot PM.Linear PM.Line ("time[sec]","GW channel") channelName' fname plotdata3
 
-  -- spectrogram of GW channel
+  -- spectrogram of non-calibrated GW channel
   let dataSpect2 = SU.gwspectrogram ifs_harf ifs_harf dfs data3
   let fname =  (showFFloat (Just 0) getGpsTime "" ) ++ "_" ++ (channelName) ++ "_GWchannel_spect.png"
   let channelName' = (channelName) ++ "__" ++ (showFFloat (Just 0) getGpsTime "" ) :: String
-  PM3.spectrogram LogYZ COLZ " " channelName' fname dataSpect2
+  PM3.spectrogram PMOP.LogYZ PMOP.COLZ " " channelName' fname dataSpect2
+  
+
+  -- using FFT, spectrum of non-calibrated GW channel
+  -- gwpsd :: [Double]-> Int -> Double -> [(Double, Double)]
+  -- gwpsd dat nfft fs = gwpsdCore Welch dat nfft fs Hann
+  let dataSpecGW = SU.gwpsd data3 (length data3) dfs
+      fname =  (showFFloat (Just 0) getGpsTime "" ) ++ "_" ++ (channelName) ++ "_GWchannel_sf.png"
+      channelName' = (channelName) ++ "__" ++ (showFFloat (Just 0) getGpsTime "" ) :: String
+  PM.plot PMOP.LogXY PMOP.Line ("frequency [Hz]","interporated transfer function") channelName' fname dataSpecGW
+
+
+  -- using transfer function, calculate calibrated spectrum
+  --let dataSpecGW' = take (length trfDataf) dataSpecGW
+  --    dataCalibSpect = zipWith (*) (map snd dataSpecGW') trfDataG
+  --    dataCalibSpectSet = zip trfDataf dataCalibSpect
+  --    fname =  (showFFloat (Just 0) getGpsTime "" ) ++ "_" ++ (channelName) ++ "_GWchannel_calibrated.png"
+  --    channelName' = (channelName) ++ "__" ++ (showFFloat (Just 0) getGpsTime "" ) :: String
+  --PM.plot PMOP.LogXY PMOP.Line ("frequency [Hz]","calibrated spectrum of GW channel") channelName' fname dataCalibSpectSet
+  --PM.plot PMOP.Linear PMOP.Line ("frequency [Hz]","calibrated spectrum of GW channel") channelName' fname dataCalibSpectSet
+
+
+  --print $ length dataCalibSpect
+  --print $ length dataSpecGW'
+  --print $ length trfDataf
+
+  --print $ take 20 $ reverse trfDataf
+  --print $ take 20 $ reverse dataCalibSpect
+
+  -- spectrogram of calibrated GW channel 
+  -- this need IFFT, so now its difficult.
+  --let dataSpect3 = SU.gwspectrogram ifs_harf ifs_harf dfs 
+  --let fname =  (showFFloat (Just 0) getGpsTime "" ) ++ "_" ++ (channelName) ++ "_GWchannel_spect.png"
+  --PM3.spectrogram PMOP.LogYZ PMOP.COLZ " " channelName' fname dataSpect3
+
+  --print $ take 15 $ zip (map fst dataSpecGW) trfDataf
+  --print $ length (map fst dataSpecGW')
+  --print $ length trfDataf
+  --print $ length trfDataG
+  --print $ head $ reverse (map fst dataSpecGW)
+
   
   
+
+
   
+
+string2Int :: String -> Int
+string2Int str = read str::Int
+
+string2Double :: String -> Double
+string2Double str = read str::Double
+
