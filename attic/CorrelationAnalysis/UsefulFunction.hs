2b1eec968752423cb600cc46c854aa99690100bc
yuzuri <yuzurihara@yukimura.hep.osaka-cu.ac.jp>
2014-09-08 07:52:06 +0900

    modify code to down-sample and take correlation between 2 channel data

--- attic/CorrelationAnalysis/UsefulFunction.hs
+++ attic/CorrelationAnalysis/UsefulFunction.hs
@@ -1,10 +1,10 @@
--- Time-stamp: "2014-09-08 06:43:04 yuzurihara"
-module UsefulFunction.hs (
+-- Time-stamp: "2014-09-08 07:29:28 yuzurihara"
+module UsefulFunction (
        taple2string
        ,string2Int
        ,string2Double
+       ,skipListByK
 ) where
-
 
 
 taple2string ::[Double] -> [Double] -> String
@@ -15,3 +15,6 @@ string2Int str = read str::Int
 
 string2Double :: String -> Double
 string2Double str = read str::Double
+
+skipListByK :: Int -> Int -> [a] -> [a]
+skipListByK k rest lst = map snd $ filter (\x -> (fst x) `mod` k == rest) $ zip [1..] lst
--- attic/CorrelationAnalysis/frameData2LPF_correlation.hs
+++ attic/CorrelationAnalysis/frameData2LPF_correlation.hs
@@ -6,8 +6,10 @@ import System.Random
 import System.Process -- system
 import Data.List.Split -- splitOn
 
-import HasKAL.FrameUtils.FrameUtils -- read Frame file
+import HasKAL.MonitorUtils.CorrelationMon.CalCorrelation
+import HasKAL.MonitorUtils.CorrelationMon.CorrelationMethod
 
+import HasKAL.FrameUtils.FrameUtils -- read Frame file
 import qualified HasKAL.PlotUtils.PlotOption.PlotOptionHROOT as PMOP
 import qualified HasKAL.PlotUtils.HROOT.PlotGraph as PM
 import qualified HasKAL.PlotUtils.HROOT.PlotGraph3D as PM3
@@ -17,18 +19,20 @@ import HasKAL.SignalProcessingUtils.Filter
 import HasKAL.SignalProcessingUtils.ButterWorth
 import HasKAL.SignalProcessingUtils.FilterType
 
+import UsefulFunction
+
 main = do
 
 
  let fileName = "/home/yuzurihara/frame/clio/X-R-1034657664-16.gwf"
 
-
- let channelName = "X1:CTR-PSL_PWR_IN1_DQ"
+ let channelName = "X1:CTR-PSL_SEIS_IN1_DQ"
  let dfs = 2048 ::Double
  readData1 <- readFrame channelName (fileName)
  let data1  = map realToFrac (eval readData1)
      tt1 = map (/dfs) $ take (length data1) [1,2..]
 
+ --print $ length data1
 
  let channelName = "X1:CTR-LSC_ETMX_OUT_DQ"
  let dfs = 16384 ::Double
@@ -40,13 +44,31 @@ main = do
 -- low-pass filter of cutoff frequency = 1024Hz
 -- let dataLPF = butter 4 dfs (2048::Double) Low
  let n=4::Int
- let lstn = [2,3..10]::[Int]
- forM lstn $ \n -> do
-  let (numCoeffLow,denomCoeffLow) = butter n dfs 1024.0 Low :: ([Double], [Double])
-      dataFiltered = iirFilter data1 numCoeffLow denomCoeffLow
+ let (numCoeffLow,denomCoeffLow) = butter n dfs 1024.0 Low :: ([Double], [Double])
+     dataFiltered = iirFilter data2 numCoeffLow denomCoeffLow
+     dataFilteredDownSample = skipListByK 8 0 dataFiltered
 
-  let fname =  (show n) ++ "_lpf_GWchannel.png"
-  PM.oPlot PMOP.Linear PMOP.Line ("time [sec]", "v(t)") (repeat "check for filted data") fname $ [zip tt data1, zip tt dataFiltered]
-  let fname =  "op_" ++ (show n) ++ "_lpf_GWchannel.png"
-  PM.plot PMOP.Linear PMOP.Line ("time [sec]", "v(t)") ("check for filted data") fname $ zip tt data1
- print n
+-- let fname =  (show n) ++ "_lpf_GWchannel.png"
+-- PM.oPlot PMOP.Linear PMOP.Line ("time [sec]", "v(t)") (repeat "check for filted data") fname $ [zip tt data1, zip tt dataFiltered]
+-- let fname =  "op_" ++ (show n) ++ "_lpf_GWchannel.png"
+--  PM.plot PMOP.Linear PMOP.Line ("time [sec]", "v(t)") ("check for filted data") fname $ zip tt data1
+
+
+ let dfs  = 2048.0 ::Double
+     maxN = floor $ 0.01 * dfs ::Int
+ print maxN
+ -- calculate correlation value
+ --takeCorrelation method x y maxN = case method of
+ --let rValueLst = takeCorrelation Peason data1 dataFilteredDownSample maxN
+ let rValueLst = takeCorrelation Peason data1 dataFilteredDownSample maxN
+ let rValue = maximum rValueLst
+ --let rValue_10 = read (showFFloat (Just 10) rValue "")::Double
+ let maxValueIndex = correlationResult2pickupMaxValueIndex rValueLst dfs
+ let tapleCorrelationValue = (rValue, fst maxValueIndex, snd maxValueIndex)
+ print $ tapleCorrelationValue
+
+
+
+
+ --print "hoge"
+
