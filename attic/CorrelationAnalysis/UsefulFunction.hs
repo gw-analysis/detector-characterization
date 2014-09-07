908704738c0ba5998458613b5491b4887ffdf838
yuzuri <yuzurihara@yukimura.hep.osaka-cu.ac.jp>
2014-09-08 08:50:47 +0900

    code to calculate correlation value over many frame data

--- attic/CorrelationAnalysis/UsefulFunction.hs
+++ attic/CorrelationAnalysis/UsefulFunction.hs
@@ -1,4 +1,4 @@
--- Time-stamp: "2014-09-08 07:29:28 yuzurihara"
+-- Time-stamp: "2014-09-08 08:40:41 yuzurihara"
 module UsefulFunction (
        taple2string
        ,string2Int
@@ -7,14 +7,44 @@ module UsefulFunction (
 ) where
 
 
+-- taple2string [1, 2, 3, 4] [4, 5, 6, 7]
+-- -> "1 4\n2 5\n3 6\n4 7\n"
+-- you can easily write ANSII txt, such as 
+--   writeFile "hoge.txt" $ taple2string [1, 2, 3, 4] [4, 5, 6, 7]
 taple2string ::[Double] -> [Double] -> String
 taple2string a b = unlines  $ zipWith (++) (map show a)  $ zipWith (++) (repeat " ")  (map show b)
 
+
+-- string2Int "3"
+-- -> return 3::Int
 string2Int :: String -> Int
 string2Int str = read str::Int
 
+
+-- string2Int "3"
+-- -> return 3.0::Double
 string2Double :: String -> Double
 string2Double str = read str::Double
 
+
+-- skipListByK 2 0 [5.0, 2.0, 3.0, 5.0, 9.0]
+--   return [2.0,5.0]
+-- you can get skiped list by k.
+-- Another case :
+-- skipListByK 2 0 [5.0, 2.0, 3.0, 5.0, 9.0]
+--   return [5.0,3.0,9.0]
+-- you can get skiped list by k.
+
 skipListByK :: Int -> Int -> [a] -> [a]
 skipListByK k rest lst = map snd $ filter (\x -> (fst x) `mod` k == rest) $ zip [1..] lst
+
+
+
+fst' :: (Double, Double, Double)  -> Double
+fst' (a, b, c) = a
+
+snd' :: (Double, Double, Double)  -> Double
+snd' (a, b, c) = b
+
+trd' :: (Double, Double, Double)  -> Double
+trd' (a, b, c) = c
--- attic/CorrelationAnalysis/frameData2LPF_correlation.hs
+++ attic/CorrelationAnalysis/frameData2LPF_correlation.hs
@@ -24,51 +24,64 @@ import UsefulFunction
 main = do
 
 
- let fileName = "/home/yuzurihara/frame/clio/X-R-1034657664-16.gwf"
+ let startGPS = 7008 :: Int
+     endGPS   = 8000 :: Int
+     listGPS' = [startGPS,(startGPS+16)..endGPS]::[Int]
+     listGPS  = map show listGPS'
+ --print listGPS
+ let frameFileName' = map ("/home/yuzurihara/frame/clio/X-R-103465"++) listGPS
+     frameFileName  = map (++ "-16.gwf") frameFileName'
+ --print frameFileName
 
- let channelName = "X1:CTR-PSL_SEIS_IN1_DQ"
- let dfs = 2048 ::Double
- readData1 <- readFrame channelName (fileName)
- let data1  = map realToFrac (eval readData1)
-     tt1 = map (/dfs) $ take (length data1) [1,2..]
+ --let fileName = "/home/yuzurihara/frame/clio/X-R-1034657664-16.gwf"
 
- --print $ length data1
 
- let channelName = "X1:CTR-LSC_ETMX_OUT_DQ"
- let dfs = 16384 ::Double
- readData2 <- readFrame channelName (fileName)
- let data2  = map realToFrac (eval readData2)
-     tt = map (/dfs) $ take (length data2) [1,2..]
+ forM frameFileName $ \fileName -> do
+ 
+  let getGpsTime = read $ (!!2) $ splitOn "-" $ last $ splitOn "/" fileName :: Int
+  let channelName = "X1:CTR-PSL_SEIS_IN1_DQ"
+  let dfs = 2048 ::Double
+  readData1 <- readFrame channelName (fileName)
+  let data1  = map realToFrac (eval readData1)
+      tt1 = map (/dfs) $ take (length data1) [1,2..]
+
+  let channelName = "X1:CTR-LSC_ETMX_OUT_DQ"
+  let dfs = 16384 ::Double
+  readData2 <- readFrame channelName (fileName)
+  let data2  = map realToFrac (eval readData2)
+      tt = map (/dfs) $ take (length data2) [1,2..]
 
 -- resampling from 16384Hz to 2048Hz
 -- low-pass filter of cutoff frequency = 1024Hz
 -- let dataLPF = butter 4 dfs (2048::Double) Low
- let n=4::Int
- let (numCoeffLow,denomCoeffLow) = butter n dfs 1024.0 Low :: ([Double], [Double])
-     dataFiltered = iirFilter data2 numCoeffLow denomCoeffLow
-     dataFilteredDownSample = skipListByK 8 0 dataFiltered
-
+  let n=4::Int
+  let (numCoeffLow,denomCoeffLow) = butter n dfs 1024.0 Low :: ([Double], [Double])
+      dataFiltered = iirFilter data2 numCoeffLow denomCoeffLow
+ 
 -- let fname =  (show n) ++ "_lpf_GWchannel.png"
 -- PM.oPlot PMOP.Linear PMOP.Line ("time [sec]", "v(t)") (repeat "check for filted data") fname $ [zip tt data1, zip tt dataFiltered]
 -- let fname =  "op_" ++ (show n) ++ "_lpf_GWchannel.png"
 --  PM.plot PMOP.Linear PMOP.Line ("time [sec]", "v(t)") ("check for filted data") fname $ zip tt data1
 
-
- let dfs  = 2048.0 ::Double
-     maxN = floor $ 0.01 * dfs ::Int
- print maxN
- -- calculate correlation value
- --takeCorrelation method x y maxN = case method of
- --let rValueLst = takeCorrelation Peason data1 dataFilteredDownSample maxN
- let rValueLst = takeCorrelation Peason data1 dataFilteredDownSample maxN
- let rValue = maximum rValueLst
- --let rValue_10 = read (showFFloat (Just 10) rValue "")::Double
- let maxValueIndex = correlationResult2pickupMaxValueIndex rValueLst dfs
- let tapleCorrelationValue = (rValue, fst maxValueIndex, snd maxValueIndex)
- print $ tapleCorrelationValue
+  let dfs  = 2048.0 ::Double
+      maxN = floor $ 0.01 * dfs ::Int
+  --print maxN
 
 
+  result <- forM [0,1..7] $ \rest ->do
+   let dataFilteredDownSample = skipListByK 8 rest dataFiltered
+   -- calculate correlation value
+   --takeCorrelation method x y maxN = case method of
+   --let rValueLst = takeCorrelation Peason data1 dataFilteredDownSample maxN
+   let rValueLst = takeCorrelation Peason data1 dataFilteredDownSample maxN
+   let rValue = maximum rValueLst
+   --let rValue_10 = read (showFFloat (Just 10) rValue "")::Double
+   let maxValueIndex = correlationResult2pickupMaxValueIndex rValueLst dfs
+   let tapleCorrelationValue = (getGpsTime, rValue, rest + (fst maxValueIndex),(fromIntegral rest)/dfs + (snd maxValueIndex))
+   --print $ tapleCorrelationValue
+   return tapleCorrelationValue
 
-
- --print "hoge"
+  --print result 
+  print $ maximum result
+ 
 
