bec750d65e527ec1c95f7655a62b496957cd033b
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2014-09-11 12:07:58 +0900

    added interpV for multi-point interpolation

--- HasKAL/src/HasKAL/SignalProcessingUtils/Interpolation.hs
+++ HasKAL/src/HasKAL/SignalProcessingUtils/Interpolation.hs
@@ -1,7 +1,8 @@
 
 module HasKAL.SignalProcessingUtils.Interpolation
   (
-  interp
+    interp
+  , interpV
   ) where
 
 
@@ -12,6 +13,7 @@ import Foreign.Storable(peek)
 import Foreign.Marshal.Array
 import HasKAL.SignalProcessingUtils.InterpolationType
 import System.IO.Unsafe
+import Control.Monad
 
 --type Interpolation = Ptr (Ptr gsl_interp_type)
 --main :: IO ()
@@ -31,6 +33,45 @@ import System.IO.Unsafe
 --  let interp_x = x!!5+0.01
 
 --data InterpType = Spline | Linear
+interpV :: [Double] -> [Double] -> [Double] -> InterpType -> [Double]
+interpV xx yy interp_xx interpType = unsafePerformIO $ do
+  let x = d2cd xx
+      y = d2cd yy
+      interp_x = d2cd interp_xx
+
+  let ndat = fromIntegral ( length x) :: CSize
+
+  -- allocate memory
+  --ptr_gsl_interp_accel :: Ptr C'gsl_interp_accel
+  ptr_gsl_interp_accel <- c'gsl_interp_accel_alloc
+
+  -- generate instance of interpolation type gsl_interp_cspline, size 10
+  -- select type of interpolation
+  ptr_interp_type <- case interpType of
+      Spline -> do
+        ptr_gsl_interp_type <- peek p'gsl_interp_cspline
+        --ptr_interp_type :: Ptr_C'gsl_spline
+        c'gsl_spline_alloc ptr_gsl_interp_type ndat
+      Linear -> do
+        ptr_gsl_interp_type <- peek p'gsl_interp_linear
+        c'gsl_spline_alloc ptr_gsl_interp_type ndat
+
+  withArray x $ \ptr'x ->
+    withArray y $ \ptr'y ->
+    -- initialize gsl_spline
+    c'gsl_spline_init ptr_interp_type ptr'x ptr'y ndat
+
+  -- perform the interpolation
+  interp_y <- mapM (\x->c'gsl_spline_eval ptr_interp_type x ptr_gsl_interp_accel) interp_x
+
+  -- freeing allocated memory
+  c'gsl_spline_free ptr_interp_type
+  c'gsl_interp_accel_free ptr_gsl_interp_accel
+
+  -- display the output
+  --putStrLn $ show $ realToFrac interp_y
+  return (cd2d interp_y)
+
 
 interp :: [Double] -> [Double] -> Double -> InterpType -> Double
 interp xx yy interp_xx interpType = unsafePerformIO $ do
@@ -77,7 +118,9 @@ d2cd [] = []
 --d2cd (x:xs) = (realToFrac x :: CDouble) : d2cd xs
 d2cd x = map realToFrac x
 
-
+cd2d :: [CDouble] -> [Double]
+cd2d [] = []
+cd2d x = map realToFrac x
 
 
 
