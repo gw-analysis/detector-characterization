17bf16ec391d9d45c063edd8cb4a5b659b12bcb9
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2014-09-23 15:46:26 +0900

    working on Injection

--- HasKAL/src/HasKAL/WaveUtils/Data.hs
+++ HasKAL/src/HasKAL/WaveUtils/Data.hs
@@ -1,19 +1,40 @@
 
-module HasKAL.WaveUtils.Data 
+module HasKAL.WaveUtils.Data
 where
 
 import HasKAL.DetectorUtils.Detector
 import HasKAL.TimeUtils.Signature
 
 
-data WaveData = WaveData { 
+data WaveData = WaveData {
     detector :: Detector
   , dataType :: String
   , samplingFrequency :: Double
   , startGPSTime :: GPSTIME
   , stopGPSTime  :: GPSTIME
-  , gwdata :: [Double]} 
+  , gwdata :: TimeSeries}
   deriving (Show, Eq, Read)
 
+data WaveProperty = WaveProperty {
+    mean :: UV.Vector Double -> Double
+  , variance :: UV.Vector Double -> Double
+  , spectrum :: UV.Vector Double -> Double -> [(Double, Double)]
+  , spectrogram :: UV.Vector Double -> Double -> [(Double, Double, Double)]
+    }
+    deriving (Show, Eq, Read)
+
+
+mkLIGOdataStructure :: String -> Double -> GPSTIME -> GPSTIME -> TimeSeries -> WaveData
+mkLIGOdataStructure datatype fs startGPS stopGPS xs
+  = WaveData { LIGO_Hanford
+             , datatype
+             , fs
+             , startGPS
+             , stopGPS
+             xs}
+
+
+getWaveProperty :: WaveData -> WaveProperty
+getWaveProperty x = undefined
 
 
--- HasKAL/src/HasKAL/WaveUtils/Signature.hs
+++ HasKAL/src/HasKAL/WaveUtils/Signature.hs
@@ -2,8 +2,12 @@
 module HasKAL.WaveUtils.Signature
 where
 
+
+import qualified Data.Array.Unboxed as UA
+
 type GravitationalWave = (Hplus,  Hcross)
 type Hplus = [Double]
 type Hcross = [Double]
-
+type TimeSeries =
+type WaveProperty =
 
