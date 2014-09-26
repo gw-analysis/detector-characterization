f7407c9237555891108b8b95d9583d8da0802c21
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2014-09-27 00:31:09 +0900

    change function fromGPS to deformatGPS

--- HasKAL/src/HasKAL/TimeUtils/Function.hs
+++ HasKAL/src/HasKAL/TimeUtils/Function.hs
@@ -1,11 +1,11 @@
 module HasKAL.TimeUtils.Function
 ( formatGPS
-, fromGPS
+, deformatGPS
 ) where
 import HasKAL.TimeUtils.Signature
 
 formatGPS :: Double -> GPSTIME
 formatGPS x = (truncate x, truncate ((x-fromIntegral (truncate x))*1E9))
 
-fromGPS :: GPSTIME -> Double
-fromGPS (x, y) = (fromIntegral x) + 1E-9 * (fromIntegral y)
+deformatGPS :: GPSTIME -> Double
+deformatGPS (x, y) = (fromIntegral x) + 1E-9 * (fromIntegral y)
