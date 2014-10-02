e99d955a37d0e7ab1bb9ea06cd94784f324f6b87
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2014-10-02 17:46:29 +0900

    minor changes

--- HasKAL/src/HasKAL/SignalProcessingUtils/LinearPrediction.hs
+++ HasKAL/src/HasKAL/SignalProcessingUtils/LinearPrediction.hs
@@ -18,14 +18,14 @@ import HasKAL.SignalProcessingUtils.Interpolation
 import HasKAL.SignalProcessingUtils.InterpolationType
 import HasKAL.SpectrumUtils.SpectrumUtils
 import HasKAL.WaveUtils.Data
- 
+
 
 {- exposed functions -}
 lpefCoeff :: Int -> [(Double,Double)] -> ([Double],Double)
 lpefCoeff p psddat = (out,rho)
   where
     (out,rho) = levinson p r
-    r = toList.fst.fromComplex.ifft.fromList 
+    r = toList.fst.fromComplex.ifft.fromList
       $ [fs*nn*x/nn:+0|x<-(snd.unzip) psddat]
     fs = last.fst.unzip $ psddat
     nn = fromIntegral $ length psddat :: Double
@@ -49,23 +49,23 @@ whitening (whnb,rho) x = map (/sqrt rho) $ fir whnb x
 
 whiteningWaveData :: ([Double],Double) -> WaveData -> WaveData
 whiteningWaveData (whnb,rho) x = do
-  let y = map (/sqrt rho) $ fir whnb (gwdata (toList x))
+  let y = map (/sqrt rho) $ fir whnb $ toList (gwdata x)
   fromJust $ updateWaveDatagwdata x (fromList y)
 
 
 {- internal functions -}
-levinsonD :: (UV.Ix a, Integral a, RealFloat b) 
+levinsonD :: (UV.Ix a, Integral a, RealFloat b)
   => UV.Array Int Double           -- ^ r
   -> Int                           -- ^ p
   -> (UV.Array Int Double, Double) -- ^ (coefficients,rho)
 levinsonD r p = (UV.array (1,p) [ (k, a UV.!(p,k)) | k <- [1..p] ], rho UV.!p)
   where
-    a = UV.array ((1,1),(p,p)) 
-	  [((k,i), ak k i) | k<-[1..p], i<-[1..k]]:: UV.Array (Int,Int) Double
+    a = UV.array ((1,1),(p,p))
+      [((k,i), ak k i) | k<-[1..p], i<-[1..k]]:: UV.Array (Int,Int) Double
     rho = UV.array (1,p) [ (k, rhok k) | k <- [1..p] ]:: UV.Array Int Double
     ak 1 1         = -r UV.!1 / r UV.!0
     ak k i | k==i  = -(r UV.!k+sum [a UV.!(k-1,l)*r UV.!(k-l)|l<-[1..(k-1)]])
-	                 / rho UV.!(k-1)
+                   / rho UV.!(k-1)
            | otherwise = a UV.!(k-1,i) + a UV.!(k,k) * a UV.!(k-1,k-i)
     rhok 1 = (1 - (abs (a UV.!(1,1)))^(2::Int)) * r UV.!0
     rhok k = (1 - (abs (a UV.!(k, k)))^(2::Int)) * rho UV.!(k-1)
