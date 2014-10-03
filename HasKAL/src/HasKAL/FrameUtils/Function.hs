9e5b16603ce9502b108b6cde9ba9776e940115ab
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2014-10-03 17:16:12 +0900

    add function for reading frame file as WaveData format

--- HasKAL/src/HasKAL/FrameUtils/Function.hs
+++ HasKAL/src/HasKAL/FrameUtils/Function.hs
@@ -1,7 +1,7 @@
 {-******************************************
   *     File Name: Functions.hs
   *        Author: Takahiro Yamamoto
-  * Last Modified: 2014/10/03 10:59:59
+  * Last Modified: 2014/10/03 17:08:45
   *******************************************-}
 
 module HasKAL.FrameUtils.Function (
@@ -14,9 +14,11 @@ module HasKAL.FrameUtils.Function (
 import qualified Control.Monad as CM
 import qualified Data.Packed.Vector as DPV
 
+import qualified HasKAL.DetectorUtils.Detector as HDD
 import qualified HasKAL.FrameUtils.FrameUtils as HFF
 import qualified HasKAL.FrameUtils.PickUpFileName as HFP
 import qualified HasKAL.TimeUtils.Function as HTF
+import qualified HasKAL.WaveUtils.Data as HWD
 
 readFrame :: String -> String -> IO [Double]
 readFrame = (CM.liftM ((map realToFrac).HFF.eval).).HFF.readFrame
@@ -39,3 +41,15 @@ readFrameFromGPS gpsTime obsTime channel cache = do
 -- readFrameUntilGPS gpsTime obsTime channel cache = do
 --   let fileNames = HFP.pickUpFileNameinFile (gpsTime - obsTime) (gpsTime - 1) cache
 --   CM.liftM concat $ mapM (readFrame channel) fileNames
+
+readFrameWaveData :: HDD.Detector -> Integer -> Integer -> String -> String -> IO HWD.WaveData
+readFrameWaveData detector gpsTime obsTime channel cache = do
+  fs <- HFF.getSamplingFrequency (head fileNames) channel
+  fileGPS <- CM.liftM HTF.deformatGPS $ HFF.getGPSTime $ head fileNames
+  let startGPS = case ((fromIntegral gpsTime) - fileGPS) <= 0 of
+        True -> fileGPS
+        False -> (truncate $ fs * ((fromIntegral gpsTime) - fileGPS)) / fs + fileGPS
+      endGPS = (truncate $ fs * (fromIntegral obsTime)) / fs + startGPS
+  soft <- readFrameFromGPS gpsTime obsTime channel cache
+  return $ mkWaveData detector "test dayo" fs (deformatGPS startGPS) (deformatGPS endGPS) soft
+  
