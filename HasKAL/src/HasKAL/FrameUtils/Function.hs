46786778006fede20d29ebb34462c22bb6d0bf7e
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2014-10-03 11:07:48 +0900

    changed to take out data from the middle of the file

--- HasKAL/src/HasKAL/FrameUtils/Function.hs
+++ HasKAL/src/HasKAL/FrameUtils/Function.hs
@@ -1,14 +1,14 @@
 {-******************************************
   *     File Name: Functions.hs
   *        Author: Takahiro Yamamoto
-  * Last Modified: 2014/10/02 18:39:10
+  * Last Modified: 2014/10/03 10:59:59
   *******************************************-}
 
 module HasKAL.FrameUtils.Function (
    readFrame
   ,readFrameV
   ,readFrameFromGPS
-  ,readFrameUntilGPS
+--  ,readFrameUntilGPS
 ) where
 
 import qualified Control.Monad as CM
@@ -16,6 +16,7 @@ import qualified Data.Packed.Vector as DPV
 
 import qualified HasKAL.FrameUtils.FrameUtils as HFF
 import qualified HasKAL.FrameUtils.PickUpFileName as HFP
+import qualified HasKAL.TimeUtils.Function as HTF
 
 readFrame :: String -> String -> IO [Double]
 readFrame = (CM.liftM ((map realToFrac).HFF.eval).).HFF.readFrame
@@ -26,9 +27,15 @@ readFrameV = (CM.liftM (DPV.fromList.(map realToFrac).HFF.eval).).HFF.readFrame
 readFrameFromGPS :: Integer -> Integer -> String -> String -> IO [Double]
 readFrameFromGPS gpsTime obsTime channel cache = do
   let fileNames = HFP.pickUpFileNameinFile gpsTime (gpsTime + obsTime - 1) cache
-  CM.liftM concat $ mapM (readFrame channel) fileNames -- stack overflowするようならmapM'に置き換える
+  fs <- HFF.getSamplingFrequency (head fileNames) channel
+  fileGPS <- CM.liftM HTF.deformatGPS $ HFF.getGPSTime $ head fileNames
+  let headNum = case ((fromIntegral gpsTime) - fileGPS) <= 0 of
+        True -> 0
+        False -> truncate $ ((fromIntegral gpsTime) - fileGPS) * fs
+      length = truncate $ fs * (fromIntegral obsTime)
+  CM.liftM ((take length).(drop headNum).concat) $ mapM (readFrame channel) fileNames
 
-readFrameUntilGPS :: Integer -> Integer -> String -> String -> IO [Double]
-readFrameUntilGPS gpsTime obsTime channel cache = do
-  let fileNames = HFP.pickUpFileNameinFile (gpsTime - obsTime) (gpsTime - 1) cache
-  CM.liftM concat $ mapM (readFrame channel) fileNames
+-- readFrameUntilGPS :: Integer -> Integer -> String -> String -> IO [Double]
+-- readFrameUntilGPS gpsTime obsTime channel cache = do
+--   let fileNames = HFP.pickUpFileNameinFile (gpsTime - obsTime) (gpsTime - 1) cache
+--   CM.liftM concat $ mapM (readFrame channel) fileNames
