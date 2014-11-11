0f2a00ce10570071b0d734ca6a2e5d0472d348c5
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2014-11-11 17:35:48 +0900

    minor change

--- attic/HROOT_TApplication/RTP/cFunction.h
+++ attic/HROOT_TApplication/RTP/cFunction.h
@@ -1,17 +1,13 @@
 /******************************************
  *     File Name: cFunction.h
  *        Author: Takahiro Yamamoto
- * Last Modified: 2014/11/11 14:58:10
+ * Last Modified: 2014/11/11 17:33:01
  ******************************************/
 
 #ifndef I_GUARD_CFUNCTION_H
 #define I_GUARD_CFUNCTION_H
 
-//#include <stdio.h>
-#include <TSystem.h>
-#include <TSysEvtHandler.h>
 #include <TCanvas.h>
-#include <TH1.h>
 
 extern "C" {
   int cModified(TCanvas *canvas);
