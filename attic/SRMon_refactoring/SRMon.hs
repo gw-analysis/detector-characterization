d020b741f4336fef92a8ee99a8d5da711187eb3d
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2014-11-28 14:30:34 +0900

    add function using unbox vector

--- attic/SRMon_refactoring/SRMon.hs
+++ attic/SRMon_refactoring/SRMon.hs
@@ -1,11 +1,12 @@
 {-******************************************
   *     File Name: SRMon.hs
   *        Author: Takahiro Yamamoto
-  * Last Modified: 2014/11/26 13:13:37
+  * Last Modified: 2014/11/28 14:29:57
   *******************************************-}
 
 import qualified Data.List as L
 import qualified Control.Monad as M
+import Data.Vector.Unboxed as V
 
 -- HasKAL
 import qualified HasKAL.MonitorUtils.SRMon.StudentRayleighFunctions as SRF
@@ -16,8 +17,10 @@ import HasKAL.ExternalUtils.GSL.RandomNumberGeneration as RNG
 import HasKAL.ExternalUtils.GSL.RandomNumberDistributions as RND
 
 main = do
+  let dsize = 2048
+  let csize = 128
   rng <- newRngWithSeed (-1)
-  dats <- M.forM [1..5] $ \idxI -> M.forM [1..256] $ \idxJ -> do
+  dats <- M.forM [1..csize] $ \idxI -> M.forM [1..dsize] $ \idxJ -> do
     r <- gslRanGaussian rng 1.0
     i <- gslRanGaussian rng 1.0
     return $ sqrt (r*r + i*i)
@@ -26,9 +29,34 @@ main = do
   print nu
   let nuRe = srMonRe 0.99 dats
   print nuRe
+  let nuV = srMonV 0.99 $ Prelude.map fromList dats
+  print nuV
+
+-- unbox vector
+srMonV :: Double -> [Vector Double] -> [Double]
+srMonV pVal dataFs = Prelude.map (getNuV pVal) dataFs
+
+getNuV :: Double -> Vector Double -> Double
+getNuV pVal dataF = getClosestValueV e t
+  where e = empiricalQuantileV pVal dataF
+        t = V.map (theoreticalQuantileV pVal) $ fromList [2.0, 2.1..100.0]
+
+empiricalQuantileV :: Double -> Vector Double -> Double
+empiricalQuantileV pVal dataF = unsafeIndex (sort4Vec dataF) (pIdx -1)
+  where pIdx = truncate $ pVal * (fromIntegral $ V.length dataF)
+        sort4Vec = fromList.L.sort.toList -- vectorのソートに変更する
+
+theoreticalQuantileV :: Double -> Double -> (Double, Double)
+theoreticalQuantileV pVal nu = (SRF.hkalCdfStudentRayleighPinv sigma nu pVal, nu)
+  where sigma = sqrt $ (nu - 2) / nu
 
 
+getClosestValueV :: Double -> Vector (Double, Double) -> Double
+getClosestValueV basis dat = snd $ V.minimum $ V.zip diff $ V.map snd dat
+  where diff = V.map abs $ V.map ((-) basis) $ V.map fst dat
 
+
+-- refactoring functions
 srMonRe :: Double -> [[Double]] -> [Double]
 srMonRe pVal dataFs = Prelude.map (getNu pVal) dataFs
 
@@ -49,7 +77,7 @@ getClosestValue :: Double -> [(Double, Double)] -> Double
 getClosestValue basis dat = snd $ L.minimum $ L.zip diff $ Prelude.map snd dat
   where diff = Prelude.map abs $ Prelude.map ((-) basis) $ Prelude.map fst dat
 
-
+-- old function
 srMon :: Double -> [[Double]] -> [Double]
 srMon pVal dataFs = Prelude.map (SRM.getOptimalNuQuant pVal) dataFs
 
