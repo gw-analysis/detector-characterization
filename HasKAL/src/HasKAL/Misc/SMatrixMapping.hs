bc991c77d21bf50abb63b5dd9b5451788419b149
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2014-12-05 16:32:16 +0900

    add module for matrix mapping (Storable, Unbox)

---
+++ HasKAL/src/HasKAL/Misc/SMatrixMapping.hs
@@ -0,0 +1,60 @@
+{-******************************************
+  *     File Name: SMatrixMapping.hs
+  *        Author: Takahiro Yamamoto
+  * Last Modified: 2014/12/05 16:28:55
+  *******************************************-}
+
+module SMatrixMapping (
+   convertS2U
+  ,convertU2S
+  ,mapRows0
+  ,mapCols0
+  ,mapRows1
+  ,mapCols1
+) where
+
+
+import qualified Control.Monad as CM (forM)
+import System.IO.Unsafe (unsafePerformIO)
+import qualified Data.Vector.Generic as G
+
+{-- Storable type --}
+import Data.Packed.Matrix
+import Data.Packed.Vector
+
+{-- Unbox type --}
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Matrix.Unboxed as M
+
+{-- matrix type converter --}
+convertS2U :: (Element a, V.Unbox a) => Matrix a -> M.Matrix a
+convertS2U mat = M.fromVector rowNum colNum $ V.convert $ flatten mat
+  where rowNum = rows mat
+        colNum = cols mat
+
+convertU2S :: (V.Unbox a, Element a) => Matrix a -> Matrix a
+convertU2S mat = reshape colNum $ V.convert $ flatten mat
+  where colNum = cols mat
+
+
+{-- map functions --}
+mapRows0 :: (Element t) => (Vector t -> t) -> Matrix t -> Vector t
+mapRows0 fx mat = unsafePerformIO $ G.forM idxV $ \idx -> return $ fx $ getVecs idx mat
+  where idxV = fromList [0..(rows mat)-1]
+        getVecs idx mat = flatten $ takeRows 1 $ dropRows idx mat
+
+mapCols0 :: (Element t) => (Vector t -> t) -> Matrix t -> Vector t
+mapCols0 fx mat = unsafePerformIO $ G.forM idxV $ \idx -> return $ fx $ getVecs idx mat
+  where idxV = fromList [0..(cols mat)-1]
+        getVecs idx mat = flatten $ takeColumns 1 $ dropColumns idx mat
+  
+mapRows1 :: (Element t) => (Vector t -> Vector t -> Vector t) -> Vector t -> Matrix t -> Matrix t
+mapRows1 fx vec mat = fromRows $ unsafePerformIO $ CM.forM idxL $ \idx -> return $ fx vec $ getVecs idx mat
+  where idxL = [0..(rows mat)-1]
+        getVecs idx mat = flatten $ takeRows 1 $ dropRows idx mat
+
+mapCols1 :: (Element t) => (Vector t -> Vector t -> Vector t) -> Vector t -> Matrix t -> Matrix t
+mapCols1 fx vec mat = fromColumns $ unsafePerformIO $ CM.forM idxL $ \idx -> return $ fx vec $ getVecs idx mat
+  where idxL = [0..(cols mat)-1]
+        getVecs idx mat = flatten $ takeColumns 1 $ dropColumns idx mat
+
--- HasKAL/src/HasKAL/Misc/UMatrixMapping.hs
+++ HasKAL/src/HasKAL/Misc/UMatrixMapping.hs
@@ -1,30 +1,31 @@
 {-******************************************
-  *     File Name: MatrixSupplement.hs
+  *     File Name: UMatrixMapping.hs
   *        Author: Takahiro Yamamoto
-  * Last Modified: 2014/12/04 14:20:37
+  * Last Modified: 2014/12/05 16:29:05
   *******************************************-}
 
-module MatrixSupplement (
+module UMatrixMapping (
    convertS2U
   ,convertU2S
   ,mapRows0
-  ,mapColumns0
+  ,mapCols0
   ,mapRows1
-  ,mapColumns1
+  ,mapCols1
 ) where
 
-
--- Storable type
-import qualified Data.Packed.Matrix as M
-
--- Unbox type
-import Data.Vector.Unboxed
-import Data.Matrix.Unboxed
 
 import qualified Control.Monad as CM (forM)
 import System.IO.Unsafe (unsafePerformIO)
 
--- convert type
+{-- Unbox type --}
+import Data.Vector.Unboxed
+import Data.Matrix.Unboxed
+
+{-- Storable type --}
+import qualified Data.Packed.Matrix as M
+
+
+{-- matrix type converter --}
 convertS2U :: (M.Element a, Unbox a) => M.Matrix a -> Matrix a
 convertS2U mat = fromVector rowNum colNum $ convert $ M.flatten mat
   where rowNum = M.rows mat
@@ -35,21 +36,21 @@ convertU2S mat = M.reshape colNum $ convert $ flatten mat
   where colNum = cols mat
 
 
--- map for Matrix
+{-- map functions --}
 mapRows0 :: (Unbox a) => (Vector a -> a) -> Matrix a -> Vector a
 mapRows0 fx mat = unsafePerformIO $ forM idxV $ \idx -> return $ fx $ takeRow idx mat
   where idxV = fromList [0..(rows mat)-1]
 
-mapColumns0 :: (Unbox a) => (Vector a -> a) -> Matrix a -> Vector a
-mapColumns0 fx mat = unsafePerformIO $ forM idxV $ \idx -> return $ fx $ takeColumn idx mat
+mapCols0 :: (Unbox a) => (Vector a -> a) -> Matrix a -> Vector a
+mapCols0 fx mat = unsafePerformIO $ forM idxV $ \idx -> return $ fx $ takeColumn idx mat
   where idxV = fromList [0..(cols mat)-1]
 
 mapRows1 :: (Unbox a) => (Vector a -> Vector a -> Vector a) -> Vector a -> Matrix a -> Matrix a
 mapRows1 fx vec mat = fromRows $ unsafePerformIO $ CM.forM idxL $ \idx -> return $ fx vec $ takeRow idx mat
   where idxL = [0..(rows mat)-1]
 
-mapColumns1 :: (Unbox a) => (Vector a -> Vector a -> Vector a) -> Vector a -> Matrix a -> Matrix a
-mapColumns1 fx vec mat = fromColumns $ unsafePerformIO $ CM.forM idxL $ \idx -> return $ fx vec $ takeColumn idx mat
+mapCols1 :: (Unbox a) => (Vector a -> Vector a -> Vector a) -> Vector a -> Matrix a -> Matrix a
+mapCols1 fx vec mat = fromColumns $ unsafePerformIO $ CM.forM idxL $ \idx -> return $ fx vec $ takeColumn idx mat
   where idxL = [0..(cols mat)-1]
         
         
