cb879c40ec7656756d244585e908fdd65dcb34e6
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-02-13 00:32:43 +0900

    modified the PSO code

--- attic/pso/test_PSO.hs
+++ attic/pso/test_PSO.hs
@@ -16,24 +16,35 @@ main = do
       m = 3 :: Int
       i0 = 10 :: Int
 
+      -- | create initial data
       initdata = map (go i0) [1..m]
         where
-        go n i = ((n, i), x, v, lr)
-        x = map (\_->unsafePerformIO $ getStdRandom $ randomR (-5, 5) :: Double) [1..d]
-        v = map (\_->unsafePerformIO $ getStdRandom $ randomR (-1, 1) :: Double) [1..d]
-        lr = likelihood x
-      p = concat [x|(_, x, _, _) <- initdata]
-      (_, g, _, _) = maxList initdata
+        go n i = ((n, i), x, v, p, lr)
+          where
+          x = map (\_->unsafePerformIO $ getStdRandom $ randomR (-5, 5) :: Double) [1..d]
+          v = map (\_->unsafePerformIO $ getStdRandom $ randomR (-1, 1) :: Double) [1..d]
+          lr = likelihood x
+          p = x
+      (_, g, _, _, _) = maxList initdata
 
-  return $ pso i0 m d w c1 c2 initdata p g
+  return $ pso i0 m d w c1 c2 initdata g
 
-
-pso 0 _ _ _ _ _ _ _ _ = []
-pso i0 m d w c1 c2 pdata p g = do
-  let updatedpdata = (map (update (i0-1) pdata) [1..m]) ++ pdata
-      update n dat i = ((n, i), updatedx, updatedv, newl)
+-- | perform particle swarm optimization
+pso :: Int
+    -> Int
+    -> Int
+    -> Double
+    -> Double
+    -> Double
+    -> [((Int, Int), [Double], [Double], [Double], Double)]
+    -> [Double]
+    -> [((Int, Int), [Double], [Double], [Double], Double)]
+pso 0 _ _ _ _ _ _ _ = []
+pso i0 m d w c1 c2 pdata g = do
+  let updatedpdata = (map (update (i0-1) pdata) [1..m])
+      update n dat i = ((n, i), updatedx, updatedv, updatedp, newl)
         where
-        (_, x, v, _) = dat!!0
+        (_, x, v, p, _) = singleList [((n, i), a, b, c, d)|((n', i'), a, b, c, d)<-dat,i'==i, n'==n]
         updatedv = map (\i->do
                        let r1 = unsafePerformIO $ getStdRandom $ randomR (0, 1) :: Double
                            r2 = unsafePerformIO $ getStdRandom $ randomR (0, 1) :: Double
@@ -41,12 +52,11 @@ pso i0 m d w c1 c2 pdata p g = do
                         [0..d-1]
         updatedx = map (\i->x!!i + updatedv!!i) [0..d-1]
         newl = likelihood updatedx
-      updatedp = concat $ map (\i->do
-        let (_, x, _, _) = maxList [((a, ind), b, c, d)|((a, ind), b, c, d) <- updatedpdata, ind==i]
-         in x)
-         [1..m]
-      (_,  updatedg,  _,  _) = maxList updatedpdata
-  pso (i0-1) m d w c1 c2 updatedpdata updatedp updatedg
+        (_, _, _, updatedp, _) = maxList [((n', ind), a, b, c, d)
+                            |((n', ind), a, b, c, d) <- updatedpdata, ind==i
+                            ]
+      (_,  updatedg,  _,  _, _) = maxList updatedpdata
+   in  updatedpdata ++ pso (i0-2) m d w c1 c2 updatedpdata updatedg
 
 -- | likelihood function
 -- |
@@ -55,23 +65,30 @@ likelihood x = ((x!!0)-0.5)**2 + ((x!!1)-0.5)**2
 
 -- | get (max value, index) from comaring two values
 -- |
-maxWindx :: ((Int, Int), [Double], [Double], Double)
-         -> ((Int, Int), [Double], [Double], Double)
-         -> ((Int, Int), [Double], [Double], Double)
-maxWindx (sa, xa, va, a) (sb, xb, vb, b)
-  | a > b = (sa, xa, va, a)
-  | a < b = (sb, xb, vb, b)
-  | a == b = (sa, xa, va, a)
+maxWindx :: ((Int, Int), [Double], [Double], [Double], Double)
+         -> ((Int, Int), [Double], [Double], [Double], Double)
+         -> ((Int, Int), [Double], [Double], [Double], Double)
+maxWindx (sa, xa, va, pa, a) (sb, xb, vb, pb, b)
+  | a > b = (sa, xa, va, pa, a)
+  | a < b = (sb, xb, vb, pb, b)
+  | a == b = (sa, xa, va, pa, a)
   | otherwise = error "check input type"
 
 -- | get (max value, index) in a list
 -- |
-maxList :: [((Int, Int), [Double], [Double], Double)]
-        -> ((Int, Int), [Double], [Double], Double)
+maxList :: [((Int, Int), [Double], [Double], [Double], Double)]
+        -> ((Int, Int), [Double], [Double], [Double], Double)
 maxList [] = error "empty"
 maxList [x] = x
 maxList (x:xs) = maxWindx x (maxList xs)
 
+-- | add element to a list
+addElem :: ((Int,Int), [[Double]]) -> [((Int,Int), [[Double]])] -> [((Int,Int), [[Double]])]
+addElem x xs = undefined
 
+-- | one element list to a value
+singleList :: [((Int, Int), [Double], [Double], [Double], Double)]
+           -> ((Int, Int), [Double], [Double], [Double], Double)
+singleList [x] = x
 
 
