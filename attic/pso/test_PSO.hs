97b392ceaf53c0ce7dd28234f2385458f37348c3
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-02-16 01:59:26 +0900

    rewrited the pso code using State Monad

--- attic/pso/test_PSO.hs
+++ attic/pso/test_PSO.hs
@@ -2,19 +2,22 @@
 -- ( maxWindx
 -- , maxList
 -- ) where
-
+import Control.Monad.State
 import System.Random
 import System.IO.Unsafe (unsafePerformIO)
 
-main :: IO ([((Int, Int), [Double], [Double], [Double], Double)], [(Int, [Double])])
-main = do
+type Particle = ((Int,  Int),  [Double],  [Double],  [Double],  Double)
+type GlobalParticle = (Int, [Double])
 
+
+main :: IO ([Particle], [GlobalParticle])
+main = do
   let w = 1 :: Double
       c1 = 2.0 :: Double
       c2 = 2.0 :: Double
       d = 2 :: Int
       m = 3 :: Int
-      i0 = 10 :: Int
+      i0 = 3 :: Int
 
       -- | create initial data
       initdata = map (go i0) [1..m]
@@ -25,9 +28,9 @@ main = do
           v = map (\_->unsafePerformIO $ getStdRandom $ randomR (-1, 1) :: Double) [1..d]
           lr = likelihood x
           p = x
-      (_, gval, _, _, _) = maxList initdata
-      g = [(i0, gval)]
-  return $ pso i0 m d w c1 c2 initdata g
+  return $ pso i0 m d w c1 c2 initdata
+
+
 
 -- | perform particle swarm optimization
 pso :: Int    -- ^ Max iteration number
@@ -36,41 +39,58 @@ pso :: Int    -- ^ Max iteration number
     -> Double -- ^ inertia weight parameter
     -> Double -- ^ cognitive weight
     -> Double -- ^ social weight
-    -> [((Int, Int), [Double], [Double], [Double], Double)] -- ^ particle data
-    -> [(Int, [Double])] -- ^ global best position of each particle
-    -> ([((Int, Int), [Double], [Double], [Double], Double)], [(Int, [Double])])
+    -> [Particle] -- ^ particle data
+    -> ([Particle], [GlobalParticle])
        -- ^ output : updated particle data
-pso 0 _ _ _ _ _ _ _ = ([], [])
-pso n m d w c1 c2 pdata g = do
-  let updatedpdata = map (update n pdata) [1..m]
-      update n dat i = ((n, i), updatedx, updatedv, updatedp, newl)
-        where
-        (_, x, v, p, _) = singleList [((n, i), a, b, c, d)|((n', i'), a, b, c, d)<-dat,i'==i, n'==n]
-        g' = snd(g!!0)
-        updatedv = map (\i->do
-                       let r1 = unsafePerformIO $ getStdRandom $ randomR (0, 1) :: Double
-                           r2 = unsafePerformIO $ getStdRandom $ randomR (0, 1) :: Double
-                        in w*v!!i + c1*r1*(p!!i-x!!i) + c2*r2*(g'!!i-x!!i))
-                        [0..d-1]
-        updatedx = map (\i->x!!i + updatedv!!i) [0..d-1]
-        newl = likelihood updatedx
-        (_, _, _, updatedp, _) = maxList [((n', ind), a, b, c, d)
-                            |((n', ind), a, b, c, d) <- updatedpdata, ind==i
-                            ]
-      (_,  updatedgval,  _,  _, _) = maxList updatedpdata
-      updatedg = (n, updatedgval):g :: [(Int, [Double])]
-   in ( fst (pso (n-1) m d w c1 c2 updatedpdata updatedg) ++ pdata
-      , snd (pso (n-1) m d w c1 c2 updatedpdata updatedg) ++ g
-      )
+pso n m d w c1 c2 pdata = runState go g
+  where
+    (_, gval, _, _, _) = maxList pdata
+    g = [(n, gval)]
+    go = psoState n m d w c1 c2 pdata  :: State [GlobalParticle] [Particle]
+
+psoState :: Int
+         -> Int
+         -> Int
+         -> Double
+         -> Double
+         -> Double
+         -> [Particle]
+         -> State [GlobalParticle] [Particle]
+psoState n m d w c1 c2 pdata = do
+  g <- get
+  let phist= concat $ for [n, n-1..0] $ \n' -> do
+      concat $ for [1..m] $ \i -> do
+        let (_, x, v, p, _) = singleList [ ((n, j), a, b, c, d)
+                                         | ((n, j), a, b, c, d)<-pdata, j==i, n==n'
+                                         ]
+        let g' = snd (g!!0)
+            updatedv = map (\i->do
+              let r1 = unsafePerformIO $ getStdRandom $ randomR (0, 1) :: Double
+                  r2 = unsafePerformIO $ getStdRandom $ randomR (0, 1) :: Double
+               in w*v!!i + c1*r1*(p!!i-x!!i) + c2*r2*(g'!!i-x!!i)
+              )[0..d-1]
+            updatedx = map (\i->x!!i + updatedv!!i) [0..d-1]
+            newl = likelihood updatedx
+            (_, _, _, updatedp, _) = maxList [pdata!!0, ((n', i),updatedx,updatedv,updatedx,newl)]
+            values = ((n', i), updatedx, updatedv, updatedp, newl)
+        (values:pdata)
+  let (_, updatedgval, _, _, _) = maxList phist
+  put ((n, updatedgval):g)
+  state $ \s ->  (phist, s)
+
+
+-- | define for
+for = flip map
+
 
 -- | likelihood function
 likelihood :: [Double] -> Double
 likelihood x = ((x!!0)-0.5)**2 + ((x!!1)-0.5)**2
 
 -- | get (max value, index) from comaring two values
-maxWindx :: ((Int, Int), [Double], [Double], [Double], Double)
-         -> ((Int, Int), [Double], [Double], [Double], Double)
-         -> ((Int, Int), [Double], [Double], [Double], Double)
+maxWindx :: Particle
+         -> Particle
+         -> Particle
 maxWindx (sa, xa, va, pa, a) (sb, xb, vb, pb, b)
   | a > b = (sa, xa, va, pa, a)
   | a < b = (sb, xb, vb, pb, b)
@@ -78,15 +98,16 @@ maxWindx (sa, xa, va, pa, a) (sb, xb, vb, pb, b)
   | otherwise = error "check input type"
 
 -- | get (max value, index) in a list
-maxList :: [((Int, Int), [Double], [Double], [Double], Double)]
-        -> ((Int, Int), [Double], [Double], [Double], Double)
+maxList :: [Particle]
+        -> Particle
 maxList [] = error "empty"
 maxList [x] = x
 maxList (x:xs) = maxWindx x (maxList xs)
 
 -- | one element list to a value
-singleList :: [((Int, Int), [Double], [Double], [Double], Double)]
-           -> ((Int, Int), [Double], [Double], [Double], Double)
+singleList :: [Particle]
+           -> Particle
+singleList [] = error "empty"
 singleList [x] = x
-
+singleList _ = error "not correct"
 
