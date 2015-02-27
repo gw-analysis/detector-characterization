a755c0398ac11f7acde8a80b6fc8d238fd2ba1ce
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2015-02-27 15:38:21 +0900

    added test code for FrameUtils and PlotUtils

--- attic/HROOT_TApplication/plotTest.hs
+++ attic/HROOT_TApplication/plotTest.hs
@@ -1,31 +1,13 @@
 {-******************************************
   *     File Name: plotTest.hs
   *        Author: Takahiro Yamamoto
-  * Last Modified: 2014/10/04 21:18:08
+  * Last Modified: 2015/02/27 15:37:05
   *******************************************-}
 
-import qualified HasKAL.PlotUtils.HROOT.PlotGraph as PM
-import qualified HasKAL.PlotUtils.HROOT.PlotGraph3D as PM3
-import qualified HasKAL.PlotUtils.HROOT.Histogram as H
-import qualified HasKAL.PlotUtils.HROOT.AppendFunctionHROOT as HAF
+import HasKAL.PlotUtils.HROOT.PlotGraph (easyPlotX, LogOption(Linear))
+import HasKAL.FrameUtils.Function (readFrame)
 
 main :: IO ()
 main = do
-  let xs = [1.0..50]
-      ys = reverse xs
-      range = ((0,0),(0,20))
-      ranges = [((-10,60),(-10,60)),((10,30),(10,30)),((10,50),(20,40))]
-
-  HAF.addSignalHandle
-  PM.oPlotX PM.Linear PM.LinePoint 2 ("X jiku","Y jiku") 0.05 "Daimei" range $ [zip xs xs, zip xs ys] 
-  -- PM.dPlotX PM.Linear PM.LinePoint 2 (repeat ("X jiku","Y jiku")) 0.06 (repeat "Daimei") ranges $ [zip xs xs, zip xs xs, zip xs xs]
-
-timeFreqData :: [Double] -> [Double] -> [[Double]] -> [(Double, Double, Double)]
-timeFreqData [] _ _ = []
-timeFreqData _ [] _ = []
-timeFreqData _ _ [] = []
-timeFreqData (x:xs) ys (zs:zss) = zip3 [x,x..] ys zs ++ timeFreqData xs ys zss
-
-mapWith :: (a -> b -> c) -> [a] -> [b] -> [[c]]
-mapWith f [] _ = []
-mapWith f (x:xs) ys = map (f x) ys : mapWith f xs ys
+  xs <- readFrame "L1:LOSC-STRAIN" "/data/L-L1_LOSC_4_V1-842743808-4096.gwf"
+  easyPlotX Linear $ take 1000 $ zip [1..] xs
