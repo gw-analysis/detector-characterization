52b08f6a84aa972d12e221306dd387368f29b233
KAGRA DetChar <kagra.detchar@gmail.com>
2015-04-30 20:36:15 +0900

    watcheNewfile takes a function to run for a new gwf file

--- HasKAL/src/HasKAL/WebUtils/FileWatcher.hs
+++ HasKAL/src/HasKAL/WebUtils/FileWatcher.hs
@@ -8,16 +8,16 @@ import System.Exit
 import System.IO.Error
 import Filesystem.Path
 import Filesystem.Path.CurrentOS (decodeString, encodeString)
-import HasKAL.WebUtils.EventDisplayXend
-import HasKAL.WebUtils.Data
+--import HasKAL.WebUtils.EventDisplayXend
+--import HasKAL.WebUtils.Data
 import System.Process (rawSystem)
 
-watchNewfile :: Param -> String -> String -> IO ()
-watchNewfile param webhomedir watchdir = do
+watchNewfile :: String -> String -> String -> IO ()
+watchNewfile f webhomedir watchdir = do
 --  dir <- getWorkingDirectory
   withManager $ \manager -> do
     watchDir manager (decodeString watchdir) (const True)
-      $ \event -> do rawSystem "./envDisplay" [webhomedir, encodeString $ eventPath event]
+      $ \event -> do rawSystem f [webhomedir, encodeString $ eventPath event]
                      print "event display updated."
 --      $ \event -> channelPlot' param (encodeString $ eventPath event) >>= genWebPage'
 
