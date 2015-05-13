41e9c62d2934c66123bdf3949615ee5f40290b4b
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2015-05-13 23:28:27 +0900

    update cgi

--- attic/CGI/pastDataViewer.cgi.hs
+++ attic/CGI/pastDataViewer.cgi.hs
@@ -8,10 +8,17 @@ Stability   : test
 Portability : POSIX
 GUI of Antenna Pattern
 -}{-
-  * Last Modified: 2015/05/07 23:34:04
+  * Last Modified: 2015/05/13 23:21:58
 -}
 
 import Network.CGI
+--import HasKAL.TimeUtils.GPSfunction
+
+pngpath :: String
+pngpath = "http://seikai.hep.osaka-cu.ac.jp/~chino/env_images/"
+
+cgihome :: String
+cgihome = "http://seikai.hep.osaka-cu.ac.jp/~chino/cgi-bin/pastDataViewer.cgi"
 
 inputForm :: String -> String
 inputForm script = concat [
@@ -19,47 +26,56 @@ inputForm script = concat [
   "<h1>Past Data Viewer</h1>",
   "<p>GPS Time: <input type=\"text\" name=\"gps\" value=\"1115040505\" /></p>",
   "<p> Channel: </p>",
-  "<p><input type=\"radio\" name=\"channel\" value=\"K1:PEM-EX_ACC_NO2_X_FLOOR\">K1:PEM-EX_ACC_NO2_X_FLOOR</p>",
-  "<p><input type=\"radio\" name=\"channel\" value=\"K1:PEM-EX_ACC_NO2_Y_FLOOR\">K1:PEM-EX_ACC_NO2_Y_FLOOR</p>",
-  "<p><input type=\"radio\" name=\"channel\" value=\"K1:PEM-EX_ACC_NO2_Z_FLOOR\">K1:PEM-EX_ACC_NO2_Z_FLOOR</p>",
-  "<p><input type=\"radio\" name=\"channel\" value=\"K1:PEM-EX_MAG_X_FLOOR\">K1:PEM-EX_MAG_X_FLOOR</p>",
-  "<p><input type=\"radio\" name=\"channel\" value=\"K1:PEM-EX_MAG_Y_FLOOR\">K1:PEM-EX_MAG_Y_FLOOR</p>",
-  "<p><input type=\"radio\" name=\"channel\" value=\"K1:PEM-EX_MAG_Z_FLOOR\">K1:PEM-EX_MAG_Z_FLOOR</p>",
-  "<p><input type=\"radio\" name=\"channel\" value=\"K1:PEM-EX_MIC_FLOOR\">K1:PEM-EX_MIC_FLOOR</p>",    
+  "<p><input type=\"checkbox\" name=\"channel\" value=\"K1:PEM-EX_ACC_NO2_X_FLOOR\">K1:PEM-EX_ACC_NO2_X_FLOOR</p>",
+  "<p><input type=\"checkbox\" name=\"channel\" value=\"K1:PEM-EX_ACC_NO2_Y_FLOOR\">K1:PEM-EX_ACC_NO2_Y_FLOOR</p>",
+  "<p><input type=\"checkbox\" name=\"channel\" value=\"K1:PEM-EX_ACC_NO2_Z_FLOOR\">K1:PEM-EX_ACC_NO2_Z_FLOOR</p>",
+  "<p><input type=\"checkbox\" name=\"channel\" value=\"K1:PEM-EX_MAG_X_FLOOR\">K1:PEM-EX_MAG_X_FLOOR</p>",
+  "<p><input type=\"checkbox\" name=\"channel\" value=\"K1:PEM-EX_MAG_Y_FLOOR\">K1:PEM-EX_MAG_Y_FLOOR</p>",
+  "<p><input type=\"checkbox\" name=\"channel\" value=\"K1:PEM-EX_MAG_Z_FLOOR\">K1:PEM-EX_MAG_Z_FLOOR</p>",
+  "<p><input type=\"checkbox\" name=\"channel\" value=\"K1:PEM-EX_MIC_FLOOR\">K1:PEM-EX_MIC_FLOOR</p>",    
   "<input type=\"submit\" value=\"view\" />",
   "</form>"]
 
 putName :: String -> String -> String
-putName gps channel = concat ["<p>GPS Time: ", gps, "</p>",
-                              "<p> Channel: ", channel, "</p>",
+putName gps channel = concat ["<Hr><h3> Channel: ", channel, "</h3>",
                               "<a href=\"", pngpath, channel, "_TS-", gps, "-32.png\">",
                               "<img alt=\"\" src=\"", pngpath, channel, "_TS-", gps, "-32.png\"",
-                              "style=\"border: 0px solid ; width: 300px;\"></a>",
+                              imgstyle,
                               "<a href=\"", pngpath, channel, "_PSD-", gps, "-32.png\">",
                               "<img alt=\"\" src=\"", pngpath, channel, "_PSD-", gps, "-32.png\"",
-                              "style=\"border: 0px solid ; width: 300px;\"></a>",
+                              imgstyle,
                               "<a href=\"", pngpath, channel, "_SPE-", gps, "-32.png\">",
                               "<img alt=\"\" src=\"", pngpath, channel, "_SPE-", gps, "-32.png\"",
-                              "style=\"border: 0px solid ; width: 300px;\"></a>"
+                              imgstyle,"<br><br><br>"
                              ]
-  where pngpath = "png/"
+  where imgstyle = "style=\"border: 0px solid ; width: 300px;\"></a>"
 
-body :: Maybe String -> Maybe String -> String -> String
-body gps channel script =
-    case (gps, channel) of
+
+putNames :: String -> [String] -> String
+putNames gps channels = header ++ (concat $ map (putName gps) channels) ++ footer
+  where header = concat ["<h1>Past Data Viewer</h1>", "<h2>GPS Time: ", gps, "</h2>"]
+        footer = concat ["<Hr>[<a href=\"", cgihome, "?gps=", (show $ (read gps) - 32), uriCh, "\">&lt; Prev</a>] ",
+                         " [<a href=\"", cgihome, "\">Back</a>] ",
+                         " [<a href=\"", cgihome, "?gps=", (show $ (read gps) + 32), uriCh, "\">Next &gt;</a>]"
+                        ]
+        uriCh = concat $ zipWith (++) (repeat "&channel=") channels
+                 
+body :: Maybe String -> [String] -> String -> String
+body gps channels script =
+    case (gps, channels) of
      (Just "", _) -> inputForm script
-     (_, Just "") -> inputForm script     
-     (Just x, Just y)  -> putName x y
+     (_, []) -> inputForm script
+     (Just x, y)  -> putNames x y
      (_, _) -> inputForm script
 
-html :: Maybe String -> Maybe String -> String -> String
-html gps channel script = concat [
+html :: Maybe String -> [String] -> String -> String
+html gps channels script = concat [
   "<html>",
   "<head><title>Past Data Viewer</title>",
   "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">",
   "</head>",
   "<body>",
-  body gps channel script,
+  body gps channels script,
   "</body>",
   "</html>"]
 
@@ -68,8 +84,8 @@ cgiMain = do
   setHeader "Content-type" "text/html; charset = UTF-8"
   script <- scriptName
   gps <- getInput "gps"
-  channel <- getInput "channel"
-  output $ html gps channel script
+  channels <- getMultiInput "channel"
+  output $ html gps channels script
 
 main :: IO ()
 main = runCGI (handleErrors cgiMain)
