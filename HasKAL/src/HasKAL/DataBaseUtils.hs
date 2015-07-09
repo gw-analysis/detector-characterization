2f10c9b0c0ee2b2fedd5a6829536ccaadadf040a
KAGRA DetChar <kagra.detchar@gmail.com>
2015-07-09 19:48:32 +0900

    added DataBaseUtils related

---
+++ HasKAL/src/HasKAL/DataBaseUtils.hs
@@ -0,0 +1,11 @@
+module HasKAL.DataBaseUtils
+( module Exports)
+where
+import HasKAL.DataBaseUtils.DataBaseAdmin as Exports
+import HasKAL.DataBaseUtils.DataSource as Exports
+import HasKAL.DataBaseUtils.Framedb as Exports
+import HasKAL.DataBaseUtils.Function as Exports
+--import HasKAL.DataBaseUtils.UpdateFrameDB as Exports
+
+
+
