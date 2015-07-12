9b473e4032304eab7f79857a8495ca4f7ee61bee
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2015-07-12 17:47:08 +0900

    added CGI script for multi-channel analysis

---
+++ attic/CGI/multiChannelViewer.cgi.hs
@@ -0,0 +1,146 @@
+{- |
+Module      : multiChannelViewer.cgi
+Description : This is documentation tests.
+Copyright   : (c) WhoAmI, 2014
+License     : ???
+Maintainer  : Takahiro Yamamoto %mail%
+Stability   : test
+Portability : POSIX
+
+-}{-
+  * Last Modified: 2015/07/12 17:27:42
+-}
+
+import Network.CGI
+import System.Directory (doesFileExist)
+import System.IO.Unsafe (unsafePerformIO)
+import Control.Monad (forM_, liftM)
+import Data.Maybe (fromJust)
+import Data.Vector.Storable (toList)
+
+import HasKAL.DataBaseUtils.Function
+import HasKAL.FrameUtils.FrameUtils
+import HasKAL.TimeUtils.GPSfunction
+import HasKAL.MonitorUtils.CorrelationMon.CalCorrelation
+import HasKAL.MonitorUtils.CorrelationMon.CorrelationMethod
+import HasKAL.MonitorUtils.CoherenceMon.Function
+import HasKAL.PlotUtils.HROOT.PlotGraph
+import CommonForm
+
+pngpath :: String
+pngpath = "../mon_images/"
+
+xendCh :: [String]
+xendCh = ["K1:PEM-EX_ACC_NO2_X_FLOOR"
+         ,"K1:PEM-EX_ACC_NO2_Y_FLOOR"
+         ,"K1:PEM-EX_ACC_NO2_Z_FLOOR"
+         ,"K1:PEM-EX_MAG_X_FLOOR"
+         ,"K1:PEM-EX_MAG_Y_FLOOR"
+         ,"K1:PEM-EX_MAG_Z_FLOOR"
+         ,"K1:PEM-EX_MIC_FLOOR"
+         ]
+
+inputForm :: String -> String
+inputForm script = concat [
+  "<form action=\"", script, "\" method=\"GET\">",
+  dateForm,
+  channelForm Both xendCh,
+  "<h3>Method: </h3>",
+  "<input type=\"checkbox\" name=\"method\" value=\"Coh\" checked=\"checked\"> Coherence</p>",
+  "<input type=\"checkbox\" name=\"method\" value=\"Peason\"> Peason</p>",
+  "<input type=\"checkbox\" name=\"method\" value=\"MIC\"> MIC</p>",
+  "<input type=\"submit\" value=\"view\" />",
+  "</form>"]
+
+putName :: String -> [String] -> String -> String -> String
+putName gps methods channel1 channel2 = concat [
+  "<Hr><h3> Channel: ", channel2, "</h3>",
+  (concat $ map func methods),
+  "<br><br><br>"]
+  where func s = concat [
+          "<nobr><a href=\"", pngpath, channel1, "--", channel2, "_", s,"-", gps, "-", "32", ".png\">",
+          "<img alt=\"\" src=\"", pngpath, channel1, "--", channel2, "_", s, "-", gps, "-", "32", ".png\"",
+          "style=\"border: 0px solid ; width: 300px;\"></a>", "</nobr>"]
+
+putNames :: String -> [String] -> String -> [String] -> String
+putNames gps methods channel1 channels2 = concat [
+  "<h2>GPS Time: ", gps, "</h2><h2>vs ", channel1, "</h2>",
+  (concat $ map (putName gps methods channel1) channels2),
+  "<Hr>[<a href=\"./cormon.cgi?gps=", (show $ (read gps) - 32), uris, "\">&lt; Prev</a>] ",
+  " [<a href=\"./multiChannelViewer.cgi\">Back</a>] ",
+  " [<a href=\"./multiChannelViewer.cgi?Date=GPS&gps=", (show $ (read gps) + 32), uris, "\">Next &gt;</a>]"  
+  ]
+  where uris = "&channel1="++channel1
+               ++ (concat $ zipWith (++) (repeat "&channel2=") channels2)
+               ++ (concat $ zipWith (++) (repeat "&method=") methods)
+
+                 
+corMain :: String -> [String] -> String -> String -> IO ()
+corMain gps methods ch1 ch2 = do
+  datMaybe1 <- kagraDataGet (read gps) (read "32") ch1
+  datMaybe2 <- kagraDataGet (read gps) (read "32") ch2
+  case (datMaybe1, datMaybe2) of
+   (Nothing, _) -> return ()
+   (_, Nothing) -> return ()
+   (_, _) -> do
+     let dat1 = fromJust datMaybe1
+         dat2 = fromJust datMaybe2
+     fs1 <- (`getSamplingFrequency` ch1) =<< liftM (head.fromJust) (kagraDataFind (read gps) 1 ch1)
+     fs2 <- (`getSamplingFrequency` ch2) =<< liftM (head.fromJust) (kagraDataFind (read gps) 1 ch2)
+     forM_ methods $ \method -> do
+       pngExits <- doesFileExist $ pngpath++ch1++"--"++ch2++"_"++method++"-"++gps++"-"++"32"++".png"
+       case (pngExits, fs1==fs2) of
+        (True, _) -> return ()
+        (False, False) -> return ()
+        (False, True) -> do
+          case method of
+           "Coh" -> do
+             let coh'f = coherenceMon (truncate fs1) fs1 dat1 dat2
+             plotV Linear LinePoint 1 BLUE ("[Hz]", "|coh(f)|^2") 0.05 method 
+               (pngpath++ch1++"--"++ch2++"_"++method++"-"++gps++"-"++"32.png") ((0,0),(0,0)) $ coh'f
+           "Peason" -> do
+             let cor = takeCorrelation (read method) (toList dat1) (toList dat2) 16
+             plot Linear LinePoint 1 BLUE ("[sec]", "correlation") 0.05 method 
+               (pngpath++ch1++"--"++ch2++"_"++method++"-"++gps++"-"++"32.png") ((0,0),(0,0)) $ zip [0,1/fs1..] cor
+           "MIC" -> do
+             let cor = takeCorrelation (read method) (toList dat1) (toList dat2) 16
+             plot Linear LinePoint 1 BLUE ("[sec]", "correlation") 0.05 method 
+               (pngpath++ch1++"--"++ch2++"_"++method++"-"++gps++"-"++"32.png") ((0,0),(0,0)) $ zip [0,1/fs1..] cor
+
+body :: Maybe String -> [String] -> Maybe String -> [String] -> String -> String
+body gps methods channel1 channels2 script =
+    unsafePerformIO $ case (gps, methods ,channel1, channels2) of
+                       (Just "", _, _, _) -> return $ inputForm script
+                       (_, [], _, _) -> return $ inputForm script
+                       (_, _, Just "", _) -> return $ inputForm script
+                       (_, _, _, []) -> return $ inputForm script
+                       (Just x, y, Just z, w)  -> do
+                         mapM_ (corMain x y z) w
+                         return $ putNames x y z w
+                       (_, _, _, _) -> return $ inputForm script
+
+cgiMain :: CGI CGIResult
+cgiMain = do
+  setHeader "Content-type" "text/html; charset = UTF-8"
+  script <- scriptName
+  date <- getInput "Date"
+  gps <- case date of
+    Just "GPS" -> getInput "gps"
+    Just "Local" -> do
+      year <- getInput "year"
+      month <- getInput "month"
+      day <- getInput "day"
+      hour <- getInput "hour"
+      minute <- getInput "minute"
+      second <- getInput "second"
+      local <- getInput "local"
+      return $ Just $ time2gps $ (fromJust year)++"-"++(fromJust month)++"-"++(fromJust day)++" "
+        ++(fromJust hour)++":"++(fromJust minute)++":"++(fromJust second)++" "++(fromJust local)
+    _ -> return $ Just ""
+  channel1 <- getInput "channel1"
+  channels2 <- getMultiInput "channel2"
+  methods <- getMultiInput "method"
+  output $ hkalFrame "Multi Channel Viewer" $ body gps methods channel1 channels2 script
+
+main :: IO ()
+main = runCGI (handleErrors cgiMain)
