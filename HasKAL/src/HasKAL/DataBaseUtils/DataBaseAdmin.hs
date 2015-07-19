4539088821c68a2a61717b8f6078e5e90c60944b
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-07-19 10:10:15 +0900

    resource-safety eddition of DataBaseAdmin.hs

--- HasKAL/src/HasKAL/DataBaseUtils/DataBaseAdmin.hs
+++ HasKAL/src/HasKAL/DataBaseUtils/DataBaseAdmin.hs
@@ -7,45 +7,77 @@ module HasKAL.DataBaseUtils.DataBaseAdmin
 ( updateFrameDB
 ) where
 
-
-import Database.Relational.Query (relation, value, InsertQuery, derivedInsertQuery, (|$|), (|*|), Pi, typedInsert, Insert)
-import Database.HDBC.Session (withConnectionIO, handleSqlError')
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Resource
+import Data.Conduit
+import qualified Data.Conduit.List as CL
+import Data.Int (Int32)
+import Data.List (isInfixOf,  (!!))
+import Database.HDBC.Session (withConnectionIO', withConnectionIO)
 import Database.HDBC.Record.Query (runQuery')
 import Database.HDBC (quickQuery', runRaw, fromSql, rollback)
 import Database.HDBC.Record (runInsertQuery, runInsert)
 import Database.HDBC.Query.TH
-
-import HasKAL.FrameUtils.FrameUtils (getGPSTime, getChannelList, getSamplingFrequency)
-import HasKAL.Misc.StrictMapping (forM')
-import Data.List (isInfixOf, (!!))
-import Data.Int (Int32)
-
-import Control.Monad (forM_)
-import HasKAL.DataBaseUtils.DataSource (connect)
-import HasKAL.DataBaseUtils.Framedb (Framedb, framedb, tableOfFramedb)
+import Database.Relational.Query ( relation
+                                 , value
+                                 , InsertQuery
+                                 , derivedInsertQuery
+                                 , (|$|)
+                                 , (|*|)
+                                 , Pi
+                                 , typedInsert
+                                 , Insert
+                                 )
+import qualified HasKAL.DataBaseUtils.DataSource as DD
+import HasKAL.DataBaseUtils.Framedb (Framedb,  framedb,  tableOfFramedb)
 import qualified HasKAL.DataBaseUtils.Framedb as Framedb
-
+import HasKAL.FrameUtils.FrameUtils (getGPSTime, getChannelList, getSamplingFrequency)
+import System.Directory (doesFileExist)
+import qualified System.IO as IO
 import System.Process (rawSystem)
-import System.Environment (getArgs)
 
 
-updateFrameDB fname = do
-  (gpsstrt', gpsstrtnano', duration') <- getGPSTime fname
-  let gpsstrt = fromIntegral gpsstrt' :: Int32
-      duration = fromIntegral (truncate duration') :: Int32
-      gpsend = gpsstrt + duration
-  chfs <- getChannelList fname
---  getChannelList fname
-  needless <- forM_ chfs $ \(ch,  fs) -> handleSqlError' $ withConnectionIO connect $ \conn -> do
-    setSqlMode conn
---    forM_ chfs $ \(ch, fs) -> do
-    let sqlstate = insertFramedb (Just fname) (Just gpsstrt) (Just gpsend) (Just ch) (Just (truncate fs)) (Just 4)
+
+updateFrameDB fname = doesFileExist fname >>= \b ->
+  if b then withConnectionIO' DD.connect $ \conn -> do
+         runResourceT $ source fname $$ sink
+         rollback conn
+       else error "file not found."
+
+
+source = myreadFile
+
+
+sink = do
+  c <- await
+  case c of
+    Nothing -> return ()
+    Just fname -> do
+      (gpsstrt', gpsstrtnano', duration') <- liftIO $ getGPSTime fname
+      let gpsstrt = fromIntegral gpsstrt' :: Int32
+          duration = fromIntegral (truncate duration') :: Int32
+          gpsend = gpsstrt + duration
+      chfs <- liftIO $ getChannelList fname
+      liftIO $ forM_ chfs $ \(ch, fs) -> do
+        let sqlstate = insertFramedb (Just fname) (Just gpsstrt) (Just gpsend) (Just ch) (Just (truncate fs)) (Just 4)
 --      putStrLn $ "SQL: " ++ show sqlstate
-      --runInsertQuery conn sqlstate ()
-    rawSystem "mysql" ["-uroot", "-e", show sqlstate]
-    rollback conn
-  return ()
---    print num
+--      runInsertQuery conn sqlstate ()
+        rawSystem "mysql" ["-uroot", "--connect_timeout=5", "-e", show sqlstate]
+      sink
+
+
+myreadFile :: FilePath -> Source (ResourceT IO) String
+myreadFile file = bracketP
+    (do h <- IO.openFile file IO.ReadMode
+        putStrLn $ "{" ++ file ++ " open}"
+        return h )
+    (\h -> do
+        IO.hClose h
+        putStrLn $ "{" ++ file ++ " closed}" )
+    fromHandle
+  where
+    fromHandle h = forever $ liftIO (IO.hGetLine h) >>= yield
 
 
 setSqlMode conn = do
@@ -90,43 +122,5 @@ data Framedb1 = Framedb1
   , f1DqFlag :: Maybe Int32
   }
 $(makeRecordPersistableDefault ''Framedb1)
-
-
-
-
-
-
-
---  framedata :: Maybe String -> Maybe Int32 -> Maybe Int32 -> Maybe String -> Maybe Int32 -> Maybe Int32 -> Framedb1
---  framedata f t0 t1 ch fs dqid = Framedb1
---    { f1Fname = f
---    , f1GpsStart = t0
---    , f1GpsStop = t1
---    , f1Chname = ch
---    , f1SamplingRate = fs
---    , f1DqFlag = dqid
---    }
-
--- insertFramedb2 :: Insert Framedb1
--- insertFramedb2 = typedInsert tableOfFramedb piFramedb1
-
-
-
---  let chfs' = chfs !! 2
---  let ch' = fst chfs'
---      fs' = snd chfs'
---  handleSqlError' $ withConnectionIO connect $ \conn -> do
---    setSqlMode conn
---    let sqlstate = insertFramedb (Just fname) (Just gpsstrt) (Just gpsend) (Just ch') (Just (truncate fs')) (Just 4)
---    rawSystem "mysql" ["-uroot", "-e", show sqlstate]
---    --putStrLn $ "SQL: " ++ show sqlstate
--- --   runInsertQuery conn sqlstate ()
---    rollback conn
-
-
---  handleSqlError' $ withConnectionIO connect $ \conn -> do
---    setSqlMode conn
---    runInsert conn insertFramedb2 (framedata (Just fname) (Just gpsstrt) (Just gpsend) (Just ch') (Just (truncate fs')) (Just 4))
---    rollback conn
 
 
