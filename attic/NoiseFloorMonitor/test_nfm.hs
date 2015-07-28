09f657d9a78fa2f09a713539e0b5bf1245f80cd1
yokozawa <yokozawa@yukimura.hep.osaka-cu.ac.jp>
2015-07-28 14:38:55 +0900

    apply nfm to detector simulated data

--- attic/NoiseFloorMonitor/test_nfm.hs
+++ attic/NoiseFloorMonitor/test_nfm.hs
@@ -1,33 +1,86 @@
 import HasKAL.FrameUtils.FrameUtils
-import HasKAL.MonitorUtils.NoiseFloorMon.NoiseFloorMon
+import NoiseFloorMon
+import HasKAL.SimulationUtils.DetectorNoiseGenerator
+import qualified HasKAL.DetectorUtils.Detector as HDD 
+import qualified Data.Complex as DC
 
 import qualified HasKAL.ExternalUtils.GSL.RandomNumberGeneration as RNG
 import qualified HasKAL.ExternalUtils.GSL.RandomNumberDistributions as RND
 --import Data.Vector.Storable as VS
 import qualified HasKAL.Misc.StrictMapping as HMS
 
+{- For fft -}
+import Numeric.GSL.Fourier
+import Numeric.LinearAlgebra 
 main = do 
    let tsSF = 2048
-       lcsz = 32 * tsSF
+       lcsz = 32 * tsSF 
        minfreq = 64.0
        maxfreq = 128.0
        printshow xx = putStrLn $ (show (fst xx)) ++ " " ++ (show (snd xx))
    (nfmmean,nfmdev)<- estimateThreshold tsSF minfreq maxfreq lcsz
 
-   ts <- genTimeGauss lcsz
---   let frame = "K-K1_R-1113204031-32.gwf" ::String
---       chname = "ADC3"
---   fdata <- readFrame chname frame
---   print "OK"
---   let ts =  map realToFrac (eval fdata)  
-   nfmstatus<-getNoiseFloorStatus ts tsSF minfreq maxfreq (nfmmean,nfmdev)
+--   ts <- genTimeGauss lcsz 1.0
+   ts <- genTDomain lcsz tsSF 40 1024 HDD.KAGRA
+   ts2 <- genTDomain' (10*tsSF) tsSF 40 1024 HDD.KAGRA
+   let ts3 = ts ++ ts2 ++ ts
+   nfmstatus<-getNoiseFloorStatus ts3 tsSF minfreq maxfreq (nfmmean,nfmdev)
    mapM_ printshow nfmstatus
---   mapM_ print simnoise
 
-genTimeGauss :: Int->IO[Double]
-genTimeGauss lcsz = do
+
+genTimeGauss :: Int->Double->IO[Double]
+genTimeGauss lcsz sigma = do
    let simnoise = Prelude.take lcsz [1,1..] ::[Double]
    rng <- RNG.newRngWithSeed (-1)
    HMS.forM' simnoise $ \j -> do
-     gaus <- RND.gslRanGaussian rng 1.0 
+     gaus <- RND.gslRanGaussian rng sigma
      return $ gaus     
+
+genTDomain :: Int->Int->Double->Double->HDD.Detector->IO[Double]
+genTDomain ndat tsSF minfreq maxfreq ifo = do
+   let nfreq = floor (0.5*(fromIntegral ndat))
+       ttot = (fromIntegral ndat) / (fromIntegral tsSF)
+       df = 1.0 / ttot
+       dt = 1.0 / (fromIntegral tsSF)
+       freqlist = 0:(take nfreq [1*df,2*df..])
+   rng <-  RNG.newRngWithSeed (-1)
+   npsd'' <- geneNPSD rng ifo freqlist 
+   let npsd' = map (replace_zero minfreq maxfreq) npsd''
+       npsdconj = tail.init $ map DC.conjugate $ reverse npsd'
+       npsd = npsd' ++ npsdconj
+       npsdamp = map DC.magnitude npsd'
+       zipnpsd = zip freqlist npsdamp
+       printshow xx = putStrLn $ (show (fst xx)) ++ " " ++ (show (snd xx))
+       noisetdomain' = ifft (fromList npsd)
+       noisetdomain = map realPart $ toList noisetdomain'
+       tlist = take ndat [dt,2*dt..]
+   return $ noisetdomain
+
+genTDomain' :: Int->Int->Double->Double->HDD.Detector->IO[Double]
+genTDomain' ndat tsSF minfreq maxfreq ifo = do
+   let nfreq = floor (0.5*(fromIntegral ndat))
+       ttot = (fromIntegral ndat) / (fromIntegral tsSF)
+       df = 1.0 / ttot
+       dt = 1.0 / (fromIntegral tsSF)
+       freqlist = 0:(take nfreq [1*df,2*df..])
+   rng <-  RNG.newRngWithSeed (-1)
+   npsd'' <- geneNPSD rng ifo freqlist 
+   let npsdd' = map (replace_zero minfreq maxfreq) npsd''
+       npsd' = map (*(2 DC.:+ 0)) npsdd' 
+       npsdconj = tail.init $ map DC.conjugate $ reverse npsd'
+       npsd = npsd' ++ npsdconj
+       npsdamp = map DC.magnitude npsd'
+       zipnpsd = zip freqlist npsdamp
+       printshow xx = putStrLn $ (show (fst xx)) ++ " " ++ (show (snd xx))
+       noisetdomain' = ifft (fromList npsd)
+       noisetdomain = map realPart $ toList noisetdomain'
+       tlist = take ndat [dt,2*dt..]
+   return $ noisetdomain
+
+
+
+replace_zero :: Double->Double->(Double, DC.Complex Double)->(DC.Complex Double)
+replace_zero minfreq maxfreq (freq, npsd) | freq == 0      = (0 DC.:+ 0)
+	     	     	     	    	  | freq < minfreq = (0 DC.:+ 0)*npsd
+	     	    	  	  	  | freq > maxfreq = (0 DC.:+ 0)*npsd
+  					  | otherwise = npsd
