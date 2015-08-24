e81b4339f41b362f37c8568f567fcd13a9c28109
yuzuri <yuzurihara@yukimura.hep.osaka-cu.ac.jp>
2015-08-24 18:27:18 +0900

    update code, but contain bug..

--- attic/testRMSMon/testRMSMon.hs
+++ attic/testRMSMon/testRMSMon.hs
@@ -1,81 +1,97 @@
 import HasKAL.PlotUtils.HROOT.PlotGraph
 import HasKAL.PlotUtils.HROOT.PlotGraph3D
---import HasKAL.FrameUtils.FrameUtils (readFrame)
 import HasKAL.FrameUtils.Function (readFrameV)
-import qualified Numeric.LinearAlgebra as NLA
 import HasKAL.SpectrumUtils.GwPsdMethod
 import HasKAL.SpectrumUtils.SpectrumUtils (gwpsdV)
+import HasKAL.SpectrumUtils.Signature
 
 import qualified Data.Vector.Generic as DVG
 import qualified Data.Vector.Storable as V (length)
+import qualified Numeric.LinearAlgebra as NLA
 import Data.Maybe (fromMaybe)
 import System.Environment (getArgs)
+import Control.Monad (forM)
+
+import ReadFiles
+
 --import qualified Data.Complex as DC
 --import Data.Complex (Complex( (:+) ))
 
+
+--import HasKAL.TimeUtils.GPSfunction
 --import HasKAL.DataBaseUtils.Function (kagraDataGet)
--- xs <- kagraDataGet 1124077565 64 K1:PEM-EX_MAG_X_FLOOR
+-- error
+--zs <- kagraDataGet 1124077565 64 "K1:PEM-EX_MAG_X_FLOOR"
 
 main = do
 
- {-- configuration --} 
-
  {-- open frame file --}
  let channel  = "K1:PEM-EX_MAG_X_FLOOR"
-     fs = 2048 ::Double
- let filelist = ["/data/kagra/xend/R0207/K-K1_R-1124077533-32.gwf","/data/kagra/xend/R0207/K-K1_R-1124077565-32.gwf","/data/kagra/xend/R0207/K-K1_R-1124077597-32.gwf","/data/kagra/xend/R0207/K-K1_R-1124077629-32.gwf","/data/kagra/xend/R0207/K-K1_R-1124077661-32.gwf","/data/kagra/xend/R0207/K-K1_R-1124077693-32.gwf","/data/kagra/xend/R0207/K-K1_R-1124077725-32.gwf","/data/kagra/xend/R0207/K-K1_R-1124077757-32.gwf","/data/kagra/xend/R0207/K-K1_R-1124077789-32.gwf","/data/kagra/xend/R0207/K-K1_R-1124077821-32.gwf"]
+     fs = 2048::Double
+     ifs = 2048::Int
+ let filelist = take 100 testFiles
+ let gps = 1124077533::Double
 
  maybexs <- mapM (readFrameV channel) filelist
  let xs = map (fromMaybe (error " no data in the file.")) maybexs
- --let ys = map (DVG.++) xs
--- print ys
- print $ length filelist
+ let ys = DVG.concat xs
+ --print $ DVG.take 100 ys
+ --print $ DVG.length ys
 
- let nchunk = 32 * 2048 ::Int
+ let nfile = length filelist
+ print nfile
 
+ let duration = 32::Double
+     iduration = 32::Int
+ let nchunk = iduration * ifs::Int
  let tlist = [1/fs, 2/fs..32]
+     nt    = length tlist
+
+ let flist = [0.0,1.0/duration ..fs/2.0]
+
+ {-- split vector --}
+ let xs1 = DVG.slice 0 nt ys
+ print $ DVG.length xs1
+
 
  {-- fft xs data --}
- let hoff1 = gwpsdV (xs!!0 ) nchunk fs
- print $ DVG.take 100 $ fst hoff1
- print $ DVG.take 100 $ snd hoff1
+ let index = [0..nfile-1]::[Int]
 
- --let a = DVG.zip (NLA.fromList [0.0,1.0..]) (fst hoff1)
-
- -- let hoff1 = gwpsdV (DVG.slice 0 nchunk ys) nchunk fs
- -- let hoff2 = gwpsdV xs2 (V.length xs2) fs
- -- let hoff3 = gwpsdV xs3 (V.length xs3) fs
- -- let hoff4 = gwpsdV xs4 (V.length xs4) fs
- -- let hoff5 = gwpsdV xs5 (V.length xs5) fs
- -- let hoff6 = gwpsdV xs6 (V.length xs6) fs
- -- let hoff7 = gwpsdV xs7 (V.length xs7) fs
- -- let hoff8 = gwpsdV xs8 (V.length xs8) fs
- -- let hoff9 = gwpsdV xs9 (V.length xs9) fs
- -- let hoff10 = gwpsdV xs10 (V.length xs10) fs
-
+ rmslist <- forM index $ \i -> do
+  let hoff = gwpsdV (DVG.slice (10*i) (10*i+nchunk) ys) nchunk fs
 --  {-- sum each frequecy band --}
---  {-- frequency band => index of vector --}
 --  {-- [0.2:1] [1:4] [4:10] Hz --}
+--  let f1 = 0.2
+--      f2 = 1.0
+  let f1 = 1.0
+      f2 = 4.0
+  --sumHoff = DVG.sum $ DVG.filter (>f1) $ DVG.filter (<f2) hoff
+  
+  let indexlist = [0..]
+  let listSpectrum = zip (NLA.toList $ fst hoff) (NLA.toList $ snd hoff)
+  let sumHoff = sum $ map snd $ filter ( \(x, y) -> f1<x && x<f2 ) listSpectrum
+
+
+--  let fname = filelist!!(i) ++ ".png"
+--  plotXV LogXY LinePoint 1 BLUE ("frequency [Hz]", "[/rHz]") 0.05 "title" ((0.001,5),(0,0)) hoff
+  
+  return sumHoff
 
 --  {-- plot RMS value --}
--- -- plotX Linear Dot 1 RED ("time", channel) 0.08 "RMSMon" ((0,0), (0,0)) $ zip tlist xs
-
---  let hoff1 = gwpsdV xs1 (V.length xs1) fs
---  let hoff2 = gwpsdV xs2 (V.length xs2) fs
--- -- let hoff  = DVG.zip hoff1 hoff2
---  oPlotXV LogXY Dot 1 [BLUE, RED] ("frequency [Hz]", "[/rHz]") 0.05 "title" ((0,0),(0,0)) [hoff1, hoff2]
-
-
-
--- plotXV LogXY LinePoint 1 BLUE ("frequency [Hz]", "[/rHz]") 0.05 "title" ((0,0),(0,0)) hoff1
-
-
 -- plotXV Linear Dot 1 RED ("time", channel) 0.08 "RMSMon" ((0,0), (0,0)) $ DVG.zip (DVG.fromList tlist) xs
 
-
+  
+ let gpslist = [gps, gps+duration..gps+(fromIntegral nfile)*duration]::[Double]
+ print gpslist
+ print rmslist
+ plotXV LogY LinePoint 1 BLUE ("GPS[sec]", "?/s^2") 0.05 "title" ((0,0),(0,0)) (NLA.fromList gpslist, NLA.fromList rmslist)
+ 
  return 0
 
 
+
+--squeezeSpectrumRange :: Spectrum -> Double -> Double -> Vector NLA.Double
+--squeezeSpectrumRange spectrum f1 f2 = 
 
 threedata2string ::[Double] -> [Double] ->[Double]-> String
 threedata2string a b c = unlines  $ zipWith (++) (map show a)  $ zipWith (++) (repeat " ")  $ zipWith (++) ( map show b) $ zipWith (++) (repeat " ")  (map show c)
