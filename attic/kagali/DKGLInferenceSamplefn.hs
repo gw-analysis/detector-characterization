7b56f93e13fa488e541d1279d3e315ea5f426aa4
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-08-31 22:12:53 +0900

    haskell wrapper for kagali

--- attic/kagali/DKGLInferenceSamplefn.hs
+++ attic/kagali/DKGLInferenceSamplefn.hs
@@ -1,27 +1,58 @@
 
+
+--
+--
+--
+--
+
+
 module KAGALIUtils
-(dKGLInferenceSamplefnCore)
+(dKGLInferenceSamplefn)
 where
 
 
 import qualified Data.Vector.Storable as VS (Vector,  length,  unsafeWith,  unsafeFromForeignPtr0, map)
-import Foreign.ForeignPtr (ForeignPtr,  newForeignPtr_)
+import Foreign.ForeignPtr (newForeignPtr_)
 import Foreign.Ptr
 import Foreign.Marshal.Array
 import Foreign.C.Types
 import System.IO.Unsafe (unsafePerformIO)
 
 
-dKGLInferenceSamplefn :: Double -> VS.Vector Double
-dKGLInferenceSamplefn
+{- exposed functions -}
+dKGLInferenceSamplefn :: VS.Vector Double  -- ^ Input Vector
+                      -> VS.Vector Double  -- ^ Output Vector
+dKGLInferenceSamplefn vIn = do
+  let vIn' = d2cdV vIn
+      ilen= VS.length vIn :: Int
+      olen= ilen
+  cd2dV $ dKGLInferenceSamplefnCore vIn' ilen olen
 
-dKGLInferenceSamplefnCore :: VS.Vector CDouble -> Int -> CDouble
-dKGLInferenceSamplefnCore Vout olen input
-  = unsafePerformIO $ allocaArray olen $ \ptrOut ->
-    do c_DKGLInferenceSamplefn ptrOut input
-       newForeignPtr_ ptrOut >>= \foreignptrOutput ->
+
+
+{- internal functions -}
+dKGLInferenceSamplefnCore :: VS.Vector CDouble  -- ^ Input Vector
+                          -> Int                -- ^ # of elements in Input Vector
+                          -> Int                -- ^ # of elements in Input Vector
+                          -> VS.Vector CDouble  -- ^ Output Vector
+dKGLInferenceSamplefnCore input ilen olen
+  = unsafePerformIO $ VS.unsafeWith input $ \ptrIn ->
+   allocaArray olen $ \ptrOut ->
+   do c_DKGLInferenceSamplefn ptrOut (fromIntegral olen) ptrIn (fromIntegral ilen)
+      newForeignPtr_ ptrOut >>= \foreignptrOutput ->
          return $ VS.unsafeFromForeignPtr0 foreignptrOutput olen
 
 
-reign import ccall "hogehoge.h DKGLInferenceSamplefn" c_DKGLInferenceSamplefn :: Ptr CDouble -> CDouble -> IO()
+d2cdV :: VS.Vector Double -> VS.Vector CDouble
+d2cdV = VS.map realToFrac
 
+
+cd2dV :: VS.Vector CDouble -> VS.Vector Double
+cd2dV = VS.map realToFrac
+
+
+foreign import ccall "hogehoge.h DKGLInferenceSamplefn" c_DKGLInferenceSamplefn :: Ptr CDouble -- ^ input pointer
+                                                                                -> CInt        -- ^ # of elements in input
+                                                                                -> Ptr CDouble -- ^ output pointer
+                                                                                -> CInt        -- ^ # of elements in output
+                                                                                -> IO()
