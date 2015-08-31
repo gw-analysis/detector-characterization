c77f0ccef1c8c45f9922d033282f1e2ce840ef3e
yokozawa <yokozawa@yukimura.hep.osaka-cu.ac.jp>
2015-09-01 08:29:29 +0900

    modified

--- HasKAL/src/HasKAL/MonitorUtils/NoiseFloorMon/NoiseFloorMon.hs
+++ HasKAL/src/HasKAL/MonitorUtils/NoiseFloorMon/NoiseFloorMon.hs
@@ -1,130 +1,126 @@
+{-# OPTIONS_GHC -XBangPatterns #-}
+
 module HasKAL.MonitorUtils.NoiseFloorMon.NoiseFloorMon
 ( 
-estimateThreshold, 
-getNoiseFloorStatus
+  NFMParam (..) 
+, estimateThreshold 
+, getNoiseFloorStatus
+, defaultNFMparam
+, makeNFMparam
 )
 where
 
 
 import qualified HasKAL.ExternalUtils.GSL.RandomNumberGeneration as RNG
 import qualified HasKAL.ExternalUtils.GSL.RandomNumberDistributions as RND
---import Data.Vector.Storable as VS
 import qualified HasKAL.Misc.StrictMapping as HMS
 
 import HasKAL.FrameUtils.FrameUtils
---import SpectrumUtilsNoiseMon
---import LinearPredictionNoiseMon
 import HasKAL.SpectrumUtils.SpectrumUtils
 import HasKAL.SignalProcessingUtils.LinearPrediction
 import HasKAL.SignalProcessingUtils.Resampling
 
-{- For plotting -}
---import HasKAL.PlotUtils.PlotOption.PlotOptionHROOT
---import qualified HasKAL.PlotUtils.HROOT.PlotGraph as HR
+import Data.List (sort, foldl')
 
-{- For fft -}
 import Numeric.GSL.Fourier
 import Numeric.LinearAlgebra
 
-{- For low-high pass filter-}
 import HasKAL.SignalProcessingUtils.Filter
 import HasKAL.SignalProcessingUtils.ButterWorth
 
-import HROOT hiding (eval)
+--import HROOT hiding (eval, mean)
+
+data NFMParam = NFMParam{ 
+  tsreSF :: Double  
+, whitenFltOrder :: Int
+, whitenFltSize :: Int
+, whitenFltSTime :: Int
+, rmSize :: Int
+, minfreq :: Double
+, maxfreq :: Double 
+, lpfOrder :: Int 
+, hpfOrder :: Int
+} deriving (Show, Eq, Read)
+
+defaultNFMparam :: NFMParam
+defaultNFMparam = NFMParam{
+   tsreSF = 1024.0 
+,  whitenFltOrder = 100
+,  whitenFltSize = 1024 * 10
+,  whitenFltSTime = 1024 * 5
+,  rmSize = 128 
+,  minfreq = 64.0
+,  maxfreq = 128.0
+,  lpfOrder = 6
+,  hpfOrder = 6
+}
+
+makeNFMparam :: Double->Int->Int->Int->Int->Double->Double->Int->Int->NFMParam
+makeNFMparam rsf wfo wfs wfst rms minf maxf lpfo hpfo = 
+   NFMParam{  
+   	      tsreSF = rsf
+   	   ,  whitenFltOrder = wfo
+   	   ,  whitenFltSize = wfs
+	   ,  whitenFltSTime = wfst
+	   ,  rmSize = rms
+	   ,  minfreq = minf
+	   ,  maxfreq = maxf
+	   ,  lpfOrder = lpfo
+	   ,  hpfOrder = hpfo
+   }
 
 {-estimateThreshold should be done before applying getNoiseFloorStatus-}
 
-
-estimateThreshold :: Int->Double->Double->Int->IO(Double,Double)
-estimateThreshold tsSF minfreqband maxfreqband lcsz = do
+estimateThreshold :: NFMParam->Int->IO(Double,Double)
+estimateThreshold np lcsz = do
    simnoise <- genTimeGauss lcsz
-   noisemon <- getThresholdStatus simnoise tsSF minfreqband maxfreqband
-   let rmnoise = map snd noisemon
-       nfmmean' = sumV rmnoise
-       nfmmean = (nfmmean' / (realToFrac (Prelude.length rmnoise)))
-       nfmdev' = sumV  (Prelude.map (\x->(x-nfmmean)*(x-nfmmean)) rmnoise)
-       nfmdev =  sqrt(nfmdev' / (realToFrac((Prelude.length rmnoise) - 1)))
+   noisemon <- getThresholdStatus simnoise np
+   let nfmmean' = sum noisemon
+       nfmmean = (nfmmean' / (realToFrac (Prelude.length noisemon)))
+       nfmdev' = sum  (Prelude.map (\x->(x-nfmmean)*(x-nfmmean)) noisemon)
+       nfmdev =  sqrt(nfmdev' / (realToFrac((Prelude.length noisemon) - 1)))
    return $ (nfmmean,nfmdev)                         
 
-{-
-getNoiseFloorStatus
-param1 : [Double] : time domain which want to check noise floor status
-param2 : Int      : Sampling frequency of time domain data
-param3 : Double   : minimum of frequency band to check noise floor status
-param4 : Double   : maximum of frequency band to check noise floor status
-param5 : [(Double,Double)] : (time, significance)
-param6 : (Double,Double) : output of estimateThreshold
-time :: set time=0 for first point of time domain data
-significance :: deviation from standard Gaussian-Stationary noise floor
--}
+getNoiseFloorStatus :: [Double]->Double->NFMParam->(Double,Double)->IO[(Double,Double)]
+getNoiseFloorStatus ts tsSF np (nfmmean,nfmdev) = do
+  let dsts = downsampleV tsSF (tsreSF np) (fromList ts)
+      nfmFltDelay = nfmCalcFltDelay (whitenFltOrder np) (tsreSF np) (lpfOrder np) (hpfOrder np) (minfreq np) (maxfreq np) ::Int
+  whitendatsample <- getWhitensample (toList dsts) np
+  let psdmedian = gwpsdV (fromList whitendatsample) (rmSize np) (tsreSF np)
+      wtts =  whitening (lpefCoeffV (whitenFltOrder np) psdmedian) (toList dsts) 
+  bpts <- applybandpass (fromList wtts) np
+  let bptssig = drop nfmFltDelay (toList bpts) 
+      bptssig2 = map (\x -> (x*x-nfmmean)/nfmdev) bptssig
+      datrunmed = runmed bptssig2 (rmSize np)
+      intervalrunmed = (fromIntegral (rmSize np)) / (tsreSF np) :: Double
+      trunmed = [(intervalrunmed),(intervalrunmed*2)..]
+--  return $ zip trunmed datrunmed
+--  return $ take 1 $ zip [1..] wtts
+  return $ zip [1..] wtts
 
-getNoiseFloorStatus :: [Double]->Int->Double->Double->(Double,Double)->IO[(Double,Double)]
-getNoiseFloorStatus ts tsSF minfreqband maxfreqband (nfmmean,nfmdev) = do
--- Hitomazu kokodeha let de initial parameter wo teigi suru
-  let whitenFltOrder = 100 :: Int
-      whitenFltSize = 10*tsSF :: Int
-      rmSize = 256 :: Int -- 1/8 * 2048
-      lpfOrder = 6 :: Int 
-      hpfOrder = 6 :: Int
-      nfmFltDelay = nfmCalcFltDelay tsSF whitenFltOrder lpfOrder hpfOrder minfreqband maxfreqband ::Int
-      lcsz = length ts
-      rmwz = rmSize
-  --(nfmmean,nfmdev) <- estimateThreshold lcsz rmwz
-  --print (nfmmean,nfmdev)
-  whitendatsample <- getWhitensample ts tsSF whitenFltSize
-  let psdmedian = gwpsd whitendatsample rmSize (fromIntegral tsSF)
-      wtts =  whitening (lpefCoeff whitenFltOrder psdmedian) ts :: [Double]
-  bpts <- applybandpass wtts tsSF minfreqband maxfreqband lpfOrder hpfOrder 
-  --mapM_ print whitendatsample
-  let bptssig = drop nfmFltDelay bpts :: [Double]
+getThresholdStatus :: [Double]->NFMParam->IO[Double]
+getThresholdStatus ts np = do
+  let nfmFltDelay = nfmCalcFltDelay 0 (tsreSF np) (lpfOrder np) (hpfOrder np) (minfreq np) (maxfreq np) ::Int
+  bpts <- applybandpass (fromList ts) np  
+  let bptssig = drop nfmFltDelay (toList bpts) :: [Double]
       bptssig2 = map (\x -> x*x) bptssig
-      datrunmed = runmed bptssig rmSize
-      datrunmedsig = map (\x -> abs ((x-nfmmean)/nfmdev)) datrunmed
-      intervalrunmed = (fromIntegral rmSize) / (fromIntegral tsSF) :: Double
-      trunmed = take (length datrunmed) [(intervalrunmed),(intervalrunmed*2)..]
-  return $ zip trunmed datrunmedsig
+      datrunmed = runmed bptssig2 (rmSize np)
+  return datrunmed
 
-getThresholdStatus :: [Double]->Int->Double->Double->IO[(Double,Double)]
-getThresholdStatus ts tsSF minfreqband maxfreqband = do
--- Hitomazu kokodeha let de initial parameter wo teigi suru
-  let whitenFltOrder = 100 :: Int
-      whitenFltSize = 10*tsSF :: Int
-      rmSize = 256 :: Int -- 1/8 * 2048
-      lpfOrder = 6 :: Int 
-      hpfOrder = 6 :: Int
-      nfmFltDelay = nfmCalcFltDelay tsSF whitenFltOrder lpfOrder hpfOrder minfreqband maxfreqband ::Int
-      lcsz = length ts
-      rmwz = rmSize
-  --(nfmmean,nfmdev) <- estimateThreshold lcsz rmwz
-  --print (nfmmean,nfmdev)
-  whitendatsample <- getWhitensample ts tsSF whitenFltSize
-  let psdmedian = gwpsd whitendatsample rmSize (fromIntegral tsSF)
-      wtts =  whitening (lpefCoeff whitenFltOrder psdmedian) ts :: [Double]
-  bpts <- applybandpass wtts tsSF minfreqband maxfreqband lpfOrder hpfOrder 
-  --mapM_ print whitendatsample
-  let bptssig = drop nfmFltDelay bpts :: [Double]
-      bptssig2 = map (\x -> x*x) bptssig
-      datrunmed = runmed bptssig rmSize
-      --datrunmedsig = map (\x -> abs ((x-nfmmean)/nfmdev)) datrunmed
-      intervalrunmed = (fromIntegral rmSize) / (fromIntegral tsSF) :: Double
-      trunmed = take (length datrunmed) [(intervalrunmed),(intervalrunmed*2)..]
-  return $ zip trunmed datrunmed
-
-
-applybandpass :: [Double]->Int->Double->Double->Int->Int->IO[Double]
-applybandpass wtts tsSF minfreqband maxfreqband lpfOrder hpfOrder = do
-  let (nfmNumCoeffLow, nfmDenomCoeffLow) = butter 6 (fromIntegral tsSF) maxfreqband Low
-      (nfmNumCoeffHigh, nfmDenomCoeffHigh) = butter 6 (fromIntegral tsSF) minfreqband High
-      nyquistfreq = (fromIntegral tsSF) / 2.0 :: Double
-      bpts' = if (maxfreqband < nyquistfreq) then (iirFilter wtts nfmNumCoeffLow nfmDenomCoeffLow)
-      	      	 	      	      	     else wtts
-      bpts = if (minfreqband > 0.0) then (iirFilter bpts' nfmNumCoeffHigh nfmDenomCoeffHigh)
-      	      	 	      	    else bpts'
+applybandpass :: Vector Double->NFMParam->IO(Vector Double)
+applybandpass wtts np = do
+  let nfmCoeffLow = butter (lpfOrder np) (tsreSF np) (maxfreq np) Low
+      nfmCoeffHigh = butter (lpfOrder np) (tsreSF np) (minfreq np) High
+      nyquistfreq = (tsreSF np) / 2.0 :: Double
+      bpts' = if ((maxfreq np) < nyquistfreq) then (iir nfmCoeffLow wtts)
+      	      	 	      	      	      else wtts
+      bpts = if ((minfreq np) > 0.0) then (iir nfmCoeffHigh bpts')
+      	      	 	      	     else bpts'
   return bpts
 
-nfmCalcFltDelay :: Int->Int->Int->Int->Double->Double->Int
+nfmCalcFltDelay :: Int->Double->Int->Int->Double->Double->Int
 nfmCalcFltDelay whitenFltOrder tsSF lpfOrder hpfOrder minfreqband maxfreqband = nfmFltDelay
-  where nyquistfreq = (fromIntegral tsSF) / 2.0 :: Double
+  where nyquistfreq = tsSF / 2.0 :: Double
         whitenFltDelay =  whitenFltOrder :: Int
         lowpassFltDelay = if (maxfreqband < nyquistfreq) then lpfOrder :: Int
 			     		    	         else 0 :: Int
@@ -132,10 +128,11 @@ nfmCalcFltDelay whitenFltOrder tsSF lpfOrder hpfOrder minfreqband maxfreqband = 
 			      		     	  else 0 :: Int
 	nfmFltDelay = (whitenFltDelay+lowpassFltDelay+highpassFltDelay) ::Int
 
-getWhitensample :: [Double]->Int->Int->IO[Double]
-getWhitensample ts tsSf whitenFltSize = do
-  if ((length ts) < whitenFltSize) then (return $ take whitenFltSize [0,0..])
-     			           else (return $ take whitenFltSize ts)
+getWhitensample :: [Double]->NFMParam->IO[Double]
+getWhitensample ts np = do
+  if ((length ts) < (whitenFltSize np)) 
+     then (return $ take (whitenFltSize np) [0,0..])
+     else (return $ take (whitenFltSize np) $ drop (whitenFltSTime np) ts)
 
 
 
@@ -148,17 +145,20 @@ genTimeGauss lcsz = do
      return $ gaus
 
 
-sumV :: [Double]->Double
-sumV [] = 0.0
-sumV (x:xs) = x + (sumV xs)
+runmed :: [Double] -> Int -> [Double]
+runmed dat nmed = do 
+  let datV = fromList dat
+      ndat = dim datV
+      maxitr = floor $ fromIntegral (ndat) / fromIntegral (nmed) :: Int
+      datList = takesV  (take maxitr (repeat nmed)) datV :: [Vector Double]
+  map median $ map (sort . toList) datList
 
-estimateThreshold' :: Int->Int->IO(Double,Double)
-estimateThreshold' lcsz rmwz = do
-   simnoise <- genTimeGauss lcsz
-   let simnoise2 = Prelude.map (\x->x*x) simnoise
-       datrunmed = runmed simnoise2 rmwz
-       nfmmean' = sumV datrunmed
-       nfmmean = sqrt (nfmmean' / (realToFrac (Prelude.length datrunmed)))
-       nfmdev' = sumV (Prelude.map (\x->(x-nfmmean)*(x-nfmmean)) datrunmed)
-       nfmdev = sqrt(nfmdev' / (realToFrac((Prelude.length datrunmed) - 1)))
-   return $ (nfmmean,nfmdev)                         
+mean :: Floating a => [a] -> a
+mean x = fst $ foldl' (\(!m, !n) x -> (m+(x-m)/(n+1),n+1)) (0,0) x
+
+median :: (Floating a, Ord a) => [a] -> a
+median x | odd n  = head  $ drop (n `div` 2) x'
+         | even n = mean $ take 2 $ drop i x'
+                  where i = (length x' `div` 2) - 1
+                        x' = sort x
+                        n  = length x
