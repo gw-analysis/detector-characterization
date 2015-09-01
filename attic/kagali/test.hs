45405ccb7e1f2ae7a7b4c73a647b0dc820dab45c
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-09-01 13:56:44 +0900

    modified

--- attic/kagali/test.hs
+++ attic/kagali/test.hs
@@ -4,9 +4,9 @@
 import qualified Data.Vector.Storable as VS
 import KAGALIUtils
 
-
+main :: IO()
 main = do
   let inputV = VS.fromList [1, 2, 3, (4::Double)]
       outputV = dKGLInferenceSamplefn inputV
-  return $ take 2 $ VS.toList outputV
+  print $ take 2 $ VS.toList outputV
 
