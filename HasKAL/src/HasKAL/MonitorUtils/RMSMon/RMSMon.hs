2b4c6148fe3fd1d08813bff719121b3c976ddacf
yuzuri <yuzurihara@yukimura.hep.osaka-cu.ac.jp>
2015-09-14 13:08:46 +0900

    update RMSMon : check normalization

--- HasKAL/src/HasKAL/MonitorUtils/RMSMon/RMSMon.hs
+++ HasKAL/src/HasKAL/MonitorUtils/RMSMon/RMSMon.hs
@@ -19,25 +19,25 @@ rmsMon nSplit fs ys freq = do
  map (\(f1, f2) -> rmsMoncore nSplit duration fs ys f1 f2) freq
      where rmsMoncore :: Int -> Double -> Double -> NLA.Vector Double -> Double -> Double -> (NLA.Vector Double,NLA.Vector Double)
            rmsMoncore nSplit duration fs ys f1 f2 = do
-             let listindex = [0..nSplit-1]::[Int]
              let df = 1.0/duration :: Double 
-             let rmsvector = NLA.fromList $ map (*df) $ map (sumHoff nSplit fs ys f1 f2) listindex
              let tn = (fromIntegral nSplit -1)*duration::Double
              let timevector = NLA.fromList [0, duration..tn]::NLA.Vector Double
+             let listindex = [0..nSplit-1]::[Int]
+             let rmsvector = NLA.fromList $ map sqrt $ map (*df) $ map (sumHoff nSplit fs ys f1 f2) listindex
              (timevector, rmsvector)
 
 rmsDailyMon :: Double -> NLA.Vector Double -> [(Double, Double)] -> [(NLA.Vector Double, NLA.Vector Double)]
 rmsDailyMon fs ys freq = do
- let nSplit = 96::Int -- 24時間のデータを15分ずつ = 96個
+ let nSplit = 96::Int -- assuming data is 24hour and divide into 15 pieces => 96 chunks
  let duration  = 0.15::Double
  map (\(f1, f2) -> rmsDailyMoncore nSplit duration fs ys f1 f2) freq
      where rmsDailyMoncore :: Int -> Double -> Double -> NLA.Vector Double -> Double -> Double -> (NLA.Vector Double,NLA.Vector Double)
            rmsDailyMoncore nSplit duration fs ys f1 f2 = do
-             let listindex = [0..nSplit-1]::[Int]
              let df = 1.0/duration :: Double 
-             let rmsvector = NLA.fromList $ map (*df) $ map (sumHoff nSplit fs ys f1 f2) listindex
              let tn = (fromIntegral nSplit -1) * duration::Double
              let timevector = NLA.fromList [0, duration..tn]::NLA.Vector Double
+             let listindex = [0..nSplit-1]::[Int]
+             let rmsvector = NLA.fromList $ map sqrt $ map (*df) $ map (sumHoff nSplit fs ys f1 f2) listindex
              (timevector, rmsvector)
 
 {-- Internal Functions --}
@@ -49,5 +49,6 @@ sumHoff nSplit fs ys f1 f2 i = do
      indx2' = DVG.findIndex (>(max f1 f2)) $ fst hoff
      indx1  = fromJust indx1'
      indx2  = fromJust indx2'
- DVG.sum . DVG.slice indx1 (indx2 - indx1) $ snd hoff
+ (*2) . DVG.sum . DVG.slice indx1 (indx2 - indx1) $ snd hoff
+ -- output of gwpsdV is two-sided spectrum -> need factor 2
 
