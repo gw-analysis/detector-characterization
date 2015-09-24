2ebf1ef0e1aabf302d75bd260d29e79936492be0
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-09-24 23:40:07 +0900

    added functions in filterFunction.[c, h]

--- HasKAL/src/HasKAL/SignalProcessingUtils/filterFunctions.c
+++ HasKAL/src/HasKAL/SignalProcessingUtils/filterFunctions.c
@@ -9,11 +9,6 @@
 #define M_PI           3.14159265358979323846
 #endif
 
-//-- prototype
-//int fir_filter_core(double*,  unsigned,  double[],  double[],  unsigned*,  unsigned, double*);
-//int fir_filter (double*, unsigned, double[], unsigned,  double*);
-//int iir_filter (double*,  unsigned,  double[],  double[], unsigned, double*);
-//
 
 
 int iir_filter (double *input, unsigned inputlen, double num_coeff[], double denom_coeff[], unsigned filterlen, double *output){
@@ -137,8 +132,6 @@ double goertzel (double *Samples, int N, double fs, double freq)
 }
 
 
-
-
 int filtfilt (double *input, unsigned inputlen, double num_coeff[], double denom_coeff[], unsigned filterlen, double *output){
 
     unsigned inputidx, k, i;
@@ -169,5 +162,47 @@ int filtfilt (double *input, unsigned inputlen, double num_coeff[], double denom
 
 
     return 1;
+}
+
+
+void sosfilter (double *input, unsigned inputlen, double *num_coeff0, double *num_coeff1,double *num_coeff2, double *denom_coeff0, double *denom_coeff1, double *denom_coeff2, unsigned nsect, double *output)
+{
+ double y;
+ int j, k;
+ double RegX1[nsect], RegX2[nsect], RegY1[nsect], RegY2[nsect];
+
+ for(j=0; j<nsect; j++)
+  {
+   RegX1[j] = 0.0;
+   RegX2[j] = 0.0;
+   RegY1[j] = 0.0;
+   RegY2[j] = 0.0;
+  }
+
+ for(j=0; j<inputlen; j++)
+  {
+   y = sosform1(0, input[j], num_coeff0, num_coeff1, num_coeff2, denom_coeff0, denom_coeff1, denom_coeff2, RegX1, RegX2, RegY1, RegY2);
+   for(k=1; k<nsect; k++)
+  {
+   y = sosform1(k, y, num_coeff0, num_coeff1, num_coeff2, denom_coeff0, denom_coeff1, denom_coeff2, RegX1, RegX2, RegY1, RegY2);
+  }
+   output[j] = y;
+  }
+}
+
+
+double sosform1(int k, double x, double *num_coeff0, double *num_coeff1, double *num_coeff2, double *denom_coeff0, double *denom_coeff1, double *denom_coeff2, double *RegX1, double *RegX2, double *RegY1, double *RegY2)
+{
+ double y, point;
+
+ point = x * num_coeff0[k] + num_coeff1[k] * RegX1[k] + num_coeff2[k] * RegX2[k];
+ y = denom_coeff0[k] * point - denom_coeff1[k] * RegY1[k] - denom_coeff2[k] * RegY2[k];
+
+ RegX2[k] = RegX1[k];
+ RegX1[k] = x;
+ RegY2[k] = RegY1[k];
+ RegY1[k] = y;
+
+ return(y);
 }
 
--- HasKAL/src/HasKAL/SignalProcessingUtils/filterFunctions.h
+++ HasKAL/src/HasKAL/SignalProcessingUtils/filterFunctions.h
@@ -10,3 +10,7 @@ int iir_filter_core (double*, unsigned, double[], double[], unsigned, double[], 
 double goertzel (double*, int, double, double);
 
 int filtfilt (double*,  unsigned,  double[],  double[], unsigned, double*);
+
+void sosfilter (double*,  unsigned,  double*,  double*,  double*,  double*,  double*,  double*,  unsigned,  double*);
+
+double sosform1 (int,  double,  double*,  double*,  double*,  double*,  double*,  double*,  double*,  double*,  double*,  double*);
