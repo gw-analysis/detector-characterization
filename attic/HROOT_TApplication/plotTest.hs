a2e154a95169d17a12d5d10964945b9b2aa71d5f
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2015-09-26 16:22:45 +0900

    update test code

--- attic/HROOT_TApplication/plotTest.hs
+++ attic/HROOT_TApplication/plotTest.hs
@@ -1,16 +1,28 @@
 {-******************************************
   *     File Name: plotTest.hs
   *        Author: Takahiro Yamamoto
-  * Last Modified: 2015/09/26 15:02:14
+  * Last Modified: 2015/09/26 16:22:29
   *******************************************-}
 
 import HasKAL.PlotUtils.HROOT.PlotGraph
+import HasKAL.PlotUtils.HROOT.PlotGraph3D
 import Data.Vector.Storable as V
+import Data.Packed.Matrix
 
 main :: IO ()
 main = do
+  {-- parameter --}
   let gps = 1125846017 -- 2015/9/10 00:00:00
-      ts = fromList [0,200..86400]
 
-  let xs = V.map (sin.(2*pi*(1/V.maximum ts)*)) ts
-  plotDateV Linear Line 1 BLUE ("x", "y") 0.05 "t" "a.png" ((0,0),(0,0)) gps (ts, xs)
+  {-- time series data --}
+  let tv1 = fromList [0,100..86400]
+      xv1 = V.map (sin.(2*pi*(1/V.maximum tv1)*)) tv1
+
+  {-- spectrogram data --}
+  let tv2 = tv1
+      fv2 = fromList [1..64]
+      ym2 = ((V.length fv2)><(V.length tv2)) [1..fromIntegral $ (V.length tv2)*(V.length fv2)]
+
+  {-- plot --}
+  plotDateV Linear Line 1 BLUE ("x", "y") 0.05 "t" "a1.png" ((0,0),(0,0)) gps (tv1, xv1)
+  histgram2dDateM Linear COLZ ("x", "y", "z") "t" "a2.png" ((0,0),(0,0)) gps (tv2, fv2, ym2)
