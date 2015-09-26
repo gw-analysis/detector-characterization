92e6a1189d328ae8c21b4d0a88f0959277eddd26
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2015-09-26 15:08:50 +0900

    modified test code for plot tool

--- attic/HROOT_TApplication/plotTest.hs
+++ attic/HROOT_TApplication/plotTest.hs
@@ -1,13 +1,16 @@
 {-******************************************
   *     File Name: plotTest.hs
   *        Author: Takahiro Yamamoto
-  * Last Modified: 2015/02/27 15:37:05
+  * Last Modified: 2015/09/26 15:02:14
   *******************************************-}
 
-import HasKAL.PlotUtils.HROOT.PlotGraph (easyPlotX, LogOption(Linear))
-import HasKAL.FrameUtils.Function (readFrame)
+import HasKAL.PlotUtils.HROOT.PlotGraph
+import Data.Vector.Storable as V
 
 main :: IO ()
 main = do
-  xs <- readFrame "L1:LOSC-STRAIN" "/data/L-L1_LOSC_4_V1-842743808-4096.gwf"
-  easyPlotX Linear $ take 1000 $ zip [1..] xs
+  let gps = 1125846017 -- 2015/9/10 00:00:00
+      ts = fromList [0,200..86400]
+
+  let xs = V.map (sin.(2*pi*(1/V.maximum ts)*)) ts
+  plotDateV Linear Line 1 BLUE ("x", "y") 0.05 "t" "a.png" ((0,0),(0,0)) gps (ts, xs)
