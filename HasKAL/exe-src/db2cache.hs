5fcbf2a203bf5056dfc2a69d397638b04c4ab950
enoshima <kazuhiro.hayama@gmail.com>
2015-09-29 13:40:32 +0900

    added function db2cache to generate cachefile from a given table

---
+++ HasKAL/exe-src/db2cache.hs
@@ -0,0 +1,14 @@
+
+
+import HasKAL.DataBaseUtils.Function (db2framecache)
+import System.Environment (getArgs)
+import System.IO (stdout, hPutStrLn)
+
+main = do
+  args <- getArgs
+  let (dbname, fileName) | length args == 2 = (args!0, args!1)
+                         | otherwise = error "usage : db2cache database_name output_file"
+  contents <- db2framecache dbname
+  case contents of
+    Nothing -> print "Nothing"
+    Just x -> writeFile fileName $ unlines contents
