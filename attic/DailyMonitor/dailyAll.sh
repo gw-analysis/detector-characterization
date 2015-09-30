4274856fd15cfc2ab52ac8dc438a99c640e68058
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2015-10-01 01:20:27 +0900

    update automation script
    added rms monitor

--- attic/DailyMonitor/dailyAll.sh
+++ attic/DailyMonitor/dailyAll.sh
@@ -2,8 +2,9 @@
 
 
 ###  User Parameters
+DEBUG_MODE=1
 CHANNELS="K1:PEM-EX_ACC_NO2_X_FLOOR \
-          K1:PEM-EX_ACC_NO2_X_FLOOR"
+          K1:PEM-EX_ACC_NO2_Y_FLOOR"
 
 # CHANNELS="K1:PEM-EX_ACC_NO2_X_FLOOR \
 # 	 K1:PEM-EX_ACC_NO2_Y_FLOOR \
@@ -13,21 +14,25 @@ CHANNELS="K1:PEM-EX_ACC_NO2_X_FLOOR \
 #  	 K1:PEM-EX_MAG_Z_FLOOR \
 #  	 K1:PEM-EX_MIC_FLOOR"
 
-
 MONITORS="./dailySRMon \
-          ./dailyRMon"
-
-MAX_CORE=4
+          ./dailyRMon \
+          ./dailyRMSMon"
 
 
 ###  Admin Parameters
+MAX_CORE=3
 YESTERDAY="2015 07 17"
 #YESTERDAY=`date -d '1 day ago' "+%Y %m %d"`
+LOG_FILE="dailyMon-2015-07-17.log"
+# LOG_FILE="dailyMon-`date -d '1 day ago' "+%Y-%m-%d"`.log"
 CMD_PARA="/home/yamamoto/apps/parallel/bin/parallel"
-LOG_FILE="dailyMon-`date -d '1 day ago' "+%Y-%m-%d"`.log"
-
 
 ###  Main
+if test ${DEBUG_MODE} = "1"
+then
+    DEBUG="--dry-run"
+fi
+
 if test ${MAX_CORE} -a ${MAX_CORE} -ge 1
 then
     JOB_NUM="--jobs ${MAX_CORE}"
@@ -38,4 +43,4 @@ then
     LOGGING="--joblog ${LOG_FILE}"
 fi
 
-${CMD_PARA} ${JOB_NUM} ${LOGGING} "{1} {2} ${YESTERDAY}" ::: ${MONITORS} ::: ${CHANNELS}
+${CMD_PARA} ${DEBUG} ${JOB_NUM} ${LOGGING} "{2} {1} ${YESTERDAY}" ::: ${CHANNELS} ::: ${MONITORS}
