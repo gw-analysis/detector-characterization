64e362ce094ad90456e3ef98d09ad09392bb175f
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2015-10-01 19:15:48 +0900

    added dailyCoherenceMon

---
+++ attic/DailyMonitor/dailyCoherenceMon.hs
@@ -0,0 +1,58 @@
+
+import Data.Maybe (fromJust)
+import System.Environment (getArgs)
+import Data.Packed.Vector (subVector)
+
+import HasKAL.TimeUtils.GPSfunction (time2gps)
+import HasKAL.FrameUtils.FrameUtils (getSamplingFrequency)
+import HasKAL.DataBaseUtils.Function (kagraDataGet, kagraDataFind)
+import HasKAL.SpectrumUtils.SpectrumUtils (gwpsdV, gwspectrogramV)
+import HasKAL.MonitorUtils.CoherenceMon.Function
+import HasKAL.PlotUtils.HROOT.PlotGraph
+
+main = do
+  args <- getArgs
+  (year, month, day, ch1, ch2) <- case length args of
+                              5 -> return (args!!0, show0 2 (args!!1), show0 2 (args!!2), args!!3, args!!4)
+                              _ -> error "Usage: dailyCoherenceMon yyyy mm dd ch1 ch2"
+
+  {-- parameters --}
+  let gps = read $ time2gps $ year++"-"++month++"-"++day++" 00:00:00 JST"
+      duration = 86400 -- seconds
+      -- for CoherenceMon
+      fftLength = 64   -- seconds
+      -- for Plot
+      oFile = ch1++"_"++ch2++"-"++year++"-"++month++"-"++day++"_Coherence.png"
+      title = "CoherenceMon: " ++ ch1 ++ " vs " ++ ch2
+      xlabel = "Date: "++year++"/"++month++"/"++day
+
+  {-- read data --}
+  mbFiles <- kagraDataFind (fromIntegral gps) (fromIntegral duration) ch1
+  let file = case mbFiles of
+              Nothing -> error $ "Can't find file: "++year++"/"++month++"/"++day
+              _ -> head $ fromJust mbFiles
+  mbDat1 <- kagraDataGet gps duration ch1
+  mbFs1 <- getSamplingFrequency file ch1
+  mbDat2 <- kagraDataGet gps duration ch2
+  mbFs2 <- getSamplingFrequency file ch2
+  let (dat1, fs1, dat2, fs2) = case (mbDat1, mbFs1, mbDat2, mbFs2) of
+                   (Just a, Just b, Just c, Just d) -> (a, b, c, d)
+                   (Nothing, _, _, _) -> error $ "Can't read data: "++ch1++"-"++year++"/"++month++"/"++day
+                   (_, Nothing, _, _) -> error $ "Can't read sampling frequency: "++ch1++"-"++year++"/"++month++"/"++day
+                   (_, _, Nothing, _) -> error $ "Can't read data: "++ch2++"-"++year++"/"++month++"/"++day
+                   (_, _, _, Nothing) -> error $ "Can't read sampling frequency: "++ch2++"-"++year++"/"++month++"/"++day
+
+  {-- main --}
+  case (fs1/=fs2) of
+   True  -> error "sampling frequency of ch1 and ch2 are difference"
+   False -> do
+     let coh = coherenceMon (truncate $ fftLength*fs1) fs1 dat1 dat2
+     plotV Linear Line 1 BLUE (xlabel, "coh(f)") 0.05 title oFile ((0,0),((-0.05),1.05)) coh
+
+
+{-- Internal Functions --}
+show0 :: Int -> String -> String
+show0 digit number
+  | len < digit = (concat $ replicate (digit-len) "0") ++ number
+  | otherwise   = number
+  where len = length number
---
+++ attic/DailyMonitor/dailyCoherenceMon.mk
@@ -0,0 +1,41 @@
+#******************************************#
+#     File Name: dailyCoherenceMon.mk
+#        Author: Takahiro Yamamoto
+# Last Modified: 2015/10/01 13:52:28
+#******************************************#
+
+# compiler option
+HC = ghc -O2
+
+# use library
+USELIB= libframe
+
+# program
+TAR1= dailyCoherenceMon
+TARs= ${TAR1}
+
+# dependency
+DEP1= ./HasKAL/PlotUtils/HROOT/AppendFunction.cc
+DEPs= ${DEP1}
+
+#########################################
+# link path
+ifneq (${USELIB},)
+CFLAGS=`pkg-config --cflags ${USELIB}`
+LDFLAGS=`pkg-config --libs-only-L ${USELIB}`
+LIBS=`pkg-config --libs-only-l ${USELIB}`
+endif
+
+# compile rule
+all: ${TARs}
+
+${TAR1}: ${TAR1}.hs ${DEPs}
+	-${HC} -o $@ $< ${CFLAGS} ${LDFLAGS} ${LIBS}
+	${HC} -o $@ $^ ${CFLAGS} ${LDFLAGS} ${LIBS}
+
+clean:
+	rm -f ./*~ ./*.o ./*.hi ./*.dyn_o ./*.dyn_hi
+
+cleanall: clean
+	rm -f ${TARs}
+
