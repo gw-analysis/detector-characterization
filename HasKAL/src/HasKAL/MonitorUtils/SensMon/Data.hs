33b3f9cfe5215a46565813b5d51d648d8aa80c15
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-10-31 09:33:57 +0900

    added dailySensMon

--- HasKAL/src/HasKAL/MonitorUtils/SensMon/Data.hs
+++ HasKAL/src/HasKAL/MonitorUtils/SensMon/Data.hs
@@ -13,3 +13,22 @@ data SensParam = SensParam
        , binlist :: [Double]
        }
 
+updateSensParam'histmin :: Double -> SensParam
+updateSensParam'histmin x = SensParam {histmin = x}
+
+updateSensParam'histmax :: Double -> SensParam
+updateSensParam'histmax x = SensParam {histmax = x}
+
+updateSensParam'ndiv :: Int -> SensParam
+updateSensParam'ndiv n = SensParam {ndiv = n}
+
+updateSensParam'inInterval :: Double -> SensParam
+updateSensParam'inInterval bi = SensParam {binInterval = bi}
+
+updateSensParam'binlist :: [Double] -> SensParam
+updateSensParam'binlist bl = SensParam {binlist = bl}
+
+
+
+
+
--- HasKAL/src/HasKAL/MonitorUtils/SensMon/SensMon.hs
+++ HasKAL/src/HasKAL/MonitorUtils/SensMon/SensMon.hs
@@ -7,14 +7,48 @@ module HasKAL.MonitorUtils.SensMon.SensMon
 --,
 ) where
 
+import Control.Monad (forM)
+import Data.Maybe (fromMaybe)
 import qualified Data.Packed.Matrix as M
 import qualified Data.Vector.Storable as VS
 import Numeric.LinearAlgebra
+import HasKAL.DataBaseUtils.Function (kagraDataGet)
+import HasKAL.FrameUtils.FrameUtils (getSamplingFrequency)
 import HasKAL.SpectrumUtils.SpectrumUtils
 import HasKAL.SpectrumUtils.Function (updateMatrixElement)
 import HasKAL.MonitorUtils.SensMon.Signature
 import HasKAL.MonitorUtils.SensMon.Data
 import HasKAL.MonitorUtils.SensMon.Function
+import HasKAL.PlotUtils.HROOT.PlotGraph3D
+import HasKAL.TimeUtils.Function(deformatGPS)
+import HasKAL.TimeUtils.GPSfunction (time2gps)
+import HasKAL.TimeUtils.Signature
+import System.IO.Unsafe (unsafePerformIO)
+
+
+type Date = (Int, Int, Int)
+
+dailySensMon :: Date -> String -> (Double, Double) -> (Double, Double) -> IO ()
+dailySensMon (year, month, day) ch (fl, fu) (hmin, hmax) = do
+  let gps = read $ time2gps $ year++"-"++month++"-"++day++" 00:00:00 JST"
+      day = 86400
+      hr = 3600
+      min = 60
+      gpslist = [gps, gps+hr..gps+day]
+      file = head $ fromMaybe (error "file not found")
+               $ kagraDataFind (fromIntegral gps) (fromIntegral hr) ch
+  fs <- getSamplingFequency file ch >>= \maybefs ->
+          return $ fromMaybe (error "fs not loaded") maybefs
+  let x = go gpslist ch
+      fname = ch++"-"++year++"-"++month++"-"++day++"_SensMon.png"
+      title = "SensMon: " ++ ch
+  histgram2dM LogXYZ COLZ ("frequency [Hz]", "ASD [1/rHz]", "count") title fname ((fl,fu), (hmin,hmax)) x
+  where
+    go (t:ts) ch =
+      let maybev = unsafePerformIO $ kagraDataGet t hr ch
+       in case maybev of
+            Nothing -> go ts ch
+            Just v -> updateSensMon (runSensMon v fs (min*fs) (hmin, min*fs, hmax)) (go ts ch)
 
 
 runSensMon :: VS.Vector Double -> Double -> Int -> (Double, Int, Double) -> SensSpectrum
@@ -25,13 +59,13 @@ runSensMon input fs n (hmin, n, hmax) =
         , ndiv = n
         , binInterval = (logBase 10 (histmax param) - logBase 10 (histmin param))/fromIntegral (ndiv param)
         , binlist = map (10**) [logBase 10 (histmin param), logBase 10 (histmin param)
-            +(binInterval param) ..logBase 10 (histmax param)]
+            +binInterval param ..logBase 10 (histmax param)]
         }
    in runSensMonCore input fs n param
 
 
 updateSensMon :: SensSpectrum -> SensSpectrum -> SensSpectrum
-updateSensMon history new = do
+updateSensMon history new =
   let (_, _, historyM) = history
       (_, _, newM)     = new
       updatedM = updateMatrixElement historyM mindxs values
@@ -41,16 +75,15 @@ updateSensMon history new = do
 
 
 runSensMonCore :: VS.Vector Double -> Double -> Int -> SensParam -> SensSpectrum
-runSensMonCore input fs n param = do
+runSensMonCore input fs n param =
   let chunks = mkChunks input n
       n2 = n `div` 2
-      vlist  = map (\x -> VS.take n2
-        $ VS.map (sqrt . (*2)) $ snd $ gwpsdV x n fs) chunks
+      vlist  = map (\x -> snd $ gwOnesidedPSDV x n fs) chunks
       eachFbin = M.toColumns . M.fromRows $ vlist
       hmax = histmax param
       hmin = histmin param
       bins = binlist param
-   in ( VS.take n2 $ linspace n (0, fs)
+   in ( fromList [fs*fromIntegral i/fromIntegral nfft|i<-[0..n2]]
       , VS.fromList $ init bins
       , M.fromColumns
         $ map (VS.fromList . snd . histogram1d hmin hmax bins . VS.toList) eachFbin)
@@ -64,10 +97,10 @@ mkChunks vIn n = mkChunksCore vIn n (VS.length vIn `div` n)
 
 
 histogram1d :: Double -> Double -> [Double] -> [Double] -> ([Double], [Double])
-histogram1d xmin xmax bins input = do
+histogram1d xmin xmax bins input =
   let intervals = zipWith (\x y ->(x, y)) (init bins) (tail bins)
       within u x = x >= fst u && x < snd u
-   in (map fst intervals, map (fromIntegral.length) $ map (\u -> filter (within u) input) intervals)
+   in (map fst intervals, map ((fromIntegral.length) . (\u -> filter (within u) input)) intervals)
 
 
 
