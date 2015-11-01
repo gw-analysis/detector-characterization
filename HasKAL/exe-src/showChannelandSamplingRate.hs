4ca15096a882c5009032f207e253d248f96a093d
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-11-01 18:34:11 +0900

    added commandline scripts showChannelandSamplingRate.hs showChannels.hs

---
+++ HasKAL/exe-src/showChannelandSamplingRate.hs
@@ -0,0 +1,14 @@
+
+
+import HasKAL.FrameUtils.FrameUtils (getChannelList)
+import System.Environment (getArgs)
+import System.IO (stdout, hPutStrLn)
+
+main = do
+  fname <- getArgs >>= \args -> case (length args) of
+    1 -> return (head args)
+    _ -> error "Usage: showChannels filename"
+  getChannelList fname >>= \maybech -> case maybech of
+    Nothing -> error "no channel in the file"
+    Just x -> mapM_ (\(y, z)-> hPutStrLn stdout $ y++" "++show z) x
+
