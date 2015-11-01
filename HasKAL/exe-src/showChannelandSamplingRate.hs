5c0e25d080e0aa362f16665a8a2baf7a6c5f9157
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-11-01 18:36:22 +0900

    fixed error message

--- HasKAL/exe-src/showChannelandSamplingRate.hs
+++ HasKAL/exe-src/showChannelandSamplingRate.hs
@@ -7,7 +7,7 @@ import System.IO (stdout, hPutStrLn)
 main = do
   fname <- getArgs >>= \args -> case (length args) of
     1 -> return (head args)
-    _ -> error "Usage: showChannels filename"
+    _ -> error "Usage: showChannelandSamplingRate filename"
   getChannelList fname >>= \maybech -> case maybech of
     Nothing -> error "no channel in the file"
     Just x -> mapM_ (\(y, z)-> hPutStrLn stdout $ y++" "++show z) x
