c3594bfe920389f766484ece658fe09e4798756c
amiyamoto <amiyamoto@yukimura.hep.osaka-cu.ac.jp>
2015-11-11 05:01:46 +0900

    CGI that generate HTML is modified

--- attic/CGI/dailySubSys.cgi.hs
+++ attic/CGI/dailySubSys.cgi.hs
@@ -37,7 +37,7 @@ html subSys date = concat [
     {- "DMG", "DAS", "GIF", "DEC"  -}
     "Bruco"],
   "<td align=center valign=middle width=98 height=30>",
-  "<div align=center><a href=\"../main_frame_1.html\">Web Tools</a></div>",
+  "<div align=center><a href=\"webToolFrame.cgi\">Web Tools</a></div>",
   "</td>",
   "</tr>",
   "</table>",
---
+++ attic/CGI/webToolFrame.cgi.hs
@@ -0,0 +1,63 @@
+
+import Network.CGI
+
+import HasKAL.TimeUtils.GPSfunction (getCurrentGps, gps2localTimetuple)
+
+
+main :: IO ()
+main = runCGI $ handleErrors cgiMain
+
+cgiMain :: CGI CGIResult
+cgiMain = do
+  gps <- return "1120543424" --liftIO getCurrentGps
+  portal <- getInput "portal"
+  output $ html portal gps
+
+html :: Maybe String -> String -> String
+html portal gps = concat [
+  "<!DOCTYPE html>",
+    "<html>",
+      "<head>",
+        "<title>HasKAL</title>",
+      "</head>",
+      "<frameset rows=\"100,*\">",
+        "<frame src=\"./webToolPortal.cgi"++uri portal++"\" name=\"portal\">",
+          "<frameset cols=\"300,*\">",
+          "<frame src=\""++(func portal)++"\" name=\"input\">",
+          "<frame src=\""++(func2 portal gps)++"\" name=\"plotframe\">",
+          "</frameset>",
+      "</frameset>",
+  "</html>"
+  ]
+  where uri mbx = case mbx of
+                   Just "" -> "?portal=Single Channel Analysis"
+                   Just x  -> "?portal="++x++"&"
+                   Nothing -> "?"
+            
+        func :: Maybe String -> String
+        func portal = case portal of
+                       Just "Single Channel Analysis" -> "date_1.cgi"
+                       Just "Coherence Analysis"      -> "date_2.cgi"
+                       Just "Correlation Map"         -> "date_3.cgi"
+                       Just "Bruco"                   -> "date_4.cgi"
+                       Just "Detection Range"         -> "date_5.cgi"
+                       Nothing                        -> "date_1.cgi"
+
+
+        func2 :: Maybe String -> String ->  String
+        func2 portal gps = case portal of
+                            Just "Single Channel Analysis" -> "date_1.cgi?Date=GPS&gps="++gps++"&channel1="++ch1
+                                                                ++"&monitor=TS\" name=\"plotframe\">"
+                            Just "Coherence Analysis"      -> "date_2.cgi?Date=GPS&gps="++gps++"&channel1="++ch1
+                                                                ++"&channel2="++ch2++"&monitor=COH\" name=\"plotframe\">"
+                            Just "Correlation Map"         -> "date_3.cgi?Date=GPS&gps="++gps++"&channel1="++ch1
+                                                                ++"&channel1="++ch2++"&monitor=Peason\" name=\"plotframe\">"
+                            Just "Bruco"                   -> "" 
+                            Just "Detection Range"         -> "date_5.cgi?Date=GPS&gps="++gps
+                                                           ++"&monitor=INSP\" name=\"plotframe\">"
+                            Nothing                        -> "date_1.cgi?Date=GPS&gps="++gps++"&channel1="++ch1 
+                                                                ++"&monitor=TS\" name=\"plotframe\">"            
+        ch1 = "K1:PEM-EX_ACC_NO2_X_FLOOR"
+        ch2 = "K1:PEM-EX_ACC_NO2_Y_FLOOR"
+
+
