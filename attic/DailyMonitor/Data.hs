5a478ecee0e15758d06d281f3f20c7c62ec241b6
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-11-13 22:26:01 +0900

    added integrated Monitor type

--- attic/DailyMonitor/Data.hs
+++ attic/DailyMonitor/Data.hs
@@ -6,4 +6,5 @@ where
 data Monitor = TimeSeries | Spectrum | Spectrogram
              | RMSMon     | SensMon  | LTF
              | RMon       | SRMon    | LTA
+  deriving ( Eq, Ord, Show, Read)
 
--- attic/DailyMonitor/genDailySummaryPage.hs
+++ attic/DailyMonitor/genDailySummaryPage.hs
@@ -1,7 +1,10 @@
 
 
+import Data.List (sort,elemIndices)
 import HasKAL.WebUtils.DailySummaryPage (genDailySummaryPage)
 import System.Environment (getArgs)
+import Data
+
 
 main = do
   (dir, date, chlist, monlist, subsystem, ncol) <- getArgs >>= \args -> case (length args) of
@@ -9,8 +12,11 @@ main = do
     _ -> error "Usage: genDailySummaryPage dir date chlist monlist subsystem ncol"
   chs <- readFile chlist >>= \x -> return $ removeCommentLines (lines x)
   mons <- readFile monlist >>= \x -> return $ removeCommentLines (lines x)
-  genDailySummaryPage dir date chs mons subsystem (read ncol::Int)
+  let mons' = map show $ sort $ map (\x->read x :: Monitor) mons
+  genDailySummaryPage dir date chs mons' subsystem (read ncol::Int)
 
 
-removeCommentLines = filter (\elem -> (head . head . words $ elem) /= '#')
+removeCommentLines = concatMap (\x-> words $ take (head' $ elemIndices '#' x) x)
+head' x | x ==[] = 0 | otherwise = head x
+
 
