8ce9a2cf507d2ecd5b57dac265abf65ffaadbd61
yuzuri <yuzurihara@yukimura.hep.osaka-cu.ac.jp>
2015-11-17 09:07:52 +0900

    correlation monitor : update sample code

--- attic/CorrelationAnalysis/check_correlation_chunk.hs
+++ attic/CorrelationAnalysis/check_correlation_chunk.hs
@@ -15,7 +15,7 @@ main = do
  args <- getArgs
  (year, month, day, ch1, ch2) <- case length args of
     5 ->  return (args!!0, show0 2 (args!!1), show0 2 (args!!2), args!!3, args!!4)
-    _ ->  error "Usage: check_correlation_chunk yyyy mm dd ch1 ch2\n\n check_correlation_chunk 2015 7 15 K1:PEM-EX_MAG_X_FLOOR K1:PEM-EX_MAG_Y_FLOOR"
+    _ ->  error "Usage: check_correlation_chunk yyyy mm dd ch1 ch2\n\ncheck_correlation_chunk 2015 7 15 K1:PEM-EX_MAG_X_FLOOR K1:PEM-EX_MAG_Y_FLOOR"
 
 
  {-- parameters --}
@@ -48,9 +48,10 @@ main = do
  let xlabel = "hour[h] since "  ++  show jst :: String
      ylabel = "Peason rho" :: String
      ylabel2 = "rho's time shift" :: String
- plotV Linear LinePoint 1 RED (xlabel, ylabel) 0.05 "test-dayo" "correlation_time-rho.png" ((0, 0),(0, 0)) (time, rho)
- plotV Linear LinePoint 1 RED (xlabel, ylabel2) 0.05 "test-dayo" "correlation_time-tshift.png.png" ((0, 0),(0, 0)) (time, tshift)
- print 1
+     title = ch1 ++ " - " ++ ch2 :: String
+ plotV Linear LinePoint 1 RED (xlabel, ylabel) 0.05 title "correlation_time-rho.png" ((0, 0),(0, 0)) (time, rho)
+ plotV Linear LinePoint 1 RED (xlabel, ylabel2) 0.05 title "correlation_time-tshift.png.png" ((0, 0),(0, 0)) (time, tshift)
+-- print rho
 
 
 {-- Internal Functions --}
@@ -59,25 +60,4 @@ show0 digit number
   | len < digit = (concat $ replicate (digit-len) "0") ++ number
   | otherwise   = number
   where len = length number
-
-
-
---  let data1 = NLA.fromList [1..30] :: NLA.Vector Double
---      data2 = NLA.fromList [5,2,3,1,57,4,2,4,5,7, 5,2,3,1,57,4,2,4,5,7, 2,3,1,57,4,2,4,5,7,8] :: NLA.Vector Double
---      data3 = NLA.fromList [2,3,1,57,4,2,4,5,7,8, 2,3,1,57,4,2,4,5,7,8, 2,3,1,57,4,2,4,5,7,8] :: NLA.Vector Double
-
--- -- let rValue2 = takeCorrelationV Peason data2 data1 2
--- -- print rValue2
-
---  let (time, rho, tshift) = correlationChunkV Peason data2 data1 30 10 1 5 :: (S.Vector Double, S.Vector Double, S.Vector Double) 
--- --plotV :: LogOption -> PlotTypeOption -> Int -> ColorOpt -> (String, String) -> Double -> String -> String -> ((Double, Double), (Double, Double)) -> Spectrum -> IO ()
---  plotV Linear LinePoint 1 RED ("time", "rho") 0.05 "test-dayo" "hoge.png" ((0, 0),(0, 0)) (time, rho)
---  print (time, rho, tshift)
-
---  let (time2, rho2, tshift2) = correlationChunkV Peason data2 data1 30 10 1 5 :: (S.Vector Double, S.Vector Double, S.Vector Double) 
---  plotV Linear LinePoint 1 RED ("time", "rho") 0.05 "test-dayo" "hoge2.png" ((0, 0),(0, 0)) (time2, rho2)
---  print (time2, rho2, tshift2)
-
-
-
 
