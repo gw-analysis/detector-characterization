e9f53546cee22996e447dbed24c9badbe4faa6e9
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-11-19 18:07:53 +0900

    nohup RegistDB succeeded

---
+++ attic/database/RegistDB.hs
@@ -0,0 +1,61 @@
+
+
+
+--module RegistDB
+--where
+
+import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
+import Control.Monad.State (liftIO)
+import Data.Conduit (Sink, Source, await, yield, ($$))
+import Data.Text (pack)
+import Filesystem.Path (extension)
+import Filesystem.Path.CurrentOS (decodeString, encodeString)
+import System.Environment (getArgs)
+import System.FSNotify (withManager, watchTree, Event(..), eventPath)
+
+
+--runRegistDB watchdir = source watchdir $$ sink
+
+main = do
+  dirname <- getArgs >>= \args -> case (length args) of
+   1 -> return (head args)
+   _ -> error "Usage : runRegistDB dir"
+  source dirname $$ sink
+
+source :: FilePath
+       -> Source IO FilePath
+source watchdir = do
+  x <- liftIO $ withManager $ \manager -> do
+    fname <- liftIO newEmptyMVar
+    _ <- watchTree manager watchdir (const True)
+      $ \event -> case event of
+        Removed _ _ -> print "file removed"
+        _           -> case extension (decodeString $ eventPath event) of
+                         Just ext -> if (ext==filepart) 
+                           then 
+                             print "file downloading"
+                           else if (ext==gwf)
+                             then do
+                               let gwfname = eventPath event
+                               putMVar fname gwfname
+                             else
+                               print "file extension should be .filepart or .gwf"
+    takeMVar fname
+  yield x >> source watchdir
+  where filepart = pack "filepart"
+        gwf = pack "gwf"
+  
+
+sink :: Sink String IO ()
+sink = do
+  c <- await
+  case c of 
+    Nothing -> return () -- liftIO $ print "Nothing" 
+    Just fname -> do liftIO $ print fname 
+                     sink
+
+
+
+
+
+
