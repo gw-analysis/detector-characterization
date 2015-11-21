c187a1b42a953acc034beb7a3163f492f03858ff
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2015-11-21 22:59:45 +0900

    added new function
    histogram of nu(f)

--- HasKAL/src/HasKAL/MonitorUtils/SRMon/StudentRayleighMon.hs
+++ HasKAL/src/HasKAL/MonitorUtils/SRMon/StudentRayleighMon.hs
@@ -5,9 +5,13 @@ module HasKAL.MonitorUtils.SRMon.StudentRayleighMon (
    FitMethod(LSM, MLE, QUANT)
   ,studentRayleighMon
   ,studentRayleighMonV
+  ,srMonNuHist
 ) where
 
+import Prelude as P
 import System.IO.Unsafe (unsafePerformIO)
+import qualified Data.Packed.Vector as PV (subVector, fromList, toList, dim)
+import qualified Data.Packed.Matrix as PM (cols, rows, toRows, fromColumns)
 import Data.Vector.Unboxed as V
 import Data.Matrix.Unboxed as M hiding ((!), convert)
 import qualified Control.Monad as CM (forM)
@@ -53,6 +57,17 @@ studentRayleighMonV method fsample stride chunck shift clusteringF (freqV1, spec
       newTV = fromList [0, dt .. dt * fromIntegral (cols newSpecM - 1)]
       newFV = fromList [df, df*2 .. df * fromIntegral (rows newSpecM)]
   (convert newTV, convert newFV, convertU2S newSpecM)
+
+srMonNuHist :: Spectrogram -- ^ nu(t, f)    : output of studentRayleighMonV
+            -> Spectrogram -- ^ yield(f, nu): for histogram plot
+srMonNuHist (tV, fV, nuM) = (fV, PV.fromList $ P.init bins, yield)
+  where bins = [1.0,2..201]
+        yield = PM.fromColumns . P.map (PV.fromList . snd . histogram1d 0 0 bins . PV.toList) . PM.toRows $ nuM
+        histogram1d xmin xmax bins input =
+          let intervals = P.zipWith (\x y ->(x, y)) (P.init bins) (P.tail bins)
+              within u x = x >= fst u && x < snd u
+           in (P.map fst intervals, P.map ((fromIntegral.P.length) . (\u -> P.filter (within u) input)) intervals)
+
 
 
 {-- Internal Functions --}
