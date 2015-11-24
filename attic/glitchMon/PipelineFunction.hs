70aa6b20301bee5708f907305d606be7ea9847df
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-11-24 20:35:44 +0900

    adding cluster id

--- attic/glitchMon/PipelineFunction.hs
+++ attic/glitchMon/PipelineFunction.hs
@@ -15,13 +15,35 @@ import HasKAL.FrameUtils.FrameUtils
 import Numeric.LinearAlgebra
 import HasKAL.TimeUtils.Signature(GPSTIME)
 import Data.Int (Int32)
-
+import qualified Data.Set as Set
 
 excludeOnePixelIsland :: [(Int, Int)] -> [(Int, Int)]
 excludeOnePixelIsland [] = []
 excludeOnePixelIsland y@(x:xs) = case (length (intersect y (basePixel25 x)) > 2) of
   True -> intersect y (basePixel5 x) ++ excludeOnePixelIsland xs
   False-> excludeOnePixelIsland xs
+
+
+addID :: [(Int, Int)] -> ([(Int, Int, Int)],Int)
+addID [] = ([],0)
+addID z = runState (go z z) 0
+  where 
+    go y (x:xs) = do 
+      ind <- get
+      let a = intersect y (basePixel25 x))
+      case (length a > 2) of
+        True -> do put (succ ind)
+                   [(p,q,ind)|(p,q)<- a]:go y xs
+        False-> go y xs
+
+
+regroup :: Int [(Int, Int, Int)] -> [[(Int, Int, Int)]]
+regroup n y@((a,b,c):xs)  = do
+  let tagertID = [y3|(y1,y2,y3) <- y, y1==a,y2==b] 
+      grouped = concatMap (\x->filter (\(_,_,r)->r==x) y) targetID
+      grouped' = [(a,b,n)| (a,b,_)<-gouped]
+      newlist =  Set.difference (Set.fromList gouped) (Set.fromList y)
+   in grouped' ++ regroup (succ n) newlist
 
 
 basePixel9 :: (Int, Int) -> [(Int, Int)]
@@ -34,6 +56,10 @@ basePixel5 (a, b) = [(a-1, b), (a, b), (a+1, b), (a, b-1), (a, b+1)]
 
 basePixel25 :: (Int, Int) -> [(Int, Int)]
 basePixel25 (a, b) = [(a-2, b-2), (a-1, b-2), (a, b-2), (a+1, b-2), (a+2, b-2), (a-2, b-1), (a-1, b-1), (a, b-1), (a+1, b-1), (a+2, b-1), (a-2, b), (a-1, b), (a, b), (a+1, b), (a+2, b), (a-2, b+1), (a-1, b+1), (a, b+1), (a+1, b+1), (a+2, b+1), (a-2, b+2), (a-1, b+2), (a, b+2), (a+1, b+2), (a+2, b+2)]
+
+
+basePixel25' :: (Int, Int, Int) -> [(Int, Int, Int)]
+basePixel25' (a, b, c) = [(a-2, b-2, c), (a-1, b-2, c), (a, b-2, c), (a+1, b-2, c), (a+2, b-2, c), (a-2, b-1, c), (a-1, b-1, c), (a, b-1, c), (a+1, b-1, c), (a+2, b-1, c), (a-2, b, c), (a-1, b, c), (a, b, c), (a+1, b, c), (a+2, b, c), (a-2, b+1, c), (a-1, b+1, c), (a, b+1, c), (a+1, b+1, c), (a+2, b+1, c), (a-2, b+2, c), (a-1, b+2, c), (a, b+2, c), (a+1, b+2, c), (a+2, b+2, c)]
 
 
 dataInfo :: String -> String -> IO (Maybe (Vector Double, Double, GPSTIME, Double))
