3ee81dab6eb436d5c0ac7eb6ec7a06f79f5f5444
enoshima <kazuhiro.hayama@gmail.com>
2015-12-01 16:53:51 +0900

    added both grey scale

--- attic/Morphology/Function.hs
+++ attic/Morphology/Function.hs
@@ -12,7 +12,8 @@ module Function
 
 
 
-import Data.List (maximum, minimum)
+import Data.List (maximum, minimum, lookup)
+import Data.Maybe (fromJust)
 import Data.Set (empty, member, insert, intersection,isSubsetOf, fromList, toList)
 
 
@@ -23,31 +24,59 @@ complement x = let maxx = maximum [y|(_, _, y)<-x]
                 in [(b, c, maxx+minx-d)|(b, c, d)<-x]
 
 
-dilationBinary :: [(Int,Int)] ->  [(Int,Int)] -> [(Int,Int)]
-dilationBinary b x = nub' [(b1+x1,b2+x2)|(b1,b2)<-b,(x1,x2)<-x]
+dilationBinary :: [(Int,Int)] -> [(Int, Int)] ->  [(Int,Int)] -> [(Int,Int)]
+dilationBinary z b x = toList $ intersection (fromList (nub' [(b1+x1,b2+x2)|(b1,b2)<-b,(x1,x2)<-x]))
+                                             (fromList z)
 
 
-erosionBinary :: [(Int,Int)] -> [(Int,Int)] -> [(Int,Int)]
-erosionBinary b x =  filter (\y-> isSubsetOf (fromList [(x1+b1,x2+b2)|(b1,b2)<-b,let x1 = fst y, let x2 = snd y]) (fromList x)) x
+erosionBinary :: [(Int,Int)] -> [(Int, Int)] -> [(Int,Int)] -> [(Int,Int)]
+erosionBinary z b x = toList $ intersection
+  (fromList (filter (\y-> isIn b x y) x))
+  (fromList z)
+  where isIn b x y = isSubsetOf (fromList [ (y1+b1,y2+b2)
+                                          | (b1,b2)<-b
+                                          , let y1 = fst y
+                                          , let y2 = snd y
+                                          , y1+b1>=b1L
+                                          , y1+b1<=b1U
+                                          , y2+b2>=b2L
+                                          , y2+b2<=b2U]) (fromList x)
+        b1L = minimum . fst . unzip $ z
+        b1U = maximum . fst . unzip $ z
+        b2L = minimum . snd . unzip $ z
+        b2U = maximum . snd . unzip $ z
 
 
 erosionGrey :: (Show a, Eq a, Num a, Ord a) => [((Int,Int),a)] -> [((Int,Int),a)] -> [((Int,Int),a)]
-erosionGrey s x = let (ind, valz) = unzip $ intersected s x
-                   in zip ind $ replicate (length valz) (minimum valz)
+erosionGrey s x = (flip map) x $ \a->updateIntersection minimum s x a
 
 
 dilationGrey:: (Show a, Eq a, Num a, Ord a) => [((Int,Int),a)] -> [((Int,Int),a)] -> [((Int,Int),a)]
-dilationGrey s x = let (ind, valz) =  unzip $ intersected s x
-                    in zip ind $ replicate (length valz) (maximum valz)
+dilationGrey s x = (flip map) x $ \a->updateIntersection maximum s x a
 
 
-intersected :: (Show a, Eq a, Num a, Ord a) => [((Int,Int),a)] -> [((Int,Int),a)] -> [((Int,Int),a)]
-intersected s x = filter (\a->isIntersect a s x) x
+updateIntersection :: (Show a, Eq a, Num a, Ord a)
+                   => ([a] -> a)
+                   -> [((Int,Int),a)]
+                   -> [((Int,Int),a)]
+                   -> ((Int,Int),a)
+                   -> ((Int,Int),a)
+updateIntersection f s x a = let axs = [ (a1+s1,a2+s2)
+                                       | ((s1,s2),1)<-s
+                                       , let a1 = fst . fst $ a
+                                       , let a2 = snd . fst $ a
+                                       , a1+s1>=x1L
+                                       , a1+s1<=x1U
+                                       , a2+s2>=x2L
+                                       , a2+s2<=x2U]
+                                 faxs = f $ (flip map) axs $ \y-> fromJust (lookup y x)
+                              in ((fst . fst $ a, snd . fst $ a), faxs)
+
  where
-  isIntersect a s x =
-   not . null . toList $
-    intersection (fromList [(a1+s1,a2+s2)|((s1,s2),_)<-s,let a1=fst (fst a),let a2=snd (fst a)])
-                 (fromList [(x1, x2)|((x1, x2), _)<-x])
+  x1L = minimum . fst . unzip . fst . unzip $ x
+  x1U = maximum . fst . unzip . fst . unzip $ x
+  x2L = minimum . snd . unzip . fst . unzip $ x
+  x2U = maximum . snd . unzip . fst . unzip $ x
 
 
 
