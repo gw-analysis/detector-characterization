4602f1d5e1a4b84049ce2fd36a3bd97edbaf4a41
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-12-02 09:16:11 +0900

    error handling

--- HasKAL/src/HasKAL/SignalProcessingUtils/Resampling.hs
+++ HasKAL/src/HasKAL/SignalProcessingUtils/Resampling.hs
@@ -10,18 +10,18 @@ module HasKAL.SignalProcessingUtils.Resampling
 , downsampleWaveData
 ) where
 
+
+import Control.Monad (when)
+import Control.Monad.ST (ST)
+import Data.Vector.Generic.Mutable (unsafeWrite,new)
+import qualified Data.Vector.Storable as SV
+import qualified Data.Vector.Unboxed as UV
 import HasKAL.SignalProcessingUtils.Filter
 import HasKAL.SignalProcessingUtils.FilterType
 import HasKAL.SignalProcessingUtils.ButterWorth
 import HasKAL.TimeUtils.Function
 import HasKAL.WaveUtils.Data
 import Numeric.LinearAlgebra
-import qualified Data.Vector.Unboxed as UV
-import qualified Data.Vector.Storable as SV
-import Control.Monad (when)
-import Control.Monad.ST (ST)
-import Data.Vector.Generic.Mutable(unsafeWrite,new)
-
 
 
 downsample :: Double -> Double -> [Double] -> [Double]
@@ -44,8 +44,8 @@ downsampleWaveData :: Double -> WaveData -> WaveData
 downsampleWaveData newfs x = y
   where y = x
         samplingFrequency y = newfs
-        gwdata y = downsampleSV fs newfs gwx'
         fs = samplingFrequency x
+        gwdata y = downsampleSV fs newfs gwx'
         gwx' = iir lpf (gwdata x)
         lpf = butter 2 fs (newfs'/2) Low
         newfs' = 2*fs*tan (pi*newfs/fs)
@@ -54,7 +54,9 @@ downsampleWaveData newfs x = y
 
 downsampleV :: Double -> Double -> Vector Double -> Vector Double
 downsampleV fs newfs x = y
-  where y = fromList $ snd.unzip $ filter (\(n, _) -> n `mod` p == 1) $ zip [1..] $ toList x'
+  where y = if (p>=1) 
+             then fromList $ snd.unzip $ filter (\(n, _) -> n `mod` p == 1) $ zip [1..] $ toList x'
+             else error "new sample rate should be <= original sample rate."
         p = truncate (fs/newfs)
         x' = iir lpf x
         lpf = butter 2 fs (newfs'/2) Low
@@ -62,35 +64,43 @@ downsampleV fs newfs x = y
 
 
 downsampleUV :: Double -> Double -> UV.Vector Double -> UV.Vector Double
-downsampleUV fs newfs v = UV.create $ do 
-  vs <- new nvs
-  let v' =  UV.convert $ iir lpf $ UV.convert v
-      lpf = butter 2 fs (newfs'/2) Low
-      newfs' = 2*fs*tan (pi*newfs/fs)
-  loop v' vs 0 nvs
-  return vs
-  where 
-    n = UV.length v
-    p = truncate $ fs/newfs
-    nvs = n `div` p
-    loop v vs i j = when (i < j) $ do
-      unsafeWrite vs i (v UV.!(i*p))
-      loop v vs (i+1) j
+downsampleUV fs newfs v = 
+  if (fs/newfs<1) 
+    then error "new sample rate should be <= original sample rate."
+    else
+      UV.create $ do 
+        vs <- new nvs
+        let v' =  UV.convert $ iir lpf $ UV.convert v
+            lpf = butter 2 fs (newfs'/2) Low
+            newfs' = 2*fs*tan (pi*newfs/fs)
+        loop v' vs 0 nvs
+        return vs
+        where 
+          n = UV.length v
+          p = truncate $ fs/newfs
+          nvs = n `div` p
+          loop v vs i j = when (i < j) $ do
+            unsafeWrite vs i (v UV.!(i*p))
+            loop v vs (i+1) j
   
 
 downsampleSV :: Double -> Double -> SV.Vector Double -> SV.Vector Double
-downsampleSV fs newfs v = SV.create $ do 
-  vs <- new nvs
-  let v' = iir lpf v
-      lpf = butter 2 fs (newfs'/2) Low
-      newfs' = 2*fs*tan (pi*newfs/fs)
-  loop v' vs 0 nvs
-  return vs
-  where 
-    n = SV.length v
-    p = truncate $ fs/newfs
-    nvs = n `div` p
-    loop v vs i j = when (i < j) $ do
-      unsafeWrite vs i (v SV.!(i*p))
-      loop v vs (i+1) j
- 
+downsampleSV fs newfs v = 
+  if (fs/newfs<1) 
+    then error "new sample rate should be <= original sample rate."
+    else
+      SV.create $ do 
+        vs <- new nvs
+        let v' = iir lpf v
+            lpf = butter 2 fs (newfs'/2) Low
+            newfs' = 2*fs*tan (pi*newfs/fs)
+        loop v' vs 0 nvs
+        return vs
+        where 
+          n = SV.length v
+          p = truncate $ fs/newfs
+          nvs = n `div` p
+          loop v vs i j = when (i < j) $ do
+            unsafeWrite vs i (v SV.!(i*p))
+            loop v vs (i+1) j
+       
