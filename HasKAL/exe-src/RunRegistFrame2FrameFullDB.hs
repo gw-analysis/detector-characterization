05e316a6a3b877dfabfc0d71299b4f6b40d0b327
kagra-detchar <kagra-detchar-dev@icrr.u-tokyo.ac.jp>
2015-12-03 13:48:34 +0900

    refactoring

--- HasKAL/exe-src/RunRegistFrame2FrameFullDB.hs
+++ HasKAL/exe-src/RunRegistFrame2FrameFullDB.hs
@@ -6,18 +6,21 @@
 
 import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
 import Control.DeepSeq (deepseq)
+import Control.Monad (forever)
 import Control.Monad.State (liftIO)
 import Data.Conduit (Sink, Source, await, yield, ($$))
 import Data.List (elemIndices)
 import Data.Text (pack)
-import Filesystem.Path (extension)
+import Filesystem.Path (extension, (</>))
 import Filesystem.Path.CurrentOS (decodeString, encodeString)
 import HasKAL.DataBaseUtils.FrameFull.DataBaseAdmin (updateFrameDB')
 import System.Environment (getArgs)
-import System.FSNotify (Debounce(..), Event(..), WatchConfig(..), withManagerConf, watchTree, eventPath)
+import System.FSNotify (Debounce(..), Event(..), WatchConfig(..), withManagerConf, watchTree, watchTreeChan, eventPath)
 import System.IO (hFlush, stdout)
-
-
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.Chan
+import System.FilePath.Posix (takeExtension, takeFileName)
+import HasKAL.TimeUtils.GPSfunction (getCurrentGps)
 --runRegistDB watchdir = source watchdir $$ sink
 
 main = do
@@ -26,28 +29,56 @@ main = do
    _ -> error "Usage : RunRegistFrame2FrameFullDB dir"
   source dirname $$ sink
 
+
 source :: FilePath
        -> Source IO FilePath
-source watchdir = do
+source watchTopdir = do
+  let predicate event' = case event' of
+        Added path _ -> head (takeFileName path) /= '.'
+        _            -> False
   let config = WatchConfig
-                 { confDebounce = DebounceDefault
-                 , confPollInterval = 20000000 -- 20seconds
+                 { confDebounce = NoDebounce
+                 , confPollInterval = 5000000 -- 5seconds
                  , confUsePolling = True
                  }
   x <- liftIO $ withManagerConf config $ \manager -> do
     fname <- liftIO newEmptyMVar
-    _ <- watchTree manager watchdir (const True)
+    dirname <- getCurrentGps >>= \x -> return $ take 5 (show ((read x ::Int)-10))
+    _ <- watchTree manager (encodeString $ decodeString watchTopdir </> decodeString dirname) predicate
       $ \event -> case event of
-        Removed _ _ -> putStrLn "file removed" >> hFlush stdout
-        _           -> do let gwfname = eventPath event
-                          case (length (elemIndices '.' gwfname)) of
-                            1 -> putMVar fname gwfname
-                            2 -> putStrLn "file saving" >> hFlush stdout
-                            _ -> putStrLn "file extension should be .filepart or .gwf" >> hFlush stdout
+        Added path _ -> case (takeExtension path `elem` [".gwf"] && head (takeFileName path) /= '.') of
+                          True  -> putMVar fname path
+                          False -> putStrLn "something strange happens." >> hFlush stdout
     takeMVar fname
-  yield x >> source watchdir
-  where filepart = pack "filepart"
-        gwf = pack "gwf"
+  liftIO $ putStrLn x >> hFlush stdout
+  yield x >> source watchTopdir
+
+
+source' :: FilePath
+       -> Source IO FilePath
+source' watchdir = do
+  let predicate event' = case event' of
+        Added path _ -> takeExtension path `elem` [".gwf"] && head (takeFileName path) /= '.'
+        _            -> False
+  eventChan <- liftIO newChan
+  let config = WatchConfig
+                 { confDebounce = NoDebounce
+                 , confPollInterval = 5000000 -- 5seconds
+                 , confUsePolling = True
+                 }
+  _ <- liftIO $ forkIO $ withManagerConf config $ \manager -> do
+    _<-watchTreeChan 
+       manager 
+       watchdir 
+       predicate
+       eventChan
+--    return ()
+    forever $ threadDelay 10000000
+  event <- liftIO $  readChan eventChan 
+  let gwfname = eventPath event
+  _<- liftIO $ putStrLn gwfname >> hFlush stdout 
+  source watchdir
+--  yield x >> source watchdir
 
 
 sink :: Sink String IO ()
