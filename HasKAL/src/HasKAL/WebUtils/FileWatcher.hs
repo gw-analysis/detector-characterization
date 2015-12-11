833462d3b63ba352c1e7eab19531e40ba8fc104d
enoshima <kazuhiro.hayama@gmail.com>
2015-12-11 16:10:57 +0900

    added hrsync

--- HasKAL/src/HasKAL/WebUtils/FileWatcher.hs
+++ HasKAL/src/HasKAL/WebUtils/FileWatcher.hs
@@ -1,46 +1,62 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module HasKAL.WebUtils.FileWatcher
-( watchNewfile
+( hrsync
 , watchNewfilewDB
 , updatingDB
 ) where
 
-import Control.Concurrent
-import Filesystem (getWorkingDirectory)
-import System.FSNotify
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.Chan (newChan, readChan)
+import Control.Monad (forever)
+import Control.Monad.State (liftIO)
 import System.Exit
 import System.IO.Error
 import Filesystem.Path
 import Filesystem.Path.CurrentOS (decodeString, encodeString)
+import System.FilePath.Posix (takeFileName)
+import System.FSNotify ( Debounce(..)
+                       , Event(..)
+                       , WatchConfig(..)
+                       , withManager
+                       , withManagerConf
+                       , watchDir
+                       , watchTreeChan
+                       , watchDirChan
+                       , eventPath
+                       )
+
 import System.Process (rawSystem)
-import Data.Text
+import Data.Text hiding (head)
 
 
-watchNewfile :: String            -- | executive filename
-             -> String            -- | location where index.html will be
-             -> String            -- | location to watch new file added
+hrsync :: String            -- | executive filename
+             -> String            -- | dir to watch
+             -> String            -- | dir to save
              -> IO ()
-watchNewfile f webhomedir watchdir = do
-  withManager $ \manager -> do
-    watchDir manager watchdir (const True)
-      $ \event -> case event of
-        Removed _ _ -> print "file removed"
-        _ -> case extension (decodeString $ eventPath event) of
-               Just "filepart" -> print "file downloading"
-               Just "gwf" -> do
-                 let gwfname = eventPath event
-                 print gwfname
-                 rawSystem f [webhomedir, gwfname]
-                 print "event display updated."
-               Nothing -> print "file extension should be .filepart or .gwf"
---      $ \event -> do rawSystem f [webhomedir, "/home/detchar/xend/K-K1_R-1113209036-32.gwf"]
---      $ \event -> channelPlot' param (encodeString $ eventPath event) >>= genWebPage'
-    waitBreak
-  where
-    waitBreak = do
-      _ <- catchIOError getLine (\e -> if isEOFError e then exitSuccess else exitFailure)
-      waitBreak
+hrsync f from to = do
+  let predicate event' = case event' of
+        Add path _ -> chekingFile path
+        _          -> False
+      config = WatchConfig
+                 { confDebounce = NoDebounce
+                 , confPollingInterval = 5000000 -- 5seconds
+                 , confUsePolling = True
+                 }
+  eventChan <- liftIO newChan
+  withManagerConf config $ \manager -> do
+    watchTreeChan
+      manager
+      from
+      predicate
+      eventChan
+    forever $ threadDelay 10000000
+  event <- liftIO $ readChan eventChan
+  let fname = eventPath event
+  rawSystem f [to, fname]
+
+
+chekingFile path = head (takeFileName path) /= '.'
 
 
 watchNewfilewDB :: String            -- | command name
