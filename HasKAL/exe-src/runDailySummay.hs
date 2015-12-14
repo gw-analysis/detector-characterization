6b0606b4e24f2ea19b7fa8fbebb75b281435103d
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-12-14 20:17:04 +0900

    added better line

--- HasKAL/exe-src/runDailySummay.hs
+++ HasKAL/exe-src/runDailySummay.hs
@@ -1,19 +1,21 @@
 
 
 import Control.Concurrent (threadDelay)
+import HasKAL.TimeUtils.GPSfunction (getCurrentLocalTime)
 import System.Environment (getArgs)
 import System.IO (hFlush, stdout)
 import System.Process (rawSystem)
 
 main = do
-  f <- getArgs >>= \args -> case (length args) of
-    1 -> return (head args)
-    _ -> error " Usage: runDailySummary f"
-  go f
+  (f, opt) <- getArgs >>= \args -> case (length args) of
+    2 -> return (head args, args!!1)
+    _ -> error " Usage: runDailySummary f opt"
+  go f opt
 
 
-go f = do putStrLn "generating today's summary page" >> hFlush stdout
-          rawSystem f []
-          threadDelay (24*3600*1000000)
-          go f
+go f opt = do localTime <- getCurrentLocalTime "JST"
+              putStrLn ("generating daily summary page for "++localTime ++".") >> hFlush stdout
+              rawSystem f [opt]
+              threadDelay (24*3600*1000000)
+              go f opt
 
