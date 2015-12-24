8fac2315298456875a55a30eb352e2db614b817a
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-12-24 19:12:55 +0900

    CDFParam type separated into Data.hs

---
+++ HasKAL/src/HasKAL/DataBaseUtils/XEndEnv/Data.hs
@@ -0,0 +1,13 @@
+
+module HasKAL.DataBaseUtils.XEndEnv.Data
+where
+
+
+data CDFParam = CDFParam { cdf'samplingFrequency :: Double
+                         , cdf'cutoffFrequencyLow :: Double
+                         , cdf'cutoffFrequencyHigh :: Double
+                         , cdf'blockSize :: Int
+                         , cdf'fftSize :: Int
+                         , cdf'chunkSize :: Int
+                         }
+
