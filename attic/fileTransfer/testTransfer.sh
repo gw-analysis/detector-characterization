29359740adf4a5cdc0d090a68bb8877573313470
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2016-01-06 15:59:28 +0900

    spel mis fixed

--- attic/fileTransfer/testTransfer.sh
+++ attic/fileTransfer/testTransfer.sh
@@ -4,7 +4,7 @@ for var in `kagraDailyRawFileList ${YESTERDAY}`
 do
   scp ${var} ${MIRROR_SERVER}:/data/kagra/raw/full/
 　filename=`basename ${var}`
-  ssh ${MIRROR_SERVER} "updateFrameFull /data/lagra/raw/full/$var"
+  ssh ${MIRROR_SERVER} "updateFrameFull /data/kagra/raw/full/$var"
 done
 
 
