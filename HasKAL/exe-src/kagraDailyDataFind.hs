b4f94a8cae3d2d9b19a15d0877c114d7b29f57f8
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2016-01-07 11:25:45 +0900

    added command line function kagraDailyDataFind

---
+++ HasKAL/exe-src/kagraDailyDataFind.hs
@@ -0,0 +1,20 @@
+
+
+import HasKAL.DataBaseUtils.FrameFull.Function (kagraDailyFileList)
+import System.Environment (getArgs)
+import System.IO (stdout,  hPutStrLn)
+
+main = do
+  args <- getArgs
+  case length args of
+    2 -> do let day = head args 
+                loc = args !! 1
+            statement <- kagraDailyFileList day loc
+            case statement of
+              Nothing -> print "Nothing"
+              Just x -> mapM_ (hPutStrLn stdout) x
+    _ -> error "Usage: kagraDailyDataFind day(yyyy-mm-dd) loocation(JST,UTC,etc)"
+
+
+
+
