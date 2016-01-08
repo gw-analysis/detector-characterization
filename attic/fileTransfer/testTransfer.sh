da74db30c82d388e2cdcbe2f4ec34d0aeb593b47
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2016-01-08 23:37:31 +0900

    refactoring

--- attic/fileTransfer/testTransfer.sh
+++ attic/fileTransfer/testTransfer.sh
@@ -6,12 +6,14 @@
 #YESTERDAY=`date -d '1 day ago' "+%Y %m %d"`
 YESTERDAY="2015-12-19"
 MIRROR_SERVER="detchar@seikai.hep.osaka-cu.ac.jp"
-
-for var in `kagraDailyDataFind ${YESTERDAY} JST`
-do
-  echo scp ${var} ${MIRROR_SERVER}:/data/kagra/raw/full/
-  echo ssh ${MIRROR_SERVER} \"updateFrameFull /data/kagra/raw/full/`basename ${var}`\"
-done
-
+filelist=`kagraDailyDataFind ${YESTERDAY} JST`
+if test filelist = "Nothing"; then
+else
+  for var in ${filelist}
+  do
+    echo scp ${var} ${MIRROR_SERVER}:/data/kagra/raw/full/
+    echo ssh ${MIRROR_SERVER} \"updateFrameFull /data/kagra/raw/full/`basename ${var}`\"
+  done
+fi
 
 
