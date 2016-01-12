5b9eeeb1ed2e9f983393f19dbddf788b8c71c22c
ueno <ueno@seikai.hep.osaka-cu.ac.jp>
2016-01-12 17:52:51 +0900

    modify LT.mk

--- attic/kagali/main_nha.mk
+++ attic/kagali/main_nha.mk
@@ -8,13 +8,16 @@ LIBS=`pkg-config --libs-only-l ${LIB}`
 
 CSRC = HasKAL/ExternalUtils/KAGALI/DKGLUtils.c
 
-all: main_nha_txt main_nha_frame
+all: main_nha_txt main_nha_xend main_nha_raw
 
 main_nha_txt: main_nha_txt.hs $(CSRC)
 	${HC} -o $@ $^ ${CFLAGS} ${LDFLAGS} ${LIBS} ${CFLAGS2}
 
-main_nha_frame: main_nha_frame.hs $(CSRC)
+main_nha_xend: main_nha_xend.hs $(CSRC)
+	${HC} -o $@ $^ ${CFLAGS} ${LDFLAGS} ${LIBS} ${CFLAGS2}
+
+main_nha_raw: main_nha_raw.hs $(CSRC)
 	${HC} -o $@ $^ ${CFLAGS} ${LDFLAGS} ${LIBS} ${CFLAGS2}
 
 clean:
-	/bin/rm -f main_nha_txt main_nha_frame *.hi *.o
+	/bin/rm -f main_nha_txt main_nha_xend main_nha_raw *.hi *.o
---
+++ attic/kagali/main_nha_raw.hs
@@ -0,0 +1,69 @@
+
+--import HasKAL.FrameUtils.FrameUtils
+import HasKAL.FrameUtils.Function (readFrameV)
+
+import qualified Data.Vector.Storable as VS
+import Data.List
+import Data.Maybe (fromJust)
+import Numeric
+import HasKAL.ExternalUtils.KAGALI.KAGALIUtils as KGL
+
+
+main :: IO()
+main = do
+--     let gps = "1134486016" ::String
+     let gps = "1134572416" ::String
+     let fname = "/data/kagra/raw/full/K-K1_C-"++gps++"-32.gwf" ::String
+     let chnamesub = "TRANS_DC" ::String
+--     let chnamesub = "MIXER" ::String
+     let chname = "K1:PSL-PMC_"++chnamesub++"_OUT_DQ" ::String
+     dataVmaybe <- readFrameV chname fname     
+--     cnsig <- getArgs
+     let nsig   = 2      :: Int
+         fs     = 2048   :: Double
+         order  = 6      :: Int
+         ifmin   = 50    :: Int
+         ifmax   = 70    :: Int
+         fmin = (fromIntegral ifmin)
+         fmax = (fromIntegral ifmax)
+--         nframe = 16384  :: Int
+         nframe = 2048   :: Int
+         nshift = 10     :: Int
+         nstart = 0      :: Int
+         nend   = 2000   :: Int
+         t0     = 0      :: Double
+         fname_out = "K1_PSL_PMC_"++chnamesub++"_"++gps++"_32_fl"
+                     ++(show ifmin)++"_fh"++(show ifmax)
+                     ++"_spc"++(show nsig)
+                     ++"_frame"++(show nframe)++"_shift"
+                     ++(show nshift)++".ana" :: String
+         dataV = fromJust dataVmaybe 
+         output = KGL.butterBandPass dataV fs fmin fmax order
+     case output of
+       Left message -> print message
+       Right frameV_bp -> do
+         let outV = KGL.nha frameV_bp fs nsig nframe nshift nstart nend t0
+{--
+             outV' = [(a,b,c,d) | (a,b,c,d) <- outV
+                                , null [ e | e <- VS.toList b
+                                           , isNaN e]
+                                , null [ e | e <- VS.toList c
+                                           , isNaN e]
+                                , null [ e | e <- VS.toList d
+                                           , isNaN e]]
+                                     
+             outText = concat $ map (toText . shift) outV'
+--}
+             outText = concat $ map (toText . shift) outV
+--         writeFile "L-L1_LOSC_4_V1-931160064-4096.ana" $ outText
+         writeFile fname_out $ outText
+
+
+toText :: [[Double]] -> String
+toText xss = unlines . map (unwords . map (\x -> Numeric.showEFloat (Just 10) x "") ) . transpose $ xss 
+
+shift :: (Double, VS.Vector Double, VS.Vector Double, VS.Vector Double) -> [[Double]]
+shift (time, x, y, z) = [time', VS.toList x, VS.toList y, VS.toList z]
+  where time' = take num $ repeat time
+        num = VS.length x
+
