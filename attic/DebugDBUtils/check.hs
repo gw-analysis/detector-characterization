77c80cab20bd190cd3c4e7f0ab67459d8212e4d3
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2016-01-27 03:31:00 +0900

    added Debug code

---
+++ attic/DebugDBUtils/check.hs
@@ -0,0 +1,61 @@
+
+import System.Environment (getArgs)
+import qualified Data.Vector.Storable as V (length)
+
+import HasKAL.TimeUtils.Function (deformatGPS)
+import HasKAL.TimeUtils.GPSfunction (time2gps)
+import HasKAL.DataBaseUtils.XEndEnv.Function (kagraWaveDataGet, kagraWaveDataGetC, kagraWaveDataGet0)
+import HasKAL.WaveUtils.Data (WaveData(..))
+import HasKAL.DetectorUtils.Detector (Detector(..))
+
+main = do
+  args <- getArgs
+  (year, month, day) <- case length args of
+                         3 -> return (args!!0, show0 2 (args!!1), show0 2 (args!!2))
+                         _ -> error "Usage: TimeSeries yyyy mm dd"
+
+  {-- parameters --}
+  let ch = "K1:PEM-EX_ACC_NO2_X_FLOOR"
+      gps = read $ time2gps $ year++"-"++month++"-"++day++" 00:00:00 JST"
+      dur = 86400
+
+  {-- read data --}
+  mbWd <- kagraWaveDataGet (fromIntegral gps) (fromIntegral dur) ch KAGRA
+  mbWdC <- kagraWaveDataGetC (fromIntegral gps) (fromIntegral dur) ch
+  mbWd0 <- kagraWaveDataGet0 (fromIntegral gps) (fromIntegral dur) ch
+  let (wd, wdC, wd0) = case (mbWd, mbWdC, mbWd0) of
+              (Nothing,_ , _) -> error $ "Can't find file: "++year++"/"++month++"/"++day
+              (_, Nothing, _) -> error $ "Can't find file: "++year++"/"++month++"/"++day
+              (_, _, Nothing) -> error $ "Can't find file: "++year++"/"++month++"/"++day
+              (Just x, Just y, Just z) -> (x, y, z)
+
+  {-- main --}
+  putStrLn $ year ++ "/" ++ month ++ "/" ++ day
+  putStrLn $ "   " ++ show (gps, 0) ++ " ~ " ++ show (gps+86400, 0)
+
+  putStrLn "\nNum of Data"
+  putStrLn . ("   kagraWaveDataGet : "++) . show . V.length . gwdata $ wd
+  putStrLn . ("   kagraWaveDataGetC: "++) . show . sum . map (V.length . gwdata) $ wdC
+  putStrLn . ("   kagraWaveDataGet0: "++) . show . V.length . gwdata $ wd0
+  
+  putStrLn "\nStart GPS"
+  putStrLn . ("   kagraWaveDataGet : "++) . show . startGPSTime $ wd
+  putStrLn . ("   kagraWaveDataGetC: "++) . show . startGPSTime . head $ wdC
+  putStrLn . ("   kagraWaveDataGet0: "++) . show . startGPSTime $ wd0
+
+  putStrLn "\nStop GPS"
+  putStrLn . ("   kagraWaveDataGet : "++) . show . stopGPSTime $ wd
+  putStrLn . ("   kagraWaveDataGetC: "++) . show . stopGPSTime . last $ wdC
+  putStrLn . ("   kagraWaveDataGet0: "++) . show . stopGPSTime $ wd0
+
+  putStrLn "\nList of StartGPS ~ StopGPS (kagraWaveDataGetC)"
+  mapM (\x -> putStrLn . ("   "++) $ (show $ startGPSTime x) ++  " ~ " ++ (show $ stopGPSTime x)) wdC
+
+
+{-- Internal Functions --}
+show0 :: Int -> String -> String
+show0 digit number
+  | len < digit = (concat $ replicate (digit-len) "0") ++ number
+  | otherwise   = number
+  where len = length number
+
