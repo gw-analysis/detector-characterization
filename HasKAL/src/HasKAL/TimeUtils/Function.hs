c13431f0759607c6fb425733411a4258ea85da32
neouniverse <yamamoto@yukimura.hep.osaka-cu.ac.jp>
2016-01-28 23:30:45 +0900

    added addition and division function for GPSTIME

--- HasKAL/src/HasKAL/TimeUtils/Function.hs
+++ HasKAL/src/HasKAL/TimeUtils/Function.hs
@@ -1,6 +1,8 @@
 module HasKAL.TimeUtils.Function
 ( formatGPS
 , deformatGPS
+, diffGPS
+, addGPS
 ) where
 import HasKAL.TimeUtils.Signature
 
@@ -9,3 +11,14 @@ formatGPS x = (truncate x, truncate ((x-fromIntegral (truncate x))*1E9))
 
 deformatGPS :: GPSTIME -> Double
 deformatGPS (x, y) = (fromIntegral x) + 1E-9 * (fromIntegral y)
+
+diffGPS :: GPSTIME -> GPSTIME -> GPSTIME
+diffGPS (s1, n1) (s2, n2)
+  | n1 >= n2  = (s1-s2, n1-n2)
+  | otherwise = (s1-s2-1, n2-n1)
+
+addGPS :: GPSTIME -> GPSTIME -> GPSTIME
+addGPS (s1, n1) (s2, n2)
+  | sumN < 1000000000 = (s1+s2, sumN)
+  | otherwise         = (s1+s2+1, sumN-1000000000)
+  where sumN = n1 + n2
