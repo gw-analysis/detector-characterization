93b96b84493fc6d5743514b5e8ab9248827afde4
kagra-detchar <kagra-detchar-dev@icrr.u-tokyo.ac.jp>
2016-03-29 00:47:16 +0900

    added rule to generate daily summary page

--- HasKAL/src/HasKAL/WebUtils/DailySummaryPage.hs
+++ HasKAL/src/HasKAL/WebUtils/DailySummaryPage.hs
@@ -25,12 +25,59 @@ module HasKAL.WebUtils.DailySummaryPage
 
 
 
-import Data.List (foldl1', intercalate, elemIndex, isInfixOf)
+import Data.List (foldl', foldl1', intercalate, elemIndex, isInfixOf, nub, delete)
 import Data.List.Split (splitOn)
 import Data.Maybe (fromMaybe)
 import System.Directory (doesDirectoryExist, createDirectoryIfMissing, createDirectory)
 import System.FilePath ((</>))
 import System.Environment (getEnv)
+
+type ChName = String
+type MonName = String
+
+
+filterRule :: [(MonName, ChName)] -> [(MonName, ChName)]
+filterRule = myfilter.nub
+  where 
+    -- enabled filters
+    myfilter = rule1 .
+               rule2 .
+               rule3 .
+               rule4 .
+               rule5 .
+               rule6 .
+               rule7 .
+               rule8 . 
+               rule9 .
+               rule10 .
+               rule11
+
+    -- each filters
+    rule1 = limitMonitors "K1:GRD-PSL_STATE_N" ["TimeSeries"]
+    rule2 = limitMonitors "K1:GRD-IMC_LOCK_STATE_N" ["TimeSeries"]
+    rule3 = limitMonitors "K1:GRD-IFO_STATE_N" ["TimeSeries"]
+    rule4 = limitMonitors "K1:GRD-LSC_LOCK_STATE_N" ["TimeSeries"]
+    rule5 = limitMonitors "K1:GRD-VIS_BS_STATE_N" ["TimeSeries"]
+    rule6 = limitMonitors "K1:GRD-VIS_MCE_STATE_N" ["TimeSeries"]
+    rule7 = limitMonitors "K1:GRD-VIS_MCI_STATE_N" ["TimeSeries"]
+    rule8 = limitMonitors "K1:GRD-VIS_MCO_STATE_N" ["TimeSeries"]
+    rule9 = limitMonitors "K1:GRD-VIS_PR3_STATE_N" ["TimeSeries"]
+    rule10= limitMonitors "K1:GRD-MICH_LOCK_STATE_N" ["TimeSeries"]
+    rule11= limitMonitors "K1:LSC-MICH_ERR_CAL_OUT_DQ" ["SensMon","RMSMon","RMon","SRMon","Spectrum","Spectrogram","LT","RangeMonNSNS"]
+
+
+limitMonitors :: ChName -> [MonName] -> [(MonName, ChName)] -> [(MonName, ChName)]
+limitMonitors ch mons orig = multiDelete del orig
+  where del = multiDelete (map (flip exacta ch) mons) $ filter ((==ch).snd) orig
+
+
+exacta :: MonName -> ChName -> (MonName, ChName)
+exacta x y = (x, y)
+
+
+multiDelete :: [(MonName, ChName)] -> [(MonName, ChName)] -> [(MonName, ChName)]
+multiDelete del orig = foldl' (\bs a -> delete a bs) orig del
+
 
 genDailySummaryPage dir date chs mons subsystem ncol = do
 --  chs <- readFile chlist >>= \x -> return $ lines x
--- attic/DailyMonitor/Data.hs
+++ attic/DailyMonitor/Data.hs
@@ -6,5 +6,6 @@ where
 data Monitor = TimeSeries | Spectrum | Spectrogram
              | RMSMon     | SensMon  | LTF
              | RMon       | SRMon    | LTA
+             | RangeMonNSNS
   deriving ( Eq, Ord, Show, Read)
 
