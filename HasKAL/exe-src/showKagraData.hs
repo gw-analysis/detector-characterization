48b94abc2447c33093b0947df31d1227761310b6
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2016-05-16 14:17:40 +0900

    showKagraData chname downsamplefactor gps duration. downsamplefactor is origfs/newfs, which is Integer. if the downsamplefactor is <1, the do nothing.

--- HasKAL/exe-src/showKagraData.hs
+++ HasKAL/exe-src/showKagraData.hs
@@ -1,6 +1,7 @@
 
 import qualified Data.Vector.Storable as V
 import HasKAL.DataBaseUtils.FrameFull.Function (kagraWaveDataGetC)
+import HasKAL.SignalProcessingUtils.Resampling (downsampleWaveData)
 import HasKAL.TimeUtils.Function (deformatGPS)
 import HasKAL.TimeUtils.Signature
 import HasKAL.WaveUtils.Data (WaveData(..))
@@ -8,14 +9,15 @@ import System.Environment (getArgs)
 import System.IO (stdout, hPutStrLn)
 
 main = do
-  (ch, gps', duration') <- getArgs >>= \args-> case (length args) of
-    3 -> return (head args, args!!1, args!!2)
-    _ -> error "Usage: showKagraData chname gps duration"
+  (ch, fsfact', gps', duration') <- getArgs >>= \args-> case (length args) of
+    4 -> return (head args, args!!1, args!!2, args!!3)
+    _ -> error "Usage: showKagraData chname downsamplefactor gps duration"
   let gps = read gps' :: Int
       duration = read duration' :: Int
+      fsfact = read fsfact' :: Int
   kagraWaveDataGetC gps duration ch >>= \judge -> case judge of
     Nothing  -> putStrLn "No data found."
-    Just wav -> do let td = concatMap timendat wav
+    Just wav -> do let td = concatMap (timendat . doDownSample fsfact) wav
                    mapM_ (\(t,x) -> hPutStrLn stdout $ (show t)++" "++show x) td
 
 
@@ -24,5 +26,11 @@ timendat y = let t = deformatGPS $ startGPSTime y
                  tl = [t+i/fs|i<-[0.0,1.0..fromIntegral (length xl) -1.0]] 
                  xl = V.toList $ gwdata y
               in zip tl xl
+
+
+doDownSample fsfact w = do let fs = samplingFrequency w
+                           case fsfact > 1 of
+                             False -> w
+                             True  -> downsampleWaveData (fs/fromIntegral fsfact) w
 
 
