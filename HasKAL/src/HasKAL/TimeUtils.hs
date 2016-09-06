26f50f560e4c93d1866ea2de5e4a35c5d790567a
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2016-09-06 18:15:06 +0900

    TimeUtils.hs updated

--- HasKAL/src/HasKAL/TimeUtils.hs
+++ HasKAL/src/HasKAL/TimeUtils.hs
@@ -6,4 +6,7 @@ where
 
 import HasKAL.TimeUtils.Signature as Exports
 import HasKAL.TimeUtils.Function as Exports
+import HasKAL.TimeUtils.GPSfunction as Exports
+import HasKAL.TimeUtils.TaiUtc as Exports
+import HasKAL.TimeUtils.TimeConvert as Exports
 
