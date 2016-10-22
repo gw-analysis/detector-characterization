8b3d36e8fe14c89652647f72a17022b65c6d0ad6
kagra-detchar <kagra.detchar@gmail.com>
2016-10-22 23:35:41 +0900

    run code uploaded

--- attic/glitchmon_stack/GlitchMon/app/testGlitchmonFile.hs
+++ attic/glitchmon_stack/GlitchMon/app/testGlitchmonFile.hs
@@ -5,51 +5,55 @@ import GlitchMon.GlitchMonFile
 import GlitchMon.GlitchParam
 import GlitchMon.PipelineFunction
 import HasKAL.DataBaseUtils.FrameFull.Data
+import System.Environment ( getArgs)
 
 main = do
-  let fs = 4096 :: Double
+  fname <- getArgs >>= \args -> case (length args) of
+    1 -> return (head args)
+    _ -> error "Usage runGlitchMonFile fname"
+
+  let dfactor = 4
+  let fs = fromIntegral (16384 `div` dfactor) :: Double
   let param = GlitchParam
                { segmentLength = 32
+--               , channel = "K1:LSC-MICH_CTRL_CAL_OUT_DQ"
                , channel = "H1:LOSC-STRAIN"
-               , chunklen = 4 :: Double -- [s]
                , samplingFrequency = fs
              -- * whitening
-               , refpsdlen = 1 :: Double -- [s]
-               , whtfiltordr = 1000
+               , traindatlen = 30 :: Double -- [s] must be > 2 * fs/whnFrequencyResolution
+               , whnFrequencyResolution = 4 :: Double -- [Hz]
                , whtCoeff = []
+               , whnMethod = FrequencyDomain
              -- * t-f expression
                , nfrequency = 0.2
                , ntimeSlide = 0.03
-             -- * clustering
+             -- * clustering             
                , cutoffFractionTFT = 0.5
                , cutoffFractionTFF = 0.5
-               , cutoffFreq = 10
-               , clusterThres = 10.0
+               , cutoffFreq = 30
+               , clusterThres = 6.0
                , celement = basePixel9
-               , minimumClusterNum = 2
+               , minimumClusterNum = 6
+               , nNeighbor = 3
              -- * clean data finder
                , cdfInterval = 32
                , cdfparameter = cdfp
                , cgps = Nothing
-             -- * temporary data
---               , refpsd =
---               , refwave =
                , reftime = 0
-               -- * for debug
+             -- * for debug
                , debugmode = 1
-                 }
-
+               }                                 
       cdfp = CDFParam
               { cdf'samplingFrequency = fs
               , cdf'cutoffFrequencyLow = 10
               , cdf'cutoffFrequencyHigh = 500
               , cdf'blockSize = 50
               , cdf'fftSize = nfrequency param
-              , cdf'chunkSize = chunklen param
+              , cdf'chunkSize = traindatlen param
               }
 
 
   runGlitchMonFile param (channel param) "/data/ligo/er8/gw150914/H-H1_LOSC_4_V1-1126259446-32.gwf"
-
+--  runGlitchMonFile param (channel param) "/data/kagra/raw/full/K-K1_C-1144374208-32.gwf"
 
 
--- attic/glitchmon_stack/GlitchMon/app/testGlitchmonTime.hs
+++ attic/glitchmon_stack/GlitchMon/app/testGlitchmonTime.hs
@@ -5,28 +5,34 @@ import GlitchMon.GlitchMonTime
 import GlitchMon.GlitchParam
 import GlitchMon.PipelineFunction
 import HasKAL.DataBaseUtils.FrameFull.Data
+import System.Environment (getArgs)
 
 main = do
-  let fs = 4096
+  fname <- getArgs >>= \args -> case (length args) of
+    1 -> return (head args)
+    _ -> error "Usage runGlitchMonTime fname"
+  let dfactor = 4 ::Int
+  let fs = fromIntegral (16384 `div` dfactor) :: Double
   let param = GlitchParam
                { segmentLength = 32
                , channel = "K1:LSC-MICH_CTRL_CAL_OUT_DQ"
-               , chunklen = 4.0 :: Double --[s]
                , samplingFrequency = fs
              -- * whitening
-               , refpsdlen = 1.0 :: Double --[s]
-               , whtfiltordr = 1000
+               , traindatlen = 30.0 :: Double --[s]
+               , whnFrequencyResolution = 4.0 :: Double --[Hz]
                , whtCoeff = []
+               , whnMethod = FrequencyDomain
              -- * t-f expression
                , nfrequency = 0.2
                , ntimeSlide = 0.03
              -- * clustering
                , cutoffFractionTFT = 0.5
                , cutoffFractionTFF = 0.5
-               , cutoffFreq = 10
-               , clusterThres = 10.0
+               , cutoffFreq = 30
+               , clusterThres = 6.0
                , celement = basePixel9
-               , minimumClusterNum = 2
+               , minimumClusterNum = 6
+               , nNeighbor = 3
              -- * clean data finder
                , cdfInterval = 32
                , cdfparameter = cdfp
@@ -36,7 +42,7 @@ main = do
 --               , refwave =
                , reftime = 0
                -- * for debug
-               , debugmode = 1
+               , debugmode = 0
                  }
 
       cdfp = CDFParam
@@ -45,11 +51,11 @@ main = do
               , cdf'cutoffFrequencyHigh = 500
               , cdf'blockSize = 50
               , cdf'fftSize = nfrequency param
-              , cdf'chunkSize = chunklen param
+              , cdf'chunkSize = traindatlen param
               }
 
 
-  runGlitchMonTime param (channel param) "./dat/test.lst"
-
+  runGlitchMonTime param (channel param) fname
+--  runGlitchMonTime param (channel param) "./dat/test3.lst"
 
 
