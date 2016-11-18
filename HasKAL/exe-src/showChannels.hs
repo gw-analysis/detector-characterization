aba51f6591a1841a781b84deaf221a897d5f972e
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2016-11-19 01:40:35 +0900

    showChannels: option -f for framefile added

--- HasKAL/exe-src/showChannels.hs
+++ HasKAL/exe-src/showChannels.hs
@@ -2,13 +2,18 @@
 import Data.Int (Int32)
 import HasKAL.DataBaseUtils.FrameFull.Function (kagraDataGPS)
 import HasKAL.FrameUtils.FrameUtils (getChannelList)
+import System.Console.GetOpt
 import System.Environment (getArgs)
 import System.IO (stdout, hPutStrLn)
 
 main = do
-  gpsstr <- getArgs >>= \args -> case (length args) of
-    1 -> return (head args)
-    _ -> error "Usage: showChannels gps"
+  (varOpt, varArgs) <- getArgs >>= \optargs ->
+    case getOpt Permute options optargs of
+      (opt, args,[]) -> return (Prelude.foldl (flip id) defaultOptions opt, args)
+      (_  , _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
+        where header = "Usage: showChannels [OPTION...] file or showChannels gps..."
+
+  let gpsstr = head varArgs
 
   kagraDataGPS (read gpsstr :: Int32) >>= \maybefiles -> case maybefiles of
     Nothing -> error "no file found."
@@ -18,4 +23,19 @@ main = do
         Nothing -> error "no channel at present."
         Just x -> mapM_ (\(y, z)-> hPutStrLn stdout y) x
 
+
+data Options = Options
+  { optFile     :: FilePath
+  } deriving (Show)
+
+defaultOptions = Options
+ { optFile     = []
+ }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['f'] ["file"]
+      ( ReqArg (\ f opts -> opts {optFile = f}) "FILE")
+      "frame file"
+  ] 
 
