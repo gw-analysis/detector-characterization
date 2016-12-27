bc2065c7ef8acbd5d9c97eec73b487a5b6b61af6
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2016-12-27 18:45:51 +0900

    error message upadted

--- HasKAL/exe-src/resampler.hs
+++ HasKAL/exe-src/resampler.hs
@@ -12,10 +12,11 @@ import System.IO.Unsafe (unsafePerformIO)
 
 main = do
   {-- parameters --}
-  varArgs <- getArgs
-  let ch = head varArgs
-      (p,q) = getPQ (varArgs !! 1) ::(Int,Int)
-      fs = read (varArgs !! 2) :: Double
+  (ch, pq, fs') <- getArgs >>= \args -> case length args of
+    3 -> return (head args, args!!1, args!!2)
+    _ -> error "resampler channel P/Q fs stdin"
+  let (p,q) = getPQ pq ::(Int,Int)
+      fs = read fs' :: Double
 
   let inputPart = unsafePerformIO $ stdin2vec
       resamplePart v = resampleSV (p,q) fs v
