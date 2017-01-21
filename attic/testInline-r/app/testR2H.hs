5f8462d9d47980114b3576832ca3315aefadd5ec
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-01-22 08:41:33 +0900

    MIC in R

--- attic/testInline-r/app/testR2H.hs
+++ attic/testInline-r/app/testR2H.hs
@@ -16,7 +16,18 @@ r_fft :: [Double] -> R s [Double]
 r_fft nums = do
     H.dynSEXP <$> [r| fft(nums_hs) |]
 
+r_mic :: [Double] -> [Double] -> R s [Double]
+r_mic x y = do
+    H.dynSEXP <$> [r| require("minerva")
+                      a <- mine(x_hs,y_hs)
+                      c(a$MIC, a$MAS, a$MEV, a$MCN, a$`MIC-R2`,a$GMIC)
+                      |]
+
+
 main :: IO ()
-main = R.withEmbeddedR R.defaultConfig $ do
-    result <- R.runRegion $ r_fft [1,2,1]
+main = do
+  let a = [1,2,1] :: [Double]
+      b = [2,4,3] :: [Double]
+  R.withEmbeddedR R.defaultConfig $ do
+    result <- R.runRegion $ r_mic a b
     print result
