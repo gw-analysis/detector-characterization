5917329c55861cec6863ff3ef1affbd067ac57d5
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-02-10 13:08:00 +0900

    nds tool code

--- attic/develAppHasKAL/app/testnds.hs
+++ attic/develAppHasKAL/app/testnds.hs
@@ -3,10 +3,7 @@
 
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 
-import HasKAL.ExternalUtils.LIGO.NDS2.Function ( ndsGetData
-                                               , ndsGetChannels
-                                               , ndsGetNumberOfChannels
-                                               )
+import HasKAL.ExternalUtils.LIGO.NDS2.Function
 import HasKAL.TimeUtils.GPSfunction (time2gps)
 import qualified Data.Vector.Storable as V
 
@@ -15,10 +12,15 @@ main = do
   let chname = "K1:PEM-TEMPERATURE_RACK_IMC"
       fs = 16
       ch = [(chname,fs)]
-      ip_nds = "10.68.10.121"
+      ip_nds = "10.68.10.122"
       gpss = read (time2gps "2017-02-01 00:00:00 JST") :: Int
       gpse = read (time2gps "2017-02-01 00:00:10 JST") :: Int
       port = 8088
-      vs = ndsGetData ip_nds port ch gpss gpse 10
+      vs = getData ip_nds port ch gpss gpse 10
   print "retrieving data from the KAGRA NDS server"
   print $ take 5 $ V.toList $ head (head vs)
+  print "Current number of channels daqed in KAGRA"
+  print $ getNumberOfChannels ip_nds port gpss
+  print "Current channel list"
+  let ch0 = head $ getChannels ip_nds port gpss
+  print ch0
