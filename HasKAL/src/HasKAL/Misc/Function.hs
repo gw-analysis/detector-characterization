3a70efee93648ed2ff5132aed3e61bbe5e62c707
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-02-13 11:39:02 +0900

    mkChunksW for chunking WaveData added

--- HasKAL/src/HasKAL/Misc/Function.hs
+++ HasKAL/src/HasKAL/Misc/Function.hs
@@ -1,9 +1,11 @@
 module HasKAL.Misc.Function
   ( mkChunksV
   , mkChunksL
+  , mkChunksW
   ) where
 
 import Data.Vector.Storable as VS
+import qualified HasKAL.WaveUtils.Data as HAL
 
 
 mkChunksV :: VS.Vector Double -> Int -> Int -> [VS.Vector Double]
@@ -20,3 +22,10 @@ mkChunksL lIn on n = mkChunksLCore lIn on n ((Prelude.length lIn -on) `div` (n-o
     mkChunksLCore _ _ _ 0 = []
     mkChunksLCore lIn on n m
       = Prelude.take n lIn :  mkChunksLCore (Prelude.drop (n-on) lIn) on n (m-1)
+
+mkChunksW :: HAL.WaveData -> Int -> Int -> [HAL.WaveData]
+mkChunksW wIn on n = mkChunksLCore wIn on n ((VS.length (HAL.gwdata wIn) -on) `div` (n-on))
+  where
+    mkChunksLCore _ _ _ 0 = []
+    mkChunksLCore wIn on n m
+      = HAL.takeWaveData n wIn :  mkChunksLCore (HAL.dropWaveData (n-on) wIn) on n (m-1)
