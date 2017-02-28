fa9b64a09251bfee8b58a6453fc9b5f260d97a21
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-02-28 11:00:25 +0900

    getData in control room

---
+++ HasKAL/exe-src/ctl_getData.hs
@@ -0,0 +1,55 @@
+
+
+import qualified Data.Vector.Storable as V
+import HasKAL.ExternalUtils.LIGO.NDS2.Function ( Daq_channel_t (..)
+                                               , getChannels
+                                               , getData
+                                               , selectKeywords
+                                               , eliminateKeywords)
+import HasKAL.TimeUtils.GPSfunction (time2gps)
+import System.Console.GetOpt
+import System.Environment (getArgs)
+import System.IO (stdout, hPutStrLn)
+
+
+main :: IO ()
+main = do
+  (varOpt, varArgs) <- getArgs >>= \optargs ->
+    case getOpt Permute options optargs of
+      (opt, args,[]) -> return (Prelude.foldl (flip id) defaultOptions opt, args)
+      (_  , _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
+        where header = "Usage: ctl_getData [OPTION...] channel gps[localtime] duration"
+
+  let ip_nds = "10.68.10.122"
+      port   = 8088
+      channel = head varArgs
+      duration = read (varArgs!!2) :: Int
+      gps = case setLocaltime varOpt of
+              True  -> read (varArgs!!1) :: Int
+              False -> read (time2gps (varArgs!!1)) :: Int
+      chinfo = head $ selectKeywords [channel] $ getChannels ip_nds port gps
+      fs = ch_rate chinfo :: Double
+      v' = getData ip_nds port [(channel,fs)] gps (gps+duration) duration
+      v = head (head v')
+  mapM_ (\x -> hPutStrLn stdout $ show x) (V.toList v)
+
+
+
+data Options = Options
+  { setLocaltime     :: Bool
+--  ,
+  } deriving (Show)
+
+defaultOptions = Options
+ { setLocaltime   = False
+ }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['l'] ["localtime"]
+      ( NoArg (\ opts -> opts {setLocaltime = True}))
+      "ctl_getData -l channel \"2017-01-01-00:00:00 JST\" duration"
+--  , Option ['e'] ["eliminatekeys"]
+--      ( ReqArg (\ f opts -> opts {eliminateKeys = Just f}) "ELIMINATEKEYWORD")
+--      "set eliminatekeywords: keyword1 keyword2 ..."
+  ]
