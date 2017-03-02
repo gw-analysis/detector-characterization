a7971160973acbf9f6b826637814b38dd555ce4e
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-03-02 18:17:41 +0900

    added codes for saving data

--- attic/testKagali/app/testchirplet2.hs
+++ attic/testKagali/app/testchirplet2.hs
@@ -12,7 +12,7 @@ import qualified Numeric.LinearAlgebra as N
 
 
 main = do
-  let v = loadASCIIdataCV "dat/KRD_SFHx_ascii.dat"
+  let v = loadASCIIdataCV "/home/kazu/attic/testKagali/app/dat/KRD_SFHx_ascii.dat"
       fs = 2048 :: Double
       fsorig = 16384 :: Double
       htv = head $ V.toList $ head v
@@ -20,7 +20,13 @@ main = do
       hc = downsampleSV fsorig fs $ v !! 2
       ts' = N.scale 1E-23 $ N.randomVector 1 N.Gaussian (V.length hp) :: V.Vector Double
       ts = addInjsig 0 ts' hp
-      w = vec2wave fs htv ts
+
+      tsll' = V.toList ts'
+      tsll = V.toList ts
+  writeFile "SN_sgnal_pls_white_noise.dat" $ unwords $ map show tsll
+  writeFile "SN_white_noise.dat" $ unwords $ map show tsll'
+
+  let w = vec2wave fs htv ts
       p = ChirpletParam
         { alpha = 5 :: Double
         , ipath = 4 :: Int
