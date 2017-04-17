c371247e044137fe9ebafda97a3732cb94951566
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-04-17 13:14:18 +0900

    y-tics text in plot updated

--- attic/develAppHasKAL/app/chk_resampling.hs
+++ attic/develAppHasKAL/app/chk_resampling.hs
@@ -48,6 +48,14 @@ main = R.withEmbeddedR R.defaultConfig $ do
       [r| require("plotly") |]
       [r| require("RColorBrewer") |]
       [r|
+      # helper function
+        scientific_10 <- function(x) {
+          ifelse(x==0,parse(text="0"),parse(text=gsub("e", " %*% 10^", scientific_format(digits=1)(x))))
+        }
+        yformatter <- function (x) {
+          ind <-floor(log10(x))
+          sprintf('%3.1E',x)
+        }
         Xv1 <- tw_hs
         Yv1 <- datw_hs
         z1 <- data.frame (Xv1,Yv1,frame=1)
@@ -57,20 +65,42 @@ main = R.withEmbeddedR R.defaultConfig $ do
         Xv3 <- tdw_hs
         Yv3 <- datdw_hs
         z3 <- data.frame (Xv3,Yv3,frame=3)
+        yu = max(c(max(z1$Yv1),max(z2$Yv2),max(z3$Yv3)))
+        yl = min(c(min(z1$Yv1),min(z2$Yv2),min(z3$Yv3)))
+        ymax = max(c(abs(yu),abs(yl)))*1.1
+        if (yu*yl<0){
+          yrange <- seq(-ymax,ymax,length=7)
+        } else {
+          yrange <- seq(yl,yu,length=7)
+        }
         p <- ggplot(NULL)
-        p <- p + geom_line(data=z1,aes(x=Xv1,y=Yv1),color="red")
+        p <- p + geom_line(data=z1,aes(x=Xv1,y=Yv1),color="red")+
+          geom_hline(aes(yintercept = 0),color="grey") + geom_vline(aes(xintercept = 0),color="grey")
         p <- p + geom_line(data=z2,aes(x=Xv2,y=Yv2),color="blue")
         p <- p + geom_line(data=z3,aes(x=Xv3,y=Yv3),color="green")
         plabs <- p + labs(title = "GW150914", x = "time[s]", y = "strain")
-
         black.bold.text <- element_text(size=20,face = "bold", color = "black")
         pfin1 <- plabs + theme(title = black.bold.text
                               , axis.title = black.bold.text
-                              , axis.text = black.bold.text
+                              , axis.text.x = element_text(size=20)
+                              , axis.text.y = element_text(size=20)
+#                              , axis.text = black.bold.text
                               , axis.ticks.length = unit(.3, "cm")
-                              , axis.ticks = element_line(size = 2))
+                              , axis.ticks = element_line(size = 1)) +
+                          scale_y_continuous(label=yformatter,breaks=yrange)
         pplotly <- ggplotly (pfin1)
         htmlwidgets::saveWidget(as.widget(pplotly), "int_timeseriesGW150914.html")
-        ggsave(file = "timeseriesGW150914.png", plot = pfin1, dpi = 100, width = 10, height = 8)
+        pfin2 <- plabs + theme(title = black.bold.text
+                         , axis.title = black.bold.text
+                         , axis.text.x = element_text(size=20)
+                         , axis.text.y = element_text(size=20)
+#                         , axis.text = black.bold.text
+                         , axis.ticks.length = unit(.3, "cm")
+                         , axis.ticks = element_line(size = 1)) +
+                       scale_y_continuous(label=scientific_10,breaks=yrange)
+
+
+
+        ggsave(file = "timeseriesGW150914.png", plot = pfin2, dpi = 100, width = 10, height = 8)
         |]
       return ();
