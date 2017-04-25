7e40e24a4866130a11f0bf6c00f3802a1aadec14
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-04-25 12:59:06 +0900

    added error message

--- HasKAL/exe-src/plotter.hs
+++ HasKAL/exe-src/plotter.hs
@@ -13,36 +13,39 @@ import System.IO.Unsafe (unsafePerformIO)
 
 main = do
   {-- parameters --}
-  (varOpt, varArgs) <- getArgs >>= \optargs -> 
+  (varOpt, varArgs) <- getArgs >>= \optargs ->
      case getOpt Permute options optargs of
        (opt, args, []) -> return (Prelude.foldl (flip id) defaultOptions opt, args)
        (_  ,  _, errs) -> ioError(userError (concat errs ++ usageInfo header options))
           where header = "Usage plotter [OPTION...] ch fs t0 stdin"
 
-  let ch = head varArgs
-      fs = read (varArgs !! 1) :: Double
-      t0 = read (varArgs !! 2) :: Double
+  case (length varArgs == 3) of
+    False -> error "Usage plotter [OPTION...] ch fs t0 stdin"
+    True ->  do
+      let ch = head varArgs
+          fs = read (varArgs !! 1) :: Double
+          t0 = read (varArgs !! 2) :: Double
 
 
-  let inputPart = unsafePerformIO $ stdin2vec
-      xplotPart x = case optXPlot varOpt of
-                      False -> return x
-                      True -> do
-                        let title = ch
-                            tv = (V.fromList [t0,t0+1/fs..t0+(fromIntegral (V.length x-1))/fs], x)
+      let inputPart = unsafePerformIO $ stdin2vec
+          xplotPart x = case optXPlot varOpt of
+                          False -> return x
+                          True -> do
+                            let title = ch
+                                tv = (V.fromList [t0,t0+1/fs..t0+(fromIntegral (V.length x-1))/fs], x)
 --                        forkIO $ plotXV Linear Line 1 BLUE ("x", "y") 0.05 title ((0,0),(0,0)) tv
 --                        return x
-                        plotXV Linear Line 1 BLUE ("x", "y") 0.05 title ((0,0),(0,0)) tv
-                        return x
-      plotPart x = case optPlot varOpt of
-                     [] -> return x
-                     f  -> do
-                       let title = ch
-                           tv = (V.fromList [t0,t0+1/fs..t0+(fromIntegral (V.length x-1))/fs], x)
-                       plotV Linear Line 1 BLUE ("x", "y") 0.05 title f ((0,0),(0,0)) tv
-                       return x
-  x1 <- xplotPart $ inputPart
-  plotPart x1
+                            plotXV Linear Line 1 BLUE ("x", "y") 0.05 title ((0,0),(0,0)) tv
+                            return x
+          plotPart x = case optPlot varOpt of
+                         [] -> return x
+                         f  -> do
+                           let title = ch
+                               tv = (V.fromList [t0,t0+1/fs..t0+(fromIntegral (V.length x-1))/fs], x)
+                           plotV Linear Line 1 BLUE ("x", "y") 0.05 title f ((0,0),(0,0)) tv
+                           return x
+      x1 <- xplotPart $ inputPart
+      plotPart x1
 
 
 
@@ -59,7 +62,7 @@ defaultOptions  = Options
 
 
 options :: [OptDescr (Options -> Options)]
-options = 
+options =
   [ Option ['X'] ["Xplot"]
       ( NoArg (\ opts -> opts {optXPlot = True}))
       "X plot"
