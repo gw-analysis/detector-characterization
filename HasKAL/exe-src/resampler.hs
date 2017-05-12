194af91a763a9e98336c502b2c342d02c92ccb8c
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-05-12 14:29:49 +0900

    resampler updated. Usage: resampler [-t(--withTime)] channel P/Q fs stdin

--- HasKAL/exe-src/resampler.hs
+++ HasKAL/exe-src/resampler.hs
@@ -11,20 +11,50 @@ import System.IO.Unsafe (unsafePerformIO)
 
 
 main = do
+  (varOpt, varArgs) <- getArgs >>= \optargs ->
+    case getOpt Permute options optargs of
+      (opt, args,[]) -> return (Prelude.foldl (flip id) defaultOptions opt, args)
+      (_  , _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
+        where header = "Usage: resampler [-t(--withTime)] channel P/Q fs stdin"
+
   {-- parameters --}
-  (ch, pq, fs') <- getArgs >>= \args -> case length args of
-    3 -> return (head args, args!!1, args!!2)
-    _ -> error "resampler channel P/Q fs stdin"
+  (ch, pq, fs') <- case length varArgs of
+    3 -> return (head varArgs, varArgs!!1, varArgs!!2)
+    _ -> error "Usage: resampler [-t(--withTime)] channel P/Q fs stdin"
   let (p,q) = getPQ pq ::(Int,Int)
       fs = read fs' :: Double
 
-  let inputPart = unsafePerformIO $ stdin2vec
-      resamplePart v = resampleSV (p,q) fs v
-  mapM_ (\y -> hPutStrLn stdout $ show y) (V.toList (resamplePart inputPart))
-
+  case optTime varOpt of
+    False -> do let inputPart = unsafePerformIO $ stdin2vec
+                    resamplePart v = resampleSV (p,q) fs v
+                mapM_ (\y -> hPutStrLn stdout $ show y) (V.toList (resamplePart inputPart))
+    True  -> do let inputPart = unsafePerformIO $ stdin2vecs
+                    resamplePart v = resampleSV (p,q) fs v
+                    pp = fromIntegral p
+                    qq = fromIntegral q
+                    newfs = pp*fs/qq
+                    t0 = V.head (head inputPart)
+                    tr = [t0,t0+1/newfs..]
+                mapM_ (\(x,y) -> hPutStrLn stdout $ (show x)++" "++show y)
+                  $ zip tr (V.toList (resamplePart (inputPart!!1)))
 
 getPQ x = let a = take 2 $ map (\y-> read y :: Int) $ splitOn "/" x
            in (head a,a!!1)
 
 
+data Options = Options
+  { optTime :: Bool
+  } deriving (Show)
 
+
+defaultOptions  = Options
+  { optTime = False
+  }
+
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['t'] ["withTime"]
+      ( NoArg (\ opts -> opts {optTime = True}))
+      "Usage: resampler [-t(--withTime)] channel P/Q fs stdin"
+  ]
