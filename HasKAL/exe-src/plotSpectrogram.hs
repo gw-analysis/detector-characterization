45d803aff3fe03ca58923c34c8447210ad837de0
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-05-15 16:48:08 +0900

    added -s option: plot scales of x,y. (Linear | LogX | LogY | LogZ | LogXY | LogXZ | LogYZ | LogXYZ)

--- HasKAL/exe-src/plotSpectrogram.hs
+++ HasKAL/exe-src/plotSpectrogram.hs
@@ -5,6 +5,7 @@ import qualified Data.Vector.Storable as V
 import HasKAL.FrameUtils.Function (readFrameV)
 import HasKAL.IOUtils.Function
 import HasKAL.PlotUtils.HROOT.PlotGraph3D
+import HasKAL.PlotUtils.PlotOption.PlotOptionHROOT
 import HasKAL.SpectrumUtils.Function (mapSpectrogram)
 import HasKAL.SpectrumUtils.SpectrumUtils (gwspectrogramV)
 import System.Console.GetOpt
@@ -26,9 +27,14 @@ main = do
       t0 = read (varArgs !! 2) :: Double
       dt = read (varArgs !! 3) :: Double
       ot = read (varArgs !! 4) :: Double
+      plotscale = read (optScale varOpt) :: LogOption
+
 
   {-- read data --}
-  let inputPart = unsafePerformIO $ stdin2vec
+  let inputPart' = unsafePerformIO $ stdin2vecs
+  let inputPart | length inputPart' == 1 = head inputPart'
+                | length inputPart' >= 2 = inputPart' !! 1
+                | otherwise = error "unknown input"
 
   {-- plot parameter --}
   let dtfft = show dt
@@ -37,19 +43,19 @@ main = do
               z = "dt_{FFT}="++dtfft++"s"
 
   {-- plot --}
-  let xplotPart x = 
+  let xplotPart x =
         case optXPlot varOpt of
           False -> return x
           True -> do
             let y = mapSpectrogram sqrt $ gwspectrogramV  (floor (ot*fs)) (floor (dt*fs)) fs x
-            histgram2dMX LogYZ COLZ ("time", "frequency [Hz]", "[1/rHz]") title ((0,0),(0,0)) y
+            histgram2dMX plotscale COLZ ("time", "frequency [Hz]", "[1/rHz]") title ((0,0),(0,0)) y
             return x
-      plotPart x = 
+      plotPart x =
         case optPlot varOpt of
           [] -> return x
           oFile  -> do
             let y = mapSpectrogram sqrt $ gwspectrogramV  (floor (ot*fs)) (floor (dt*fs)) fs x
-            histgram2dM LogYZ COLZ ("time", "frequency [Hz]", "[1/rHz]") title oFile ((0,0),(0,0)) y
+            histgram2dM plotscale COLZ ("time", "frequency [Hz]", "[1/rHz]") title oFile ((0,0),(0,0)) y
             return x
 
   result1 <- xplotPart inputPart
@@ -60,12 +66,14 @@ main = do
 data Options = Options
  { optXPlot    :: Bool
  , optPlot     :: FilePath
+ , optScale    :: String
  } deriving (Show)
 
 
 defaultOptions = Options
  { optXPlot    = True
  , optPlot     = []
+ , optScale    = "LogYZ"
  }
 
 
@@ -77,8 +85,7 @@ options =
   , Option ['p'] ["plot"]
       ( ReqArg (\p opts -> opts {optPlot = p}) "FILE")
       "plot file"
+  , Option ['s'] ["scale"]
+      ( ReqArg (\s opts -> opts {optScale = s}) "plot Scale")
+      "plot scale"
   ]
-
-
-
-
