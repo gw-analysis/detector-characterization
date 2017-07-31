0ac1990e59d431afa82d2ec1a8e04f173a070f8f
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-07-31 17:12:17 +0900

    comment added

--- HasKAL/src/HasKAL/SpectrumUtils/Signature.hs
+++ HasKAL/src/HasKAL/SpectrumUtils/Signature.hs
@@ -5,5 +5,5 @@ module HasKAL.SpectrumUtils.Signature where
 import Numeric.LinearAlgebra
 
 
-type Spectrogram = (Vector Double, Vector Double, Matrix Double)
+type Spectrogram = (Vector Double, Vector Double, Matrix Double) --(t,f,specgram)
 type Spectrum = (Vector Double, Vector Double)
