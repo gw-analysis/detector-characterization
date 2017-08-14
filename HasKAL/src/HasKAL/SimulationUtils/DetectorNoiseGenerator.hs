2cf8a4f78cae8303be94e3519026a1fef425178f
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2017-08-14 16:16:19 +0900

    genDetNoise: simulated time-series of detector noise

--- HasKAL/src/HasKAL/SimulationUtils/DetectorNoiseGenerator.hs
+++ HasKAL/src/HasKAL/SimulationUtils/DetectorNoiseGenerator.hs
@@ -1,9 +1,10 @@
 
 
-module HasKAL.SimulationUtils.DetectorNoiseGenerator (
-   geneNPSD
-  ,geneNPSDV
-  ,geneNPSD'
+module HasKAL.SimulationUtils.DetectorNoiseGenerator
+( geneNPSD
+, geneNPSDV
+, geneNPSD'
+, genDetNoise
 --   ,geneNonGaussNPSD
 ) where
 
@@ -14,12 +15,16 @@ import qualified Data.List as DL
 import qualified Numeric.LinearAlgebra as NLA
 import Data.Vector.Storable ((!))
 import qualified Data.Vector.Storable as V
+import System.IO.Unsafe (unsafePerformIO)
 
-import qualified HasKAL.DetectorUtils.Detector as HDD 
+import qualified HasKAL.DetectorUtils.Detector as HDD
 import qualified HasKAL.ExternalUtils.GSL.RandomNumberGeneration as RNG
 import qualified HasKAL.ExternalUtils.GSL.RandomNumberDistributions as RND
 import qualified HasKAL.Misc.StrictMapping as HMS
+import qualified HasKAL.SignalProcessingUtils.FilterDesign as FD
+import qualified HasKAL.SignalProcessingUtils.FilterX as FX
 import qualified HasKAL.SpectrumUtils.DetectorSensitivity as HSD
+import HasKAL.TimeUtils.GPSfunction (getCurrentGps)
 
 
 {-- Test Code --}
@@ -78,8 +83,8 @@ geneNPSD' num ifo fin = do
   let sensCurve = (zip fin).(map sqrt).NLA.toList.(HSD.ifonoisepsd ifo).NLA.fromList $ fin
   rng <- RNG.newRngWithSeed (-1)
 
-  HMS.forM' [1..num] $ \idx -> do 
-    HMS.forM' sensCurve $ \(freq, curve) -> do 
+  HMS.forM' [1..num] $ \idx -> do
+    HMS.forM' sensCurve $ \(freq, curve) -> do
       real <- RND.gslRanGaussian rng (sqrt 0.5)
       imag <- RND.gslRanGaussian rng (sqrt 0.5)
       return $ (freq, (curve :+ 0) * (real :+ imag) )
@@ -95,9 +100,25 @@ geneNPSD' num ifo fin = do
 -- geneNonGaussNPSD rng ifo fin nu = do
 --   let sensCurve = (zip fin).(map sqrt).NLA.toList.(HSD.ifonoisepsd ifo).NLA.fromList $ fin
 
---   HMS.forM' sensCurve $ \(freq, curve) -> do 
+--   HMS.forM' sensCurve $ \(freq, curve) -> do
 --     real <- RND.gslRanGaussian rng nu
 --     imag <- RND.gslRanGaussian rng nu
 --     return $ (freq, (curve/nu :+ 0) * (real :+ imag) )
 
 
+genDetNoise :: HDD.Detector
+            -> Int
+            -> Double
+            -> Double
+            -> V.Vector Double
+genDetNoise ifo ntap fs dt = unsafePerformIO $ do
+  gps <- getCurrentGps >>= \t -> return (read t :: Int)
+  let n = floor (fs*(dt+2)) :: Int
+      randDat = NLA.randomVector gps NLA.Gaussian n
+      f = [0..fs/2] :: [Double]
+      vf = V.fromList f :: V.Vector Double
+      psd = HSD.ifonoisepsd ifo vf :: V.Vector Double
+      spe = V.map (sqrt . (*fs)) psd
+      (b, a) = FD.fir2 ntap (vf, spe)
+      filtered = FX.filtfilt0 (b,a) randDat
+  return (V.drop (floor fs) . V.take (n-floor fs) $ filtered)
